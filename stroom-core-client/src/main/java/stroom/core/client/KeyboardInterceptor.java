5c5924166ad103350cf415def988686aeb1a2f70
stroomdev66 <stroomdev66@gmail.com>
2016-11-25 16:00:54 +0000

    Merge branch 'dev' of github.com:gchq/stroom into StandaloneDashboard
    # Conflicts:
    #	stroom-app/pom.xml
    #	stroom-core-client/src/main/java/stroom/core/client/KeyboardInterceptor.java
    #	stroom-dashboard/pom.xml

--- stroom-core-client/src/main/java/stroom/core/client/KeyboardInterceptor.java
+++ stroom-core-client/src/main/java/stroom/core/client/KeyboardInterceptor.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package stroom.app.client;
+package stroom.core.client;
 
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.dom.client.KeyDownEvent;
