d7688bc357fb32df1bab1a47985de011667d4a25
stroomdev66 <stroomdev66@gmail.com>
2017-01-12 17:45:21 +0000

    Issue #88 : Pipeline reference edit dialog now correctly selects the current stream type.

--- stroom-core-client/src/main/java/stroom/pipeline/structure/client/presenter/PipelineReferenceListPresenter.java
+++ stroom-core-client/src/main/java/stroom/pipeline/structure/client/presenter/PipelineReferenceListPresenter.java
@@ -315,7 +315,7 @@ public class PipelineReferenceListPresenter extends MyPresenterWidget<DataGridVi
                                 added.add(pipelineReference);
                             }
 
-                            setDirty(true);
+                            setDirty(isNew || editor.isDirty());
                             refresh();
                             HidePopupEvent.fire(PipelineReferenceListPresenter.this, editor);
                         }
