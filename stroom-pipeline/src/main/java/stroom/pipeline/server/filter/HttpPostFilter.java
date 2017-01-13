d4012c90ed025e1c41a4f45e021abc8608e4cb4a
jc064522 <jc064522@gmail.com>
2017-01-13 08:13:34 +0000

    Add error handling to HttpPostFilter

--- stroom-pipeline/src/main/java/stroom/pipeline/server/filter/HttpPostFilter.java
+++ stroom-pipeline/src/main/java/stroom/pipeline/server/filter/HttpPostFilter.java
@@ -14,17 +14,18 @@ import stroom.util.logging.StroomLogger;
 import stroom.util.spring.StroomScope;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Component
 @Scope(StroomScope.PROTOTYPE)
 @ConfigurableElement(type = "HttpPostFilter", category = PipelineElementType.Category.FILTER, roles = {
         PipelineElementType.ROLE_TARGET, PipelineElementType.ROLE_HAS_TARGETS,
-        PipelineElementType.VISABILITY_SIMPLE }, icon = ElementIcons.STREAM)
+        PipelineElementType.VISABILITY_SIMPLE}, icon = ElementIcons.STREAM)
 public class HttpPostFilter extends TestFilter {
-    private final Client client = Client.create();
-    private WebResource webResource;
 
     private static final StroomLogger LOGGER = StroomLogger.getLogger(HttpPostFilter.class);
+    private final Client client = Client.create();
+    private WebResource webResource;
 
     @Override
     public void endDocument() throws SAXException {
@@ -32,22 +33,27 @@ public class HttpPostFilter extends TestFilter {
         String xml = getOutput();
         try {
             ClientResponse response = webResource
-                    .accept("application/xml", "application/json")
+                    .accept(MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON)
                     .type(MediaType.APPLICATION_XML)
                     .post(ClientResponse.class, xml);
-            LOGGER.info("POSTed document to API.");
-        } catch ( Exception e){
-            LOGGER.error("Unable to POST document to API.");
+            if (response.getStatusInfo() != Response.Status.ACCEPTED) {
+                //TODO Add stroom error handling
+                LOGGER.error("POST was not accepted by the API: {}", response);
+            } else {
+                LOGGER.info("POSTed document to API.");
+            }
+        } catch (Exception e) {
+            LOGGER.error("Unable to POST document to API: {}", e);
         }
     }
 
     @PipelineProperty(description = "The URL of the receiving API.")
-    public void setReceivingApiUrl(final String receivingApiUrl){
+    public void setReceivingApiUrl(final String receivingApiUrl) {
         try {
             webResource = client.resource(receivingApiUrl);
-        } catch(IllegalArgumentException | NullPointerException e){
-            LOGGER.error("Bad URL: {}", receivingApiUrl);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            //TODO: Add stroom error handling
+            LOGGER.error("Unable to create an API client with this URL: {}. Exception was: {}", receivingApiUrl, e);
         }
     }
-
 }
