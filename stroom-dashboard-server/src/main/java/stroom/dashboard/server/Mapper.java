139c75309c8c95f219a4bdbe2b95ba7745effe12
stroomdev66 <stroomdev66@gmail.com>
2017-01-27 13:54:40 +0000

    Started constructing mapper.

--- stroom-dashboard-server/src/main/java/stroom/dashboard/server/Mapper.java
+++ stroom-dashboard-server/src/main/java/stroom/dashboard/server/Mapper.java
@@ -16,12 +16,79 @@
 
 package stroom.dashboard.server;
 
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MappingContext;
+import org.modelmapper.spi.MatchingStrategy;
+import stroom.query.api.Param;
+import stroom.query.api.Query;
 import stroom.query.api.SearchRequest;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Mapper {
+    private static Param[] convert(final Map<String, String> paramMap) {
+        if (paramMap == null || paramMap.size() == 0) {
+            return null;
+        }
+
+        final Param[] arr = new Param[paramMap.size()];
+        int i = 0;
+        for (final Entry<String, String> entry : paramMap.entrySet()) {
+            arr[i++] = new Param(entry.getKey(), entry.getValue());
+        }
+
+        return arr;
+    }
+
     public SearchRequest mapRequest(final stroom.dashboard.shared.SearchRequest in) {
+//        Converter<Map<String, String>, Collection<Param>> mapCollectionConverter = new Converter<Map<String, String>, Collection<Param>>() {
+//            @Override
+//            public Collection<Param> convert(final MappingContext<Map<String, String>, Collection<Param>> context) {
+//                return null;
+//            }
+//        };
+
+        PropertyMap<stroom.dashboard.shared.Search, Query> searchMapping = new PropertyMap<stroom.dashboard.shared.Search, Query>() {
+            @Override
+            protected void configure() {
+                map(source.getDataSourceRef(), destination.getDataSource());
+                map(source.getExpression(), destination.getExpression());
+                skip(source.getComponentSettingsMap());
+//                map().setParams(convert(source.getParamMap()));
+
+                skip(source.getParamMap());
+                skip().setParams(null);
+
+                skip(source.getIncremental());
+            }
+        };
+
+        PropertyMap<stroom.dashboard.shared.SearchRequest, SearchRequest> searchRequestMapping = new PropertyMap<stroom.dashboard.shared.SearchRequest, SearchRequest>() {
+            @Override
+            protected void configure() {
+                skip().setKey(null);
+                map(source.getSearch(), destination.getQuery());
+//                map(source.getComponentResultRequests(), destination.getResultRequests());
+
+                skip(source.getComponentResultRequests());
+                skip().setResultRequests(null);
+
+                map().setDateTimeLocale(source.getDateTimeLocale());
+                map().setIncremental(source.getSearch().getIncremental());
+            }
+        };
+
         final ModelMapper modelMapper = new ModelMapper();
+//        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        modelMapper.addMappings(searchRequestMapping);
+        modelMapper.addMappings(searchMapping);
+        modelMapper.validate();
+
         SearchRequest out = modelMapper.map(in, SearchRequest.class);
         return out;
     }
--- stroom-dashboard-server/src/test/java/stroom/dashboard/server/TestMapper.java
+++ stroom-dashboard-server/src/test/java/stroom/dashboard/server/TestMapper.java
@@ -17,12 +17,89 @@
 package stroom.dashboard.server;
 
 import org.junit.Test;
+import stroom.dashboard.shared.ComponentResultRequest;
+import stroom.dashboard.shared.ComponentSettings;
+import stroom.dashboard.shared.DateTimeFormatSettings;
+import stroom.dashboard.shared.Field;
+import stroom.dashboard.shared.Filter;
+import stroom.dashboard.shared.Format;
+import stroom.dashboard.shared.Format.Type;
+import stroom.dashboard.shared.NumberFormatSettings;
+import stroom.dashboard.shared.Search;
 import stroom.dashboard.shared.SearchRequest;
+import stroom.dashboard.shared.Sort;
+import stroom.dashboard.shared.TableComponentSettings;
+import stroom.dashboard.shared.TableResultRequest;
+import stroom.dashboard.shared.TimeZone;
+import stroom.entity.shared.SharedDocRef;
+import stroom.query.api.DocRef;
+import stroom.query.api.ExpressionOperator;
+import stroom.query.api.ExpressionOperator.Op;
+import stroom.query.api.ExpressionTerm.Condition;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class TestMapper {
     @Test
     public void testRequest() {
         final Mapper mapper = new Mapper();
-        mapper.mapRequest(new SearchRequest());
+        final stroom.query.api.SearchRequest result = mapper.mapRequest(getSearchRequest());
+    }
+
+    private static SearchRequest getSearchRequest() {
+        DocRef docRef = new DocRef("docRefType", "docRefUuid", "docRefName");
+
+        ExpressionOperator expressionOperator = new ExpressionOperator(Op.AND);
+        expressionOperator.addTerm("field1", Condition.EQUALS, "value1");
+        expressionOperator.addOperator(Op.AND);
+        expressionOperator.addTerm("field2", Condition.BETWEEN, "value2");
+
+        TableComponentSettings tableSettings = new TableComponentSettings();
+        tableSettings.setQueryId("someQueryId");
+        tableSettings.addField(new Field("name1", "expression1",
+                new Sort(1, Sort.SortDirection.ASCENDING),
+                new Filter("include1", "exclude1"),
+                new Format(Type.NUMBER, new NumberFormatSettings(1, false)), 1, 200, true));
+        tableSettings.addField(new Field("name2", "expression2",
+                new Sort(2, Sort.SortDirection.DESCENDING),
+                new Filter("include2", "exclude2"),
+                new Format(Type.DATE_TIME, createDateTimeFormat()), 2, 200, true));
+        tableSettings.setExtractValues(false);
+        tableSettings.setExtractionPipeline(new SharedDocRef("docRefType2", "docRefUuid2", "docRefName2"));
+        tableSettings.setMaxResults(new int[]{1, 2});
+        tableSettings.setShowDetail(false);
+
+        Map<String, ComponentSettings> componentSettingsMap = new HashMap<>();
+        componentSettingsMap.put("componentSettingsMapKey", tableSettings);
+
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("param1", "val1");
+        paramMap.put("param2", "val2");
+
+        final Search search = new Search(docRef, expressionOperator, componentSettingsMap, paramMap, true);
+
+        final Map<String, ComponentResultRequest> componentResultRequestMap = new HashMap<>();
+        for (final Entry<String, ComponentSettings> entry : componentSettingsMap.entrySet()) {
+            TableResultRequest tableResultRequest = new TableResultRequest();
+            tableResultRequest.setTableSettings((TableComponentSettings) entry.getValue());
+            tableResultRequest.setWantsData(true);
+            componentResultRequestMap.put(entry.getKey(), tableResultRequest);
+        }
+
+        SearchRequest searchRequest = new SearchRequest(search, componentResultRequestMap, "en-gb");
+
+        return searchRequest;
+    }
+
+    private static DateTimeFormatSettings createDateTimeFormat() {
+        final TimeZone timeZone = TimeZone.fromOffset(2, 30);
+
+        final DateTimeFormatSettings dateTimeFormat = new DateTimeFormatSettings();
+        dateTimeFormat.setPattern("yyyy-MM-dd'T'HH:mm:ss");
+        dateTimeFormat.setTimeZone(timeZone);
+
+        return dateTimeFormat;
     }
 }
