4c1f727fd6c6feb2ce5848a68a80b4ae360a8564
stroomdev66 <stroomdev66@gmail.com>
2017-02-07 11:35:14 +0000

    Issue #50 : Defaults table result fields to use local time without outputting the timezone.

--- stroom-dashboard/src/main/java/stroom/dashboard/client/table/FormatPresenter.java
+++ stroom-dashboard/src/main/java/stroom/dashboard/client/table/FormatPresenter.java
@@ -1,17 +1,19 @@
 /*
- * Copyright 2016 Crown Copyright
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2017 Crown Copyright
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package stroom.dashboard.client.table;
@@ -70,7 +72,7 @@ public class FormatPresenter extends MyPresenterWidget<FormatPresenter.FormatVie
             getView().setWrap(false);
         }
 
-        final PopupSize popupSize = new PopupSize(390, 193, 390, 193, true);
+        final PopupSize popupSize = new PopupSize(390, 217, 390, 217, true);
         ShowPopupEvent.fire(tablePresenter, this, PopupType.OK_CANCEL_DIALOG, popupSize,
                 "Format '" + field.getName() + "'", this);
     }
