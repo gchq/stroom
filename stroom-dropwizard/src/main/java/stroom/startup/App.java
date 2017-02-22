c4452638003c4ed94372f4a8c9bfcec616bb3413
jc064522 <jc064522@gmail.com>
2017-02-22 18:44:31 +0000

    Add asset bundle for static resources

--- stroom-dropwizard/src/main/java/stroom/startup/App.java
+++ stroom-dropwizard/src/main/java/stroom/startup/App.java
@@ -1,9 +1,11 @@
 package stroom.startup;
 
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import stroom.Config;
+import stroom.security.spring.SecurityConfiguration;
 import stroom.util.spring.StroomSpringProfiles;
 import stroom.util.thread.ThreadScopeContextHolder;
 
@@ -14,16 +16,16 @@ public class App extends Application<Config> {
 
     @Override
     public void initialize(Bootstrap<Config> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/webapp", "/stroom/", "stroom.jsp", "webapp"));
     }
 
     @Override
     public void run(Config configuration, Environment environment) throws Exception {
         // We need to set this otherwise we won't have all the beans we need.
-        System.setProperty("spring.profiles.active", StroomSpringProfiles.PROD);
+        System.setProperty("spring.profiles.active", String.format("%s,%s", StroomSpringProfiles.PROD, SecurityConfiguration.PROD_SECURITY));
 
         // We need to prime this otherwise we won't have a thread scope context and bean initialisation will fail
         ThreadScopeContextHolder.createContext();
-
 
         ApplicationContexts.configure(configuration, environment);
         Servlets.loadInto(environment);
