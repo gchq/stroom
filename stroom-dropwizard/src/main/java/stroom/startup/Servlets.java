0ca19b9406c4e0f83fea57062e918719ac8e26f5
jc064522 <jc064522@gmail.com>
2017-02-22 18:45:26 +0000

    Fix servlets

--- stroom-dropwizard/src/main/java/stroom/startup/Servlets.java
+++ stroom-dropwizard/src/main/java/stroom/startup/Servlets.java
@@ -10,58 +10,66 @@ public class Servlets {
      */
     @SuppressWarnings("unchecked")
     static void loadInto(Environment environment) throws ClassNotFoundException {
-        environment.getApplicationContext().addServlet(Servlets.createLog4jServlet(), "");
-        environment.getApplicationContext().addServlet(Servlets.createUpgradeDispatcherServlet(), "/thing/*");
-        environment.getApplicationContext().addServlet(Servlets.createServletContainer(), "/rest/*");
+        addServlet(environment, Servlets.newLog4jServlet(), "", null);
+        addServlet(environment, Servlets.newUpgradeDispatcherServlet(), "/", new String[]{
+                "*.rpc",
+                "/dynamic.css",
+                "/script",
+                "/datafeed",
+                "/datafeed/*",
+                "/resourcestore/*",
+                "/export/*",
+                "/echo",
+                "/debug",
+                "/status",
+                "/sessionList",
+                "/gwtRequest"
+        });
+        addServlet(environment, Servlets.newServletContainer(), "/rest/*", null);
     }
 
-    private static ServletHolder createLog4jServlet() {
+    private static void addServlet(
+            Environment environment,
+            ServletHolder servletHolder,
+            String servletMapping,
+            String[] furtherServletMappings){
+        environment.getApplicationContext().addServlet(servletHolder, servletMapping);
+        if(furtherServletMappings != null && furtherServletMappings.length > 0){
+            servletHolder.getRegistration().addMapping(furtherServletMappings);
+        }
+    }
+
+    private static ServletHolder newLog4jServlet() {
         ServletHolder servlet = new ServletHolder(stroom.util.logging.Log4JServlet.class);
         servlet.setName("log4j");
         servlet.setInitParameter("log4j", "classpath:log4j.xml");
         return servlet;
     }
 
-    private static ServletHolder createUpgradeDispatcherServlet() {
+    private static ServletHolder newUpgradeDispatcherServlet() {
         ServletHolder servlet = new ServletHolder(stroom.util.upgrade.UpgradeDispatcherServlet.class);
         servlet.setName("spring");
         servlet.setInitParameter("spring.profiles.active", "production,PROD_SECURITY");
-        servlet.setInitParameter("upgrade-class", "stroom.upgrade.StroomUpgradeHandler");
+//        servlet.setInitParameter("upgrade-class", "stroom.upgrade.StroomUpgradeHandler");
         servlet.setInitParameter("contextClass", "org.springframework.web.context.support.AnnotationConfigWebApplicationContext");
-        servlet.setInitParameter("contextConfigLocation", "stroom.spring.ScopeConfiguration,\n" +
-                "                stroom.spring.PersistenceConfiguration,\n" +
-                "                stroom.spring.ServerComponentScanConfiguration,\n" +
-                "                stroom.spring.ServerConfiguration,\n" +
-                "                stroom.spring.CachedServiceConfiguration,\n" +
-                "                stroom.logging.spring.EventLoggingConfiguration,\n" +
-                "                stroom.index.spring.IndexConfiguration,\n" +
-                "                stroom.search.spring.SearchConfiguration,\n" +
-                "                stroom.script.spring.ScriptConfiguration,\n" +
-                "                stroom.visualisation.spring.VisualisationConfiguration,\n" +
-                "                stroom.dashboard.spring.DashboardConfiguration,\n" +
-                "                stroom.spring.CoreClientConfiguration,\n" +
-                "                stroom.statistics.spring.StatisticsConfiguration,\n" +
-                "                stroom.security.spring.SecurityConfiguration");
-//        if(servlet.getServletHandler() == null){
-//            System.out.println("NULL!");
-//        }
-//        servlet.getRegistration().addMapping(
-//                "*.rpc",
-//                "/dynamic.css",
-//                "/script",
-//                "/datafeed",
-//                "/datafeed/*",
-//                "/resourcestore/*",
-//                "/export/*",
-//                "/echo",
-//                "/debug",
-//                "/status",
-//                "/sessionList",
-//                "/gwtRequest");
+//        servlet.setInitParameter("contextConfigLocation", "stroom.spring.ScopeConfiguration,\n" +
+//                "                stroom.spring.PersistenceConfiguration,\n" +
+//                "                stroom.spring.ServerComponentScanConfiguration,\n" +
+//                "                stroom.spring.ServerConfiguration,\n" +
+//                "                stroom.spring.CachedServiceConfiguration,\n" +
+//                "                stroom.logging.spring.EventLoggingConfiguration,\n" +
+//                "                stroom.index.spring.IndexConfiguration,\n" +
+//                "                stroom.search.spring.SearchConfiguration,\n" +
+//                "                stroom.script.spring.ScriptConfiguration,\n" +
+//                "                stroom.visualisation.spring.VisualisationConfiguration,\n" +
+//                "                stroom.dashboard.spring.DashboardConfiguration,\n" +
+//                "                stroom.spring.CoreClientConfiguration,\n" +
+//                "                stroom.statistics.spring.StatisticsConfiguration,\n" +
+//                "                stroom.security.spring.SecurityConfiguration");
         return servlet;
     }
 
-    private static ServletHolder createServletContainer(){
+    private static ServletHolder newServletContainer(){
         ServletHolder servlet = new ServletHolder(org.glassfish.jersey.servlet.ServletContainer.class);
         servlet.setName("SpringApplication");
         servlet.setInitParameter("javax.ws.rs.Application", "stroom.search.spring.JerseyApplication");
