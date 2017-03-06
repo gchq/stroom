cf4cc4aeba6a6f2bbedeb88466bfa736674981c3
jc064522 <jc064522@gmail.com>
2017-03-06 07:24:27 +0000

    Update config

--- stroom-dropwizard/src/main/java/stroom/Config.java
+++ stroom-dropwizard/src/main/java/stroom/Config.java
@@ -2,5 +2,16 @@ package stroom;
 
 import io.dropwizard.Configuration;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 public class Config extends Configuration {
+
+    @NotNull
+    @Valid
+    private String uiPath;
+
+    public String getUiPath() {
+        return uiPath;
+    }
 }
