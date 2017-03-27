4ed803b532b886ffbf2c1d2b4ff3d6d05b472adc
jc064522 <jc064522@gmail.com>
2017-03-27 07:16:22 +0100

    Add dependency building to CI

---
+++ .travis.install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function buildIt {
+    if [ -f "build.gradle" ]; then
+        ./gradlew clean build publishToMavenLocal
+    else
+        mvn clean install
+    fi
+}
+
+function sortItAllOut {
+    if [[ -d $1 ]]; then
+        # Update the repo
+        echo -e "Checking repo \e[96m$1\e[0m for updates"
+        cd $1
+        git pull
+
+        buildIt
+
+        cd ..
+    else
+        # Clone the repo
+        echo -e "Cloning \e[96m$1\e[0m"
+        git clone git@github.com:gchq/$1.git
+        cd $1
+
+        buildIt
+
+        cd ..
+    fi
+}
+
+sortItAllOut event-logging
+sortItAllOut hadoop-hdfs-shaded
+sortItAllOut hadoop-common-shaded
+sortItAllOut hbase-common-shaded
