f4deb685cb41b6661622cf838b7ffcf695aa27d3
stroomdev66 <stroomdev66@gmail.com>
2017-04-06 10:23:00 +0100

    Removed default memory options.

--- stroom-app-distribution/bin/env.sh
+++ stroom-app-distribution/bin/env.sh
@@ -2,4 +2,4 @@ ulimit -v unlimited
  
 export MALLOC_ARENA_MAX=4
 
-export JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true -Xms1g -Xmx8g @@JAVA_OPTS@@"
+export JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true @@JAVA_OPTS@@"
