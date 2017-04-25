7b072ad55566fe24269585f51a10eaba6f545d39
jc064522 <jc064522@gmail.com>
2017-04-25 06:48:16 +0100

    Improve naming in URL download plugin

--- buildSrc/src/main/groovy/GitHubReleaseDependency.groovy
+++ buildSrc/src/main/groovy/GitHubReleaseDependency.groovy
@@ -4,34 +4,31 @@ import org.gradle.api.Project
 class GitHubReleaseDependency implements Plugin<Project> {
     void apply(Project project) {
 
-        project.extensions.create("gitHubDeps", GitHubReleaseDependencyExtension)
-        project.task('hello') {
-            doLast {
-                println "Hello from the GitHubReleaseDependency"
-            }
-        }
+        project.extensions.create("urlDependencies", GitHubReleaseDependencyExtension)
 
-        project.task('setupFileDependencies') {
+        project.task('downloadUrlDependencies') {
             shouldRunAfter 'assemble'
             doLast {
-                for(dependency in project.gitHubDeps.dependencies){
+                for(dependency in project.urlDependencies.dependencies){
                     download(dependency.value, "libs/${dependency.key}.jar")
 
-                    project.gitHubDeps.files.put(dependency.key, "libs/${dependency.key}.jar")
+                    project.urlDependencies.files.put(dependency.key, "libs/${dependency.key}.jar")
                 }
             }
         }
     }
 
     def download(String remoteUrl, String localUrl) {
-        // TODO This works for getting the content length
+        // TODO This works for getting the content length - using this would make the selective download more
+        // sophisticated - the user wouldn't have to delete the files to re-download. But if the deps
+        // are named with version this probably won't be a big issue.
 //        URLConnection connection = new URL(remoteUrl).openConnection()
 //        def contentLength = connection.contentLength
 
         def file = new File(localUrl)
         if(!file.exists()) {
             new File("$localUrl").withOutputStream { out ->
-                new URL(remoteUrl).withInputStream { from -> out << from; }
+                new URL(remoteUrl).withInputStream { from -> out << from }
             }
         }
     }
