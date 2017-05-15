4ab4f16fa8c418bc1290b6ccc7be25751a0324ae
jc064522 <jc064522@gmail.com>
2017-05-15 16:06:37 +0100

    Added serialisation annotations

--- stroom-app/src/main/java/stroom/resources/AuthorisationRequest.java
+++ stroom-app/src/main/java/stroom/resources/AuthorisationRequest.java
@@ -1,10 +1,15 @@
 package stroom.resources;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import stroom.query.api.DocRef;
 
 public class AuthorisationRequest {
+    @JsonProperty
     private DocRef docRef;
+    @JsonProperty
     private String permissions;
+
+    public AuthorisationRequest() {}
 
     public AuthorisationRequest(DocRef docRef, String permissions){
         this.docRef = docRef;
