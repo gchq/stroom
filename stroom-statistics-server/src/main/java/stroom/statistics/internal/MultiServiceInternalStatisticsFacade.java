cdffce48c8a34f5f06fa35d9d9a523cd638046ec
at055612 <at055612@gmail.com>
2017-06-07 16:06:04 +0100

    Remove async execution

--- stroom-statistics-server/src/main/java/stroom/statistics/internal/MultiServiceInternalStatisticsFacade.java
+++ stroom-statistics-server/src/main/java/stroom/statistics/internal/MultiServiceInternalStatisticsFacade.java
@@ -6,8 +6,6 @@ import stroom.query.api.v1.DocRef;
 
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -25,21 +23,37 @@ class MultiServiceInternalStatisticsFacade implements InternalStatisticsFacade {
 
     @Override
     public void putEvents(List<InternalStatisticEvent> statisticEvents, Consumer<Throwable> exceptionHandler) {
+        ;
 
-        //Group the events by service and docref
-        Map<InternalStatisticsService, Map<DocRef, List<InternalStatisticEvent>>> serviceToEventsMapMap = statisticEvents.stream()
-                .flatMap(event ->
-                        internalStatisticDocRefCache.getDocRefs(event.getKey()).stream()
-                                .map(docRef -> new Tuple3<>(docRefTypeToServiceMap.get(docRef.getType()), docRef, event))
-                                .filter(tuple3 -> tuple3._1() != null))
-                .collect(Collectors.groupingBy(
-                        Tuple3::_1, //service
-                        Collectors.groupingBy(
-                                Tuple3::_2, //docRef
-                                Collectors.mapping(
-                                        Tuple3::_3, //event
-                                        Collectors.toList()))));
+        try {
+            //Group the events by service and docref
+            Map<InternalStatisticsService, Map<DocRef, List<InternalStatisticEvent>>> serviceToEventsMapMap =
+                    statisticEvents.stream()
+                            .flatMap(event ->
+                                    internalStatisticDocRefCache.getDocRefs(event.getKey()).stream()
+                                            .map(docRef ->
+                                                    new Tuple3<>(docRefTypeToServiceMap.get(docRef.getType()),
+                                                            docRef,
+                                                            event))
+                                            .filter(tuple3 -> tuple3._1() != null))
+                            .collect(Collectors.groupingBy(
+                                    Tuple3::_1, //service
+                                    Collectors.groupingBy(
+                                            Tuple3::_2, //docRef
+                                            Collectors.mapping(
+                                                    Tuple3::_3, //event
+                                                    Collectors.toList()))));
 
+            serviceToEventsMapMap.entrySet().forEach(entry -> {
+                InternalStatisticsService service = entry.getKey();
+                service.putEvents(entry.getValue());
+            });
+        } catch (Exception e) {
+            LOGGER.error("Error sending internal stats to all services", e);
+            exceptionHandler.accept(e);
+        }
+
+        /*
         //fire off the list of internal stats events to each service asynchronously
         //ensuring all are completed
         CompletableFuture<Void>[] futures = serviceToEventsMapMap.entrySet().stream()
@@ -64,5 +78,6 @@ class MultiServiceInternalStatisticsFacade implements InternalStatisticsFacade {
             LOGGER.error("Error sending internal stats to all services", e);
             exceptionHandler.accept(e);
         }
+        */
     }
 }
