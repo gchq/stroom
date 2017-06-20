31cb8adf088743df57792a368975e8e03ba16f38
at055612 <at055612@gmail.com>
2017-06-20 15:58:51 +0100

    Add LogConfigurationTask admin task

--- stroom-app/src/main/java/stroom/startup/AdminTasks.java
+++ stroom-app/src/main/java/stroom/startup/AdminTasks.java
@@ -1,4 +1,15 @@
 package stroom.startup;
 
+import io.dropwizard.servlets.tasks.LogConfigurationTask;
+
 public class AdminTasks {
+
+    private AdminTasks() {
+    }
+
+    public static void registerAdminTasks(final io.dropwizard.setup.Environment environment) {
+
+        //Task to allow configuration of log levels at runtime
+        environment.admin().addTask(new LogConfigurationTask());
+    }
 }
