90509fea6bc636c34f4f44434f7d1b19884f173a
at055612 <at055612@gmail.com>
2017-08-16 23:16:41 +0100

    gh-360 Change SqlStatisticsStore to use ResultStoreCreator

--- stroom-statistics-server/src/main/java/stroom/statistics/server/sql/search/SqlStatisticsStore.java
+++ stroom-statistics-server/src/main/java/stroom/statistics/server/sql/search/SqlStatisticsStore.java
@@ -1,18 +1,19 @@
 package stroom.statistics.server.sql.search;
 
 import stroom.mapreduce.v2.UnsafePairQueue;
+import stroom.query.api.v2.TableSettings;
+import stroom.query.v2.CompiledSorter;
 import stroom.query.v2.Coprocessor;
 import stroom.query.v2.CoprocessorSettingsMap;
 import stroom.query.v2.Data;
 import stroom.query.v2.Item;
-import stroom.query.v2.Items;
-import stroom.query.v2.ItemsArrayList;
 import stroom.query.v2.Key;
 import stroom.query.v2.Payload;
+import stroom.query.v2.ResultStoreCreator;
 import stroom.query.v2.Store;
 import stroom.query.v2.TablePayload;
+import stroom.query.v2.TrimSettings;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +22,12 @@ public class SqlStatisticsStore implements Store {
     private CoprocessorSettingsMap coprocessorSettingsMap;
     private Map<CoprocessorSettingsMap.CoprocessorKey, Coprocessor> coprocessorMap;
     private Map<CoprocessorSettingsMap.CoprocessorKey, Payload> payloadMap;
+
+    private final TableSettings tableSettings;
+
+    public SqlStatisticsStore(final TableSettings tableSettings) {
+        this.tableSettings = tableSettings;
+    }
 
     @Override
     public void destroy() {
@@ -45,28 +52,31 @@ public class SqlStatisticsStore implements Store {
 
         //TODO create a resultStoreCreator (expose it as public) then call
 
-
-//        final ResultStoreCreator resultStoreCreator = new ResultStoreCreator(compiledSorter);
-//        resultStoreCreator.read(queue);
+        CompiledSorter compiledSorter = new CompiledSorter(tableSettings.getFields());
+        final ResultStoreCreator resultStoreCreator = new ResultStoreCreator(compiledSorter);
+        resultStoreCreator.read(queue);
 //
 //        // Trim the number of results in the store.
-//        resultStoreCreator.trim(trimSettings);
+        //TODO where does defaultStoreTrimSizes come from?
+        final TrimSettings trimSettings = new TrimSettings(tableSettings.getMaxResults(), null);
+        resultStoreCreator.trim(trimSettings);
 
-        //TODO then may need to do something with compileSorter
 
-        Map<Key, Items<Item>> childMap = new HashMap<>();
-        // We should now have a reduction in the reducedQueue.
-        queue.forEach(pair -> {
-            final Item item = pair.getValue();
+//        Map<Key, Items<Item>> childMap = new HashMap<>();
+//        // We should now have a reduction in the reducedQueue.
+//        queue.forEach(pair -> {
+//            final Item item = pair.getValue();
+//
+//            if (item.getKey() != null) {
+//                childMap.computeIfAbsent(item.getKey().getParent(), k -> new ItemsArrayList<>()).add(item);
+//            } else {
+//                childMap.computeIfAbsent(null, k -> new ItemsArrayList<>()).add(item);
+//            }
+//        });
 
-            if (item.getKey() != null) {
-                childMap.computeIfAbsent(item.getKey().getParent(), k -> new ItemsArrayList<>()).add(item);
-            } else {
-                childMap.computeIfAbsent(null, k -> new ItemsArrayList<>()).add(item);
-            }
-        });
-
-        return new Data(childMap, queue.size(), queue.size());
+//        return new Data(childMap, queue.size(), queue.size());
+        //TODO need to check args for this method
+        return resultStoreCreator.create(queue.size(), queue.size());
     }
 
     @Override
