83d5a98e0057f85a1718e6f3f4d85272a05fe1c5
jc064522 <jc064522@gmail.com>
2017-08-22 14:52:28 +0100

    :sparkles: Enable CORS

--- stroom-app/src/main/java/stroom/startup/App.java
+++ stroom-app/src/main/java/stroom/startup/App.java
@@ -19,9 +19,14 @@ package stroom.startup;
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stroom.Config;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class App extends Application<Config> {
     private final Logger LOGGER = LoggerFactory.getLogger(App.class);
@@ -50,5 +55,15 @@ public class App extends Application<Config> {
         Resources resources = new Resources(environment.jersey(), servletMonitor);
         HealthChecks.registerHealthChecks(environment.healthChecks(), resources, servletMonitor);
         AdminTasks.registerAdminTasks(environment);
+        configureCors(environment);
+    }
+
+    private static final void configureCors(io.dropwizard.setup.Environment environment) {
+        FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, new String[]{"/*"});
+        cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_METHODS_HEADER, "GET,PUT,POST,DELETE,OPTIONS");
+        cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_HEADERS_HEADER, "*");
+        cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_CREDENTIALS_HEADER, "true");
     }
 }
