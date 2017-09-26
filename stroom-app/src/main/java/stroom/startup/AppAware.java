120d62dc184e7cc9a057a916a7d05904309d220c
stroomdev66 <stroomdev66@gmail.com>
2017-09-26 14:13:38 +0100

    Made Stroom modular

--- stroom-app/src/main/java/stroom/startup/AppAware.java
+++
@@ -1,8 +0,0 @@
-package stroom.startup;
-
-import io.dropwizard.Configuration;
-import io.dropwizard.setup.Environment;
-
-public interface AppAware {
-    void initialize(Configuration configuration, Environment environment);
-}
