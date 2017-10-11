b52406363eea7187606c2299abebb862f068c27a
at055612 <at055612@gmail.com>
2017-10-11 11:37:29 +0100

    Change travis build to only tag git on a cron event
    Also:
    * moved some before_script code out into its own shell script
    * commented out the install of docker/compose as travis now comes with

---
+++ travis.before_script.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#exit script on any error
+set -e
+
+#Shell Colour constants for use in 'echo -e'
+#e.g.  echo -e "My message ${GREEN}with just this text in green${NC}"
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Colour 
+
+echo -e "TRAVIS_EVENT_TYPE:   [${GREEN}${TRAVIS_EVENT_TYPE}${NC}]"
+
+if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
+    echo "Cron build so don't set up gradle plugins or docker containers"
+
+else
+    echo -e "JAVA_OPTS: [${GREEN}$JAVA_OPTS${NC}]"
+    # Increase the size of the heap
+    export JAVA_OPTS=-Xmx1024m
+
+    echo "Clone build and publish our urlDependencies plugin"
+    mkdir -p ../git_work
+    pushd ../git_work
+    git clone https://github.com/gchq/urlDependencies-plugin.git
+    pushd urlDependencies-plugin
+    ./gradlew clean build publishToMavenLocal
+    popd
+
+    echo "Clone our stroom-resources repo"
+    git clone https://github.com/gchq/stroom-resources.git
+    pushd stroom-resources/dev-resources/compose
+
+    echo "Start all the services we need to run the integration tests in stroom"
+    docker-compose -f kafka-stroomDb-stroomStatsDb-zk.yml up -d
+    popd
+    popd
+
+    echo "Configure our plugins directory so stroom can find the kafka jar"
+    mkdir -p ~/.stroom/plugins
+    cp stroom.conf.example ~/.stroom/stroom.conf
+    echo "stroom.plugins.lib.dir=$HOME/.stroom/plugins" >> ~/.stroom/stroom.conf
+fi
+
+exit 0
