0d0dd0e96a399219536a2d7421150f3eb1eada61
GCHQ-11 <gchq.developer.11@gmail.com>
2017-10-25 10:48:13 +0100

    removing gratuitous use of builder

--- stroom-core-shared/src/main/java/stroom/explorer/shared/ExplorerConstants.java
+++ stroom-core-shared/src/main/java/stroom/explorer/shared/ExplorerConstants.java
@@ -5,17 +5,9 @@ import stroom.query.api.v2.DocRef;
 public final class ExplorerConstants {
     public static final String SYSTEM = "System";
     public static final String FOLDER = "Folder";
-    public static final DocRef ROOT_DOC_REF = new DocRef.Builder()
-            .type(SYSTEM)
-            .uuid("0")
-            .name(SYSTEM)
-            .build();
+    public static final DocRef ROOT_DOC_REF = new DocRef(SYSTEM, "0",SYSTEM);
     public static final String ANNOTATIONS = "Annotations";
-    public static final DocRef ANNOTATIONS_DOC_REF = new DocRef.Builder()
-            .type(ANNOTATIONS)
-            .uuid("1")
-            .name(ANNOTATIONS)
-            .build();
+    public static final DocRef ANNOTATIONS_DOC_REF = new DocRef(ANNOTATIONS, "1",ANNOTATIONS);
 
     private ExplorerConstants() {
     }
