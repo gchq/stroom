6d8cdb8317f05e58cbaba67bc13efc9b3eaf48a2
GCHQ-11 <gchq.developer.11@gmail.com>
2017-10-31 14:59:40 +0000

    fixing merge

--- stroom-annotations-server/src/main/java/stroom/annotations/StroomAnnotationsExplorerActionHandler.java
+++ stroom-annotations-server/src/main/java/stroom/annotations/StroomAnnotationsExplorerActionHandler.java
@@ -4,16 +4,20 @@ import org.springframework.stereotype.Component;
 import stroom.entity.shared.PermissionException;
 import stroom.explorer.server.ExplorerActionHandler;
 import stroom.query.api.v2.DocRef;
+import stroom.security.SecurityContext;
 
 @Component
 public class StroomAnnotationsExplorerActionHandler implements ExplorerActionHandler {
 
-    public StroomAnnotationsExplorerActionHandler() {
+    private final SecurityContext securityContext;
+
+    public StroomAnnotationsExplorerActionHandler(final SecurityContext securityContext) {
+        this.securityContext = securityContext;
     }
 
     @Override
     public DocRef createDocument(String name, String parentFolderUUID) {
-        throw new PermissionException("You cannot create the Annotations node");
+        throw new PermissionException(securityContext.getUserId(), "You cannot create the Annotations node");
     }
 
     @Override
