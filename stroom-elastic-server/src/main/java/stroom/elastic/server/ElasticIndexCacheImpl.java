b51c12337b86af21834a6dcd4bc04a2273611244
GCHQ-11 <gchq.developer.11@gmail.com>
2017-12-01 10:48:13 +0000

    changing the elastic index cache impl to use the docRefHttpClient to load full index configs

--- stroom-elastic-server/src/main/java/stroom/elastic/server/ElasticIndexCacheImpl.java
+++ stroom-elastic-server/src/main/java/stroom/elastic/server/ElasticIndexCacheImpl.java
@@ -8,20 +8,14 @@ import org.apache.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import stroom.elastic.shared.ElasticIndex;
 import stroom.node.server.StroomPropertyService;
-import stroom.node.shared.ClientProperties;
 import stroom.pipeline.server.errorhandler.LoggedException;
 import stroom.query.api.v2.DocRef;
+import stroom.query.audit.DocRefResourceHttpClient;
 import stroom.util.cache.CacheManager;
 
 import javax.inject.Inject;
-import javax.ws.rs.core.MediaType;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import javax.ws.rs.core.Response;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 import static stroom.entity.server.ExternalDocumentEntityServiceImpl.BASE_URL_PROPERTY;
 
@@ -32,51 +26,28 @@ public class ElasticIndexCacheImpl implements ElasticIndexCache {
     private final LoadingCache<DocRef, ElasticIndexConfig> cache;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final Function<String, String> fetchDocRefUrl;
+    private final DocRefResourceHttpClient docRefHttpClient;
 
     @Inject
     ElasticIndexCacheImpl(final CacheManager cacheManager,
                           final StroomPropertyService propertyService) {
         final String urlPropKey = String.format(BASE_URL_PROPERTY, ElasticIndex.ENTITY_TYPE);
-        this.fetchDocRefUrl = (uuid) ->
-                String.format("%s/%s", propertyService.getProperty(urlPropKey), uuid);
+        docRefHttpClient = new DocRefResourceHttpClient(propertyService.getProperty(urlPropKey));
 
         final CacheLoader<DocRef, ElasticIndexConfig> cacheLoader = CacheLoader.from(k -> {
-            HttpURLConnection connection = null;
-
             try {
-                //Create connection
-                URL url = new URL(fetchDocRefUrl.apply(k.getUuid()));
-                connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("accept", MediaType.APPLICATION_JSON);
+                final Response response = docRefHttpClient.get(k.getUuid());
 
-                //Get Response
-                final InputStream is = connection.getInputStream();
-                final BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-                final StringBuilder response = new StringBuilder();
-                String line;
-                while ((line = rd.readLine()) != null) {
-                    response.append(line);
-                    response.append('\r');
-                }
-                rd.close();
-                final String body = response.toString();
-
-                if (connection.getResponseCode() != HttpStatus.SC_OK) {
+                if (response.getStatus() != HttpStatus.SC_OK) {
                     final String msg = String.format("Invalid status returned by Elastic Explorer Service: %d - %s ",
-                            connection.getResponseCode(),
-                            body);
+                            response.getStatus(),
+                            response.getEntity());
                     throw new RuntimeException(msg);
                 }
 
-                return objectMapper.readValue(body, ElasticIndexConfig.class);
-            } catch (Exception e) {
+                return objectMapper.readValue(response.getEntity().toString(), ElasticIndexConfig.class);
+            } catch (Throwable e) {
                 throw new LoggedException(String.format("Failed to retrieve elastic index config for %s", k.getUuid()), e);
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
             }
         });
 
