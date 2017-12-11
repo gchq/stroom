59cf3ae9305cf0e9a6f6669c1181344e626df97a
jc064522 <jc064522@gmail.com>
2017-12-11 16:40:59 +0000

    Improve naming

--- travis.before_script.sh
+++ travis.before_script.sh
@@ -32,7 +32,7 @@ else
     echo "Clone our stroom-resources repo"
     git clone https://github.com/gchq/stroom-resources.git
     pushd stroom-resources/dev-resources/compose
-    git checkout $RESOURCES_BRANCH
+    git checkout $STROOM_RESOURCES_BRANCH
 
     echo "Start all the services we need to run the integration tests in stroom"
     docker-compose -f kafka-stroomDb-stroomStatsDb-zk.yml up -d
