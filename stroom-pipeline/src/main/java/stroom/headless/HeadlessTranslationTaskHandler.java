e6ddfb5528cdf8b03974284007def18770ded352
stroomdev66 <stroomdev66@gmail.com>
2017-12-12 15:03:02 +0000

    Merge branch '5.0' of github.com:gchq/stroom into 5.1
    # Conflicts:
    #	CHANGELOG.md
    #	pom.xml
    #	stroom-app-client/pom.xml
    #	stroom-app-distribution/pom.xml
    #	stroom-app/pom.xml
    #	stroom-config/pom.xml
    #	stroom-core-client-widget/pom.xml
    #	stroom-core-client/pom.xml
    #	stroom-core-server/pom.xml
    #	stroom-core-shared/pom.xml
    #	stroom-dashboard/pom.xml
    #	stroom-entities/pom.xml
    #	stroom-expression/pom.xml
    #	stroom-index/pom.xml
    #	stroom-integrationtest/pom.xml
    #	stroom-pipeline/pom.xml
    #	stroom-pipeline/src/main/java/stroom/headless/HeadlessTranslationTaskHandler.java
    #	stroom-query-api/pom.xml
    #	stroom-query-common/pom.xml
    #	stroom-remote/pom.xml
    #	stroom-security-api/pom.xml
    #	stroom-security-client-api/pom.xml
    #	stroom-security/pom.xml
    #	stroom-startup/pom.xml
    #	stroom-statistics-api/pom.xml
    #	stroom-statistics-client/pom.xml
    #	stroom-statistics-server/pom.xml
    #	stroom-statistics-shared/pom.xml
    #	stroom-test-common/pom.xml
    #	stroom-util-shared/pom.xml
    #	stroom-util/pom.xml

--- stroom-pipeline/src/main/java/stroom/headless/HeadlessTranslationTaskHandler.java
+++ stroom-pipeline/src/main/java/stroom/headless/HeadlessTranslationTaskHandler.java
@@ -19,6 +19,7 @@ package stroom.headless;
 import org.springframework.context.annotation.Scope;
 import stroom.entity.shared.BaseResultList;
 import stroom.entity.shared.StringCriteria;
+import stroom.feed.MetaMap;
 import stroom.feed.shared.Feed;
 import stroom.feed.shared.FeedService;
 import stroom.feed.shared.FindFeedCriteria;
@@ -58,9 +59,12 @@ import stroom.util.io.IgnoreCloseInputStream;
 import stroom.util.shared.Severity;
 import stroom.util.shared.VoidResult;
 import stroom.util.spring.StroomScope;
-import stroom.util.zip.HeaderMap;
 import stroom.util.zip.StroomHeaderArguments;
-import stroom.feed.MetaMap;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @TaskHandlerBean(task = HeadlessTranslationTask.class)
 @Scope(StroomScope.TASK)
