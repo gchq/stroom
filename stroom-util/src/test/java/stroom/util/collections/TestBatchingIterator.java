30ed68160b6d47eba8390f6b795a964eb797ce31
at055612 <at055612@gmail.com>
2018-01-31 14:42:07 +0000

    gh-570 Add extra test for BatchingIterator and swap to AssertJ

--- stroom-util/src/test/java/stroom/util/collections/TestBatchingIterator.java
+++ stroom-util/src/test/java/stroom/util/collections/TestBatchingIterator.java
@@ -1,13 +1,13 @@
 package stroom.util.collections;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestBatchingIterator {
 
@@ -20,11 +20,25 @@ public class TestBatchingIterator {
         List<List<Integer>> batches = BatchingIterator.batchedStreamOf(sourceStream, 3)
                 .collect(Collectors.toList());
 
-        Assert.assertEquals(4, batches.size());
-        Assert.assertEquals(Arrays.asList(1,2,3), batches.get(0));
-        Assert.assertEquals(Arrays.asList(4,5,6), batches.get(1));
-        Assert.assertEquals(Arrays.asList(7,8,9), batches.get(2));
-        Assert.assertEquals(Arrays.asList(10), batches.get(3));
+        assertThat(batches).hasSize(4);
+        assertThat(batches.get(0)).containsExactly(1,2,3);
+        assertThat(batches.get(1)).containsExactly(4,5,6);
+        assertThat(batches.get(2)).containsExactly(7,8,9);
+        assertThat(batches.get(3)).containsExactly(10);
+    }
+
+    @Test
+    public void batchedStreamOf_exact() throws Exception {
+
+        Stream<Integer> sourceStream = IntStream.rangeClosed(1, 10)
+                .boxed();
+
+        List<List<Integer>> batches = BatchingIterator.batchedStreamOf(sourceStream, 5)
+                .collect(Collectors.toList());
+
+        assertThat(batches).hasSize(2);
+        assertThat(batches.get(0)).containsExactly(1,2,3,4,5);
+        assertThat(batches.get(1)).containsExactly(6,7,8,9,10);
     }
 
     @Test
@@ -35,8 +49,8 @@ public class TestBatchingIterator {
         List<List<Integer>> batches = BatchingIterator.batchedStreamOf(sourceStream, 3)
                 .collect(Collectors.toList());
 
-        Assert.assertNotNull(batches);
-        Assert.assertEquals(0, batches.size());
+        assertThat(batches).isNotNull();
+        assertThat(batches).isEmpty();
     }
 
 }
