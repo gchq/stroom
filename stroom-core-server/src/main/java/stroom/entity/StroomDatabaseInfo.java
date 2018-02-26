36ae5ceaa80039059b89144b19e1b37eb931af28
stroomdev66 <stroomdev66@gmail.com>
2018-02-26 12:54:16 +0000

    Got TestDataFeedServiceImpl working

---
+++ stroom-core-server/src/main/java/stroom/entity/StroomDatabaseInfo.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package stroom.entity;
+
+import org.springframework.beans.factory.annotation.Value;
+import stroom.util.io.StreamUtil;
+
+import javax.inject.Inject;
+
+public class StroomDatabaseInfo {
+    private String driverClassName;
+
+    @Inject
+    StroomDatabaseInfo(@Value("#{propertyConfigurer.getProperty('stroom.jdbcDriverClassName')}") final String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public boolean isMysql() {
+        return driverClassName.toLowerCase(StreamUtil.DEFAULT_LOCALE).contains("mysql");
+    }
+}
