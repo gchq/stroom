91aaa131f5f93dcebcd6bdf2873b104b481e11a0
jc064522 <jc064522@gmail.com>
2018-04-18 15:37:04 +0100

    gh-729 - Add table sorting for Monitoring -> Server Tasks

--- stroom-core-server/src/main/java/stroom/task/FindTaskProgressHandlerBase.java
+++ stroom-core-server/src/main/java/stroom/task/FindTaskProgressHandlerBase.java
@@ -21,6 +21,7 @@ import stroom.entity.shared.Action;
 import stroom.entity.shared.BaseResultList;
 import stroom.entity.shared.PageRequest;
 import stroom.entity.shared.ResultList;
+import stroom.entity.shared.Sort;
 import stroom.node.shared.Node;
 import stroom.task.cluster.ClusterCallEntry;
 import stroom.task.cluster.ClusterDispatchAsyncHelper;
@@ -141,7 +142,7 @@ abstract class FindTaskProgressHandlerBase<T extends Task<R>, R extends SharedOb
 
             final List<TaskProgress> rtnList = new ArrayList<>();
 
-            sortTaskProgressList(sortedRootNodes);
+            sortTaskProgressList(sortedRootNodes, criteria);
             for (final TaskProgress taskProgress : sortedRootNodes) {
                 buildTreeNode(rtnList, criteria, taskProgress, totalChildMap, 0);
             }
@@ -153,8 +154,64 @@ abstract class FindTaskProgressHandlerBase<T extends Task<R>, R extends SharedOb
         }
     }
 
-    private void sortTaskProgressList(final List<TaskProgress> rtnList) {
-        rtnList.sort((lhs, rhs) -> CompareUtil.compareLong(lhs.getSubmitTimeMs(), rhs.getSubmitTimeMs()));
+    private void sortTaskProgressList(final List<TaskProgress> rtnList, FindTaskProgressCriteria criteria) {
+        rtnList.sort((TaskProgress lhs, TaskProgress rhs) -> {
+            criteria.validateSortField();
+
+            // We're only implementing sorting by a single column
+            Sort sort = criteria.getSortList().isEmpty() ? new Sort() : criteria.getSortList().get(0);
+
+            // We're not going to have a default for the direction because why would it be anything other than asc/desc?
+            switch (sort.getField()) {
+                case FindTaskProgressCriteria.FIELD_AGE:
+                    switch (sort.getDirection()) {
+                        case ASCENDING:
+                            return CompareUtil.compareLong(lhs.getAgeMs(), rhs.getAgeMs());
+                        case DESCENDING:
+                            return CompareUtil.compareLong(rhs.getAgeMs(), lhs.getAgeMs());
+                    }
+                case FindTaskProgressCriteria.FIELD_INFO:
+                    switch (sort.getDirection()) {
+                        case ASCENDING:
+                            return CompareUtil.compareString(lhs.getTaskInfo(), rhs.getTaskInfo());
+                        case DESCENDING:
+                            return CompareUtil.compareString(rhs.getTaskInfo(), lhs.getTaskInfo());
+                    }
+                case FindTaskProgressCriteria.FIELD_NAME:
+                    switch (sort.getDirection()) {
+                        case ASCENDING:
+                            return CompareUtil.compareString(lhs.getTaskName(), rhs.getTaskName());
+                        case DESCENDING:
+                            return CompareUtil.compareString(rhs.getTaskName(), lhs.getTaskName());
+                    }
+                case FindTaskProgressCriteria.FIELD_NODE:
+                    switch (sort.getDirection()) {
+                        case ASCENDING:
+                            return CompareUtil.compareString(lhs.getNode().getName(), rhs.getNode().getName());
+                        case DESCENDING:
+                            return CompareUtil.compareString(rhs.getNode().getName(), lhs.getNode().getName());
+                    }
+                case FindTaskProgressCriteria.FIELD_SUBMIT_TIME:
+                    switch (sort.getDirection()) {
+                        case ASCENDING:
+                            return CompareUtil.compareLong(lhs.getSubmitTimeMs(), rhs.getSubmitTimeMs());
+                        case DESCENDING:
+                            return CompareUtil.compareLong(rhs.getSubmitTimeMs(), lhs.getSubmitTimeMs());
+                    }
+                case FindTaskProgressCriteria.FIELD_USER:
+                    switch (sort.getDirection()) {
+                        case ASCENDING:
+                            return CompareUtil.compareString(lhs.getUserName(), rhs.getUserName());
+                        case DESCENDING:
+                            return CompareUtil.compareString(rhs.getUserName(), lhs.getUserName());
+                    }
+                default:
+                    // If the field is valid, and it should be because we've validated it, and the sort direction is valid,
+                    // and it should be because it's an enum and the only sensible sort directions are asc and desc,
+                    // then we'll probably never get here.
+                    return CompareUtil.compareLong(lhs.getSubmitTimeMs(), rhs.getSubmitTimeMs());
+            }
+        });
     }
 
     private void buildTreeNode(final List<TaskProgress> rtnList, final FindTaskProgressCriteria criteria,
@@ -162,7 +219,7 @@ abstract class FindTaskProgressHandlerBase<T extends Task<R>, R extends SharedOb
         rtnList.add(node);
         final List<TaskProgress> childList = totalChildMap.get(node.getId());
         if (childList != null) {
-            sortTaskProgressList(childList);
+            sortTaskProgressList(childList, criteria);
             // Force expansion on tasks that are younger than a second.
             final boolean forceExpansion = node.getAgeMs() < 1000;
             final boolean expanded = criteria.isExpanded(node);
