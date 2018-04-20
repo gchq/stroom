43f7d1853dfab42cdc943e62ceb1c0cd472e9e7e
at055612 <at055612@gmail.com>
2018-04-20 14:32:19 +0100

    Remove popd cmd in travis.before_script

--- travis.before_script.sh
+++ travis.before_script.sh
@@ -33,7 +33,6 @@ else
     echo "Start all the services we need to run the integration tests in stroom"
     ./bounceIt.sh 'up -d --build' -e -y -x kafka stroom-db stroom-stats-db zookeeper
     popd
-    popd
 
     echo "Configure our plugins directory so stroom can find the kafka jar"
     mkdir -p ~/.stroom/plugins
