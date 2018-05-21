0ee183912c5c1c4b78168fb8e87331839ff834b8
jc064522 <jc064522@gmail.com>
2018-05-21 09:23:58 +0100

    gh-728 - Make references go via index.js
    Useful because it makes coupling explicit.

--- stroom-ui/src/components/ExpressionBuilder/storybook/expressionBuilder.stories.js
+++ stroom-ui/src/components/ExpressionBuilder/storybook/expressionBuilder.stories.js
@@ -20,71 +20,52 @@ import { storiesOf, addDecorator } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withNotes } from '@storybook/addon-notes';
 
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 import { ReduxDecorator } from 'lib/storybook/ReduxDecorator';
 
-import { 
-    testMultiInitialisationDecorator,
-    testInitialisationDecorator
+import {
+  testMultiInitialisationDecorator,
+  testInitialisationDecorator,
 } from 'lib/storybook/testDataDecorator';
 
-import {
-    DragDropDecorator
-} from 'lib/storybook/DragDropDecorator';
+import { DragDropDecorator } from 'lib/storybook/DragDropDecorator';
+
+import { ExpressionTerm, ExpressionOperator, ExpressionBuilder } from '../index';
+
+import { expressionChanged } from '../redux';
+
+import { receiveDataSource } from 'components/DataSource';
+
+import { receiveDocTree, testTree } from 'components/DocExplorer';
 
 import {
-    ExpressionTerm,
-    ExpressionOperator,
-    ExpressionBuilder
-} from '../index';
-
-import {
-    expressionChanged
-} from '../redux';
-
-import {
-    receiveDataSource
-} from 'components/DataSource/redux';
-
-import {
-    receiveDocTree,
-} from 'components/DocExplorer/redux';
-
-import { testTree } from 'components/DocExplorer/storybook/testTree';
-
-import {
-    testExpression,
-    simplestExpression,
-    testDataSource,
-    emptyDataSource,
-    testAndOperator,
-    testOrOperator,
-    testNotOperator
-} from './testExpression'
-import markdown from './expressionBuilder.md'
-
+  testExpression,
+  simplestExpression,
+  testDataSource,
+  emptyDataSource,
+  testAndOperator,
+  testOrOperator,
+  testNotOperator,
+} from './testExpression';
+import markdown from './expressionBuilder.md';
 
 storiesOf('Expression Builder', module)
-    .addDecorator(testInitialisationDecorator(receiveDocTree, testTree))
-    .addDecorator(testMultiInitialisationDecorator(receiveDataSource, {
-        'testDs' : testDataSource
-    }))
-    .addDecorator(testMultiInitialisationDecorator(expressionChanged, {
-        'populatedEx' : testExpression,
-        'simplestEx' : simplestExpression
-    }))
-    .addDecorator(ReduxDecorator) // must be recorder after/outside of the test initialisation decorators
-    .addDecorator(DragDropDecorator)
-    .add('Populated', () =>
-        <ExpressionBuilder dataSourceUuid='testDs' expressionId='populatedEx'/>
-    )
-    .add('Simplest', () => 
-        <ExpressionBuilder dataSourceUuid='testDs' expressionId='simplestEx'/>
-    )
-    .add('Missing Data Source', () => 
-        <ExpressionBuilder dataSourceUuid='missingDs' expressionId='simplestEx'/>
-    )
-    .add('Missing Expression', () => 
-        <ExpressionBuilder dataSourceUuid='testDs' expressionId='missingEx'/>
-    )
+  .addDecorator(testInitialisationDecorator(receiveDocTree, testTree))
+  .addDecorator(testMultiInitialisationDecorator(receiveDataSource, {
+    testDs: testDataSource,
+  }))
+  .addDecorator(testMultiInitialisationDecorator(expressionChanged, {
+    populatedEx: testExpression,
+    simplestEx: simplestExpression,
+  }))
+  .addDecorator(ReduxDecorator) // must be recorder after/outside of the test initialisation decorators
+  .addDecorator(DragDropDecorator)
+  .add('Populated', () => <ExpressionBuilder dataSourceUuid="testDs" expressionId="populatedEx" />)
+  .add('Simplest', () => <ExpressionBuilder dataSourceUuid="testDs" expressionId="simplestEx" />)
+  .add('Missing Data Source', () => (
+    <ExpressionBuilder dataSourceUuid="missingDs" expressionId="simplestEx" />
+  ))
+  .add('Missing Expression', () => (
+    <ExpressionBuilder dataSourceUuid="testDs" expressionId="missingEx" />
+  ));
