ab132910c716695de0e5e8153ea19f67d7a7cf58
jc064522 <jc064522@gmail.com>
2018-05-22 14:46:49 +0100

    gh-728 - Added story for a tracker with a long name
    Also improved notes and randomising of test data

--- stroom-ui/src/sections/TrackerDashboard/trackerTestData.test.js
+++ stroom-ui/src/sections/TrackerDashboard/trackerTestData.test.js
@@ -1,11 +1,13 @@
 import loremIpsum from 'lorem-ipsum';
 
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
 const minimalTracker_undefinedLastPollAge = {
   filterId: 1,
   enabled: true,
   lastPollAge: undefined,
-  pipelineName: 'Long, long, long-long-long name',
-  priority: 89,
+  pipelineName: loremIpsum({ count: 3, units: 'words' }),
+  priority: getRandomInt(1, 99),
   filterXml: '<xml/>',
   createUser: 'tester',
   createdOn: 87134234234,
@@ -15,8 +17,8 @@ const minimalTracker_nullLastPollAge = {
   filterId: 2,
   enabled: true,
   lastPollAge: null,
-  pipelineName: 'Long, long, long-long-long name',
-  priority: 89,
+  pipelineName: loremIpsum({ count: 3, units: 'words' }),
+  priority: getRandomInt(1, 99),
   filterXml: '<xml/>',
   createUser: 'tester',
   createdOn: 87134234234,
@@ -26,8 +28,8 @@ const minimalTracker_emptyLastPollAge = {
   filterId: 3,
   enabled: true,
   lastPollAge: '',
-  pipelineName: 'Long, long, long-long-long name',
-  priority: 89,
+  pipelineName: loremIpsum({ count: 3, units: 'words' }),
+  priority: getRandomInt(1, 99),
   filterXml: '<xml/>',
   createUser: 'tester',
   createdOn: 87134234234,
@@ -36,9 +38,9 @@ const minimalTracker_emptyLastPollAge = {
 const maximalTracker = {
   filterId: 4,
   enabled: true,
-  pipelineName: 'Pipeline name',
-  trackerPercent: 79,
-  priority: 89,
+  pipelineName: loremIpsum({ count: 3, units: 'words' }),
+  trackerPercent: getRandomInt(0, 100),
+  priority: getRandomInt(1, 99),
   filterXml: '<xml/>',
   lastPollAge: '1.5d',
   taskCount: 4,
@@ -52,7 +54,24 @@ const maximalTracker = {
   updatedOn: 87934234234,
 };
 
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const maximalTracker_withLongName = {
+  filterId: 5,
+  enabled: true,
+  pipelineName: loremIpsum({ count: 10, units: 'words' }),
+  trackerPercent: getRandomInt(0, 100),
+  priority: getRandomInt(1, 99),
+  filterXml: '<xml/>',
+  lastPollAge: '1.5d',
+  taskCount: 4,
+  trackerMs: 87834234234,
+  status: 'Active',
+  streamCount: 5,
+  eventCount: 6,
+  createUser: 'tester',
+  createdOn: 87134234234,
+  updateUser: 'tester2',
+  updatedOn: 87934234234,
+};
 
 export const generateGenericTracker = filterId => ({
   filterId,
@@ -78,4 +97,5 @@ export const trackers = {
   minimalTracker_nullLastPollAge,
   minimalTracker_emptyLastPollAge,
   maximalTracker,
+  maximalTracker_withLongName,
 };
