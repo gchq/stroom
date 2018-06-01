62f179fb33d4c381ae760c7452600e85dac7dcd6
jc064522 <jc064522@gmail.com>
2018-06-01 16:50:41 +0100

    gh-278 - Remove trackers from index.js

--- stroom-ui/src/testData/index.js
+++ stroom-ui/src/testData/index.js
@@ -1,57 +1,32 @@
-import {
-    testDataSource,
-    emptyDataSource
-} from './dataSource';
+import { testDataSource, emptyDataSource } from './dataSource';
+
+import { DOC_REF_TYPES, testTree } from './documentTree';
 
 import {
-    DOC_REF_TYPES,
-    testTree
-} from './documentTree';
-
-import {
-    testExpression,
-    simplestExpression,
-    testAndOperator,
-    testOrOperator,
-    testNotOperator
+  testExpression,
+  simplestExpression,
+  testAndOperator,
+  testOrOperator,
+  testNotOperator,
 } from './queryExpression';
 
-import {
-    trackers,
-    generateGenericTracker
-} from './tracker';
+import { testElementTypes, testElementProperties, elements } from './testElements';
 
-import {
-	testElementTypes,
-    testElementProperties,
-    elements
-} from './testElements';
-
-import {
-	testPipeline,
-	testPipelineElements
-} from './testPipelines';
+import { testPipeline, testPipelineElements } from './testPipelines';
 
 export {
-    testDataSource,
-    emptyDataSource,
-
-    DOC_REF_TYPES,
-    testTree,
-
-    testExpression,
-    simplestExpression,
-    testAndOperator,
-    testOrOperator,
-    testNotOperator,
-
-	testElementTypes,
-    testElementProperties,
-	elements,
-    
-	testPipeline,
-	testPipelineElements,
-
-    trackers,
-    generateGenericTracker
-}
+  testDataSource,
+  emptyDataSource,
+  DOC_REF_TYPES,
+  testTree,
+  testExpression,
+  simplestExpression,
+  testAndOperator,
+  testOrOperator,
+  testNotOperator,
+  testElementTypes,
+  testElementProperties,
+  elements,
+  testPipeline,
+  testPipelineElements,
+};
