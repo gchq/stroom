4038d4ee3ded782b109800033048c6ee296fef7e
jc064522 <jc064522@gmail.com>
2018-06-13 06:42:58 +0100

    gh-787 - Tidy up

--- stroom-ui/src/prototypes/PipelineEditor/ElementPalette/ElementCategory.js
+++ stroom-ui/src/prototypes/PipelineEditor/ElementPalette/ElementCategory.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { compose, withState } from 'recompose';
 
-import { Header, Icon, Accordion } from 'semantic-ui-react';
+import { Icon, Accordion } from 'semantic-ui-react';
 
 import NewElement from './NewElement';
 import { ElementCategories } from '../ElementCategories';
@@ -16,13 +16,6 @@ const ElementCategory = ({
     <Accordion styled>
       <Accordion.Title active={isOpen} onClick={() => setIsOpen(!isOpen)}>
         <Icon name="dropdown" /> {ElementCategories[category].displayName}
-        {/* <Header
-      icon={`caret ${isOpen ? 'down' : 'right'}`}
-      as="h3"
-      onClick={() => setIsOpen(!isOpen)}
-      className={isOpen ? 'open' : 'closed'}
-      content={ElementCategories[category].displayName}
-    /> */}
       </Accordion.Title>
       <Accordion.Content active={isOpen}>
         <div className={`element-palette-category__elements--${isOpen ? 'open' : 'closed'}`}>
