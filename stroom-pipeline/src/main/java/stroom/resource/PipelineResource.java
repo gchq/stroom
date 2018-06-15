c342603659201bd3e245e05080540aa3a7c59893
GCHQ-11 <gchq.developer.11@gmail.com>
2018-06-15 07:12:54 +0100

    changin the nature of the pipeline data being returned by the resource

--- stroom-pipeline/src/main/java/stroom/resource/PipelineResource.java
+++ stroom-pipeline/src/main/java/stroom/resource/PipelineResource.java
@@ -5,16 +5,25 @@ import io.swagger.annotations.Api;
 import org.eclipse.jetty.http.HttpStatus;
 import stroom.docref.DocRef;
 import stroom.docstore.db.DocumentNotFoundException;
+import stroom.guice.PipelineScopeRunnable;
 import stroom.pipeline.PipelineStore;
+import stroom.pipeline.factory.PipelineDataValidator;
+import stroom.pipeline.factory.PipelineStackLoader;
+import stroom.pipeline.shared.PipelineDataMerger;
 import stroom.pipeline.shared.PipelineDoc;
 import stroom.pipeline.shared.data.PipelineData;
+import stroom.pipeline.shared.data.PipelineElementType;
+import stroom.pipeline.shared.data.SourcePipeline;
+import stroom.security.Security;
 import stroom.util.HasHealthCheck;
+import stroom.util.shared.SharedList;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Api(
         value = "pipeline - /v1",
@@ -24,30 +33,56 @@ import java.util.Optional;
 public class PipelineResource implements HasHealthCheck  {
 
     private final PipelineStore pipelineStore;
+    private final PipelineStackLoader pipelineStackLoader;
+    private final PipelineDataValidator pipelineDataValidator;
+    private final Security security;
+    private final PipelineScopeRunnable pipelineScopeRunnable;
 
     @Inject
-    public PipelineResource(final PipelineStore pipelineStore) {
+    public PipelineResource(final PipelineStore pipelineStore,
+                            final PipelineStackLoader pipelineStackLoader,
+                            final PipelineDataValidator pipelineDataValidator,
+                            final Security security,
+                            final PipelineScopeRunnable pipelineScopeRunnable) {
         this.pipelineStore = pipelineStore;
+        this.pipelineStackLoader = pipelineStackLoader;
+        this.pipelineDataValidator = pipelineDataValidator;
+        this.security = security;
+        this.pipelineScopeRunnable = pipelineScopeRunnable;
     }
 
     @GET
     @Path("/{pipelineId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response fetch(@PathParam("pipelineId") final String pipelineId) {
-        try {
-            final PipelineDoc doc = pipelineStore.readDocument(new DocRef.Builder()
+        return security.secureResult(() -> {
+            final PipelineDoc pipelineDoc = pipelineStore.readDocument(new DocRef.Builder()
                     .uuid(pipelineId)
                     .type(PipelineDoc.DOCUMENT_TYPE)
                     .build());
+            final SharedList<PipelineData> result = new SharedList<>();
 
-            final PipelineData pipelineData = Optional.of(doc)
-                    .map(PipelineDoc::getPipelineData)
-                    .orElseThrow(() -> new NotFoundException("Pipeline Data was Missing"));
+            pipelineScopeRunnable.scopeRunnable(() -> {
+                // A user should be allowed to read pipelines that they are inheriting from as long as they have 'use' permission on them.
+                security.useAsRead(() -> {
+                    final List<PipelineDoc> pipelines = pipelineStackLoader.loadPipelineStack(pipelineDoc);
 
-            return Response.ok(pipelineData).build();
-        } catch (NotFoundException | DocumentNotFoundException e) {
-            return Response.status(HttpStatus.NOT_FOUND_404).build();
-        }
+                    final Map<String, PipelineElementType> elementMap = PipelineDataMerger.createElementMap();
+                    for (final PipelineDoc pipe : pipelines) {
+                        final PipelineData pipelineData = pipe.getPipelineData();
+
+                        // Validate the pipeline data and add element and property type
+                        // information.
+                        final SourcePipeline source = new SourcePipeline(pipe);
+                        pipelineDataValidator.validate(source, pipelineData, elementMap);
+                        result.add(pipelineData);
+                    }
+
+                });
+            });
+
+            return Response.ok(result).build();
+        });
     }
 
     @Override
