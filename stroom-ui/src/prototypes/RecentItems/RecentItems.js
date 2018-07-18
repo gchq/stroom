98cef3df285d5c624663dec0a63d0ad08e52a64c
jc064522 <jc064522@gmail.com>
2018-07-18 13:41:08 +0100

    Add shortcut keys for recent items

--- stroom-ui/src/prototypes/RecentItems/RecentItems.js
+++ stroom-ui/src/prototypes/RecentItems/RecentItems.js
@@ -14,16 +14,22 @@
  * limitations under the License.
  */
 import React from 'react';
-import { compose } from 'recompose';
+import { compose, lifecycle } from 'recompose';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import { Button, Header, Icon, Modal, Menu } from 'semantic-ui-react';
 
+import Mousetrap from 'mousetrap';
+
 import { actionCreators as recentItemsActionCreators } from './redux';
 import openDocRef from './openDocRef';
 
-const { recentItemsClosed } = recentItemsActionCreators;
+const {
+  recentItemsClosed,
+  recentItemsSelectionUp,
+  recentItemsSelectionDown,
+} = recentItemsActionCreators;
 
 const enhance = compose(
   withRouter,
@@ -31,41 +37,73 @@ const enhance = compose(
     (state, props) => ({
       isOpen: state.recentItems.isOpen,
       openItemStack: state.recentItems.openItemStack,
+      selectedItem: state.recentItems.selectedItem,
+      selectedDocRef: state.recentItems.selectedDocRef,
     }),
-    { recentItemsClosed, openDocRef },
+    {
+      recentItemsClosed,
+      openDocRef,
+      recentItemsSelectionUp,
+      recentItemsSelectionDown,
+    },
   ),
+  lifecycle({
+    componentDidMount() {
+      const { recentItemsSelectionUp, recentItemsSelectionDown } = this.props;
+      Mousetrap.bind(['k', 'up'], () => recentItemsSelectionUp());
+      Mousetrap.bind(['j', 'down'], () => recentItemsSelectionDown());
+    },
+    componentWillUnmount() {
+      Mousetrap.unbind(['k', 'up']);
+      Mousetrap.unbind(['j', 'down']);
+      Mousetrap.unbind('enter');
+    },
+  }),
 );
 
 const RecentItems = ({
-  history, isOpen, recentItemsClosed, openItemStack, openDocRef,
-}) => (
-  <Modal open={isOpen} onClose={recentItemsClosed} size="small" dimmer="inverted">
-    <Header icon="file outline" content="Recent Items" />
-    <Modal.Content>
-      <Menu vertical fluid>
-        {openItemStack.map((docRef) => {
-          const title = docRef.name;
-          return (
-            <Menu.Item
-              key={docRef.uuid}
-              name={title}
-              onClick={() => {
-                openDocRef(history, docRef);
-                recentItemsClosed();
-              }}
-            >
-              {title}
-            </Menu.Item>
-          );
-        })}
-      </Menu>
-    </Modal.Content>
-    <Modal.Actions>
-      <Button negative onClick={recentItemsClosed} inverted>
-        <Icon name="checkmark" /> Close
-      </Button>{' '}
-    </Modal.Actions>
-  </Modal>
-);
+  history,
+  isOpen,
+  recentItemsClosed,
+  openItemStack,
+  openDocRef,
+  selectedItem,
+  selectedDocRef,
+}) => {
+  Mousetrap.bind('enter', () => {
+    openDocRef(history, selectedDocRef);
+    recentItemsClosed();
+  });
+  return (
+    <Modal open={isOpen} onClose={recentItemsClosed} size="small" dimmer="inverted">
+      <Header icon="file outline" content="Recent Items" />
+      <Modal.Content>
+        <Menu vertical fluid>
+          {openItemStack.map((docRef, i) => {
+            const title = docRef.name;
+            return (
+              <Menu.Item
+                active={selectedItem === i}
+                key={docRef.uuid}
+                name={title}
+                onClick={() => {
+                  openDocRef(history, docRef);
+                  recentItemsClosed();
+                }}
+              >
+                {title}
+              </Menu.Item>
+            );
+          })}
+        </Menu>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button negative onClick={recentItemsClosed} inverted>
+          <Icon name="checkmark" /> Close
+        </Button>{' '}
+      </Modal.Actions>
+    </Modal>
+  );
+};
 
 export default enhance(RecentItems);
