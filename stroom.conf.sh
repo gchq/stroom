5d5e3cf1ad090b8d63686b1b0c3bc0361eaa2835
GCHQ-11 <gchq.developer.11@gmail.com>
2018-08-22 12:41:54 +0100

    adding override to use STROOM_RESOURCES_ADVERTISED_HOST for IP address in the stroom.conf.sh script

--- stroom.conf.sh
+++ stroom.conf.sh
@@ -12,14 +12,20 @@ YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
-if [ "$(uname)" == "Darwin" ]; then
-    # Code required to find IP address is different in MacOS
-    ip=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
+if [ "x${STROOM_RESOURCES_ADVERTISED_HOST}" != "x" ]; then
+    ip="${STROOM_RESOURCES_ADVERTISED_HOST}"
+    echo
+    echo -e "Using IP ${GREEN}${ip}${NC} as the advertised host, as obtained from ${BLUE}STROOM_RESOURCES_ADVERTISED_HOST${NC}"
 else
-    ip=$(ip route get 1 |awk 'match($0,"src [0-9\\.]+") {print substr($0,RSTART+4,RLENGTH-4)}')
+    if [ "$(uname)" == "Darwin" ]; then
+        # Code required to find IP address is different in MacOS
+        ip=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
+    else
+        ip=$(ip route get 1 |awk 'match($0,"src [0-9\\.]+") {print substr($0,RSTART+4,RLENGTH-4)}')
+    fi
+    echo
+    echo -e "Using IP ${GREEN}${ip}${NC} as the IP, as determined from the operating system"
 fi
-echo
-echo -e "Using IP ${GREEN}${ip}${NC} as the IP, as determined from the operating system"
 
 stroomDir=~/.stroom
 confFile=${stroomDir}/stroom.conf
