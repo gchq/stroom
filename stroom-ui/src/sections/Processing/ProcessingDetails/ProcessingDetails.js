f4674773988cc359686ead1baa1720c6d0bbed56
jc064522 <jc064522@gmail.com>
2018-08-29 07:52:29 +0100

    Modernising

--- stroom-ui/src/sections/Processing/ProcessingDetails/ProcessingDetails.js
+++ stroom-ui/src/sections/Processing/ProcessingDetails/ProcessingDetails.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { compose, branch, renderComponent, withProps, withHandlers } from 'recompose';
+import {
+  compose,
+  branch,
+  renderComponent,
+  withProps,
+  withHandlers,
+  withPropsOnChange,
+} from 'recompose';
 import { connect } from 'react-redux';
 
 import moment from 'moment';
@@ -17,7 +24,7 @@ const { updateTrackerSelection } = actionCreators;
 
 const enhance = compose(
   connect(
-    ({ processing: { trackers } }) => ({ trackers }),
+    ({ processing: { trackers, selectedTrackerId } }) => ({ trackers, selectedTrackerId }),
     { enableToggle, updateTrackerSelection },
   ),
   withHandlers({
@@ -31,94 +38,101 @@ const enhance = compose(
   withProps(({ trackers, selectedTrackerId }) => ({
     selectedTracker: trackers.find(tracker => tracker.filterId === selectedTrackerId),
   })),
-  branch(({ selectedTracker }) => !selectedTracker, renderComponent(() => <div />)),
+  withProps(({ selectedTracker }) => ({
+    title: selectedTracker !== undefined ? selectedTracker.pipelineName : '',
+    // It'd be more convenient to just check for truthy, but I'm not sure if '0' is a valid lastPollAge
+    lastPollAgeIsDefined:
+      selectedTracker !== undefined &&
+      (selectedTracker.lastPollAge === null ||
+        selectedTracker.lastPollAge === undefined ||
+        selectedTracker.lastPollAge === ''),
+  })),
+  branch(
+    ({ selectedTracker }) => !selectedTracker,
+    renderComponent(() => <div>No tracker selected</div>),
+  ),
 );
 
-const ProcessingDetails = ({ selectedTracker, onHandleEnableToggle, onHandleTrackerSelection }) => {
-  const title = selectedTracker.pipelineName;
+const ProcessingDetails = ({
+  title,
+  selectedTracker,
+  lastPollAgeIsDefined,
+  onHandleEnableToggle,
+  onHandleTrackerSelection,
+}) => (
+  <HorizontalPanel
+    className="element-details__panel"
+    title={title}
+    content={
+      <Grid centered divided columns={3} className="details-grid">
+        <Grid.Column textAlign="left" width={10}>
+          <ExpressionBuilder expressionId="trackerDetailsExpression" />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <Card.Meta>This tracker:</Card.Meta>
 
-  const headerMenuItems = (
-    <Checkbox
-      toggle
-      checked={selectedTracker.enabled}
-      onMouseDown={() => onHandleEnableToggle(selectedTracker.filterId, selectedTracker.enabled)}
-    />
-  );
-
-  const content = (
-    <Grid centered divided columns={3} className="details-grid">
-      <Grid.Column textAlign="left" width={10}>
-        <ExpressionBuilder expressionId="trackerDetailsExpression" />
-      </Grid.Column>
-      <Grid.Column width={6}>
-        <Card.Meta>This tracker:</Card.Meta>
-
-        <List bulleted>
-          {/* It'd be more convenient to just check for truthy, but I'm not sure if '0' is a valid lastPollAge */}
-          {selectedTracker.lastPollAge === null ||
-          selectedTracker.lastPollAge === undefined ||
-          selectedTracker.lastPollAge === '' ? (
-            <List.Item>has not yet done any work</List.Item>
-          ) : (
-            <React.Fragment>
-              <List.Item>
-                has a <strong>last poll age</strong> of {selectedTracker.lastPollAge}
-              </List.Item>
-              <List.Item>
-                has a <strong>task count</strong> of {selectedTracker.taskCount}
-              </List.Item>
-              <List.Item>
-                was <strong>last active</strong>{' '}
-                {moment(selectedTracker.trackerMs)
-                  .calendar()
-                  .toLowerCase()}
-              </List.Item>
-              <List.Item>
-                {selectedTracker.status ? 'has a' : 'does not have a'} <strong>status</strong>
-                {selectedTracker.status ? ` of ${selectedTracker.status}` : undefined}
-              </List.Item>
-              <List.Item>
-                {selectedTracker.streamCount ? 'has a' : 'does not have a'}{' '}
-                <strong>stream count</strong>
-                {selectedTracker.streamCount ? ` of ${selectedTracker.streamCount}` : undefined}
-              </List.Item>
-              <List.Item>
-                {selectedTracker.eventCount ? 'has an' : 'does not have an'}{' '}
-                <strong>event count</strong>
-                {selectedTracker.eventCount ? ` of ${selectedTracker.eventCount}` : undefined}
-              </List.Item>
-            </React.Fragment>
-          )}
-          <List.Item>
-            was <strong>created</strong> by '{selectedTracker.createUser}'{' '}
-            {moment(selectedTracker.createdOn)
-              .calendar()
-              .toLowerCase()}
-          </List.Item>
-          <List.Item>
-            was <strong>updated</strong> by '{selectedTracker.updateUser}'{' '}
-            {moment(selectedTracker.updatedOn)
-              .calendar()
-              .toLowerCase()}
-          </List.Item>
-        </List>
-      </Grid.Column>
-    </Grid>
-  );
-
-  return (
-    <HorizontalPanel
-      className="element-details__panel"
-      title={title}
-      content={content}
-      onClose={() => onHandleTrackerSelection(null)}
-      titleColumns={6}
-      menuColumns={10}
-      headerMenuItems={headerMenuItems}
-      headerSize="h3"
-    />
-  );
-};
+          <List bulleted>
+            {lastPollAgeIsDefined ? (
+              <React.Fragment>
+                <List.Item>
+                  has a <strong>last poll age</strong> of {selectedTracker.lastPollAge}
+                </List.Item>
+                <List.Item>
+                  has a <strong>task count</strong> of {selectedTracker.taskCount}
+                </List.Item>
+                <List.Item>
+                  was <strong>last active</strong>
+                  {moment(selectedTracker.trackerMs)
+                    .calendar()
+                    .toLowerCase()}
+                </List.Item>
+                <List.Item>
+                  {selectedTracker.status ? 'has a' : 'does not have a '} <strong>status</strong>
+                  {selectedTracker.status ? ` of ${selectedTracker.status}` : undefined}
+                </List.Item>
+                <List.Item>
+                  {selectedTracker.streamCount ? 'has a' : 'does not have a '}
+                  <strong>stream count</strong>
+                  {selectedTracker.streamCount ? ` of ${selectedTracker.streamCount}` : undefined}
+                </List.Item>
+                <List.Item>
+                  {selectedTracker.eventCount ? 'has an' : 'does not have an '}
+                  <strong>event count</strong>
+                  {selectedTracker.eventCount ? ` of ${selectedTracker.eventCount}` : undefined}
+                </List.Item>
+              </React.Fragment>
+            ) : (
+              <List.Item>has not yet done any work</List.Item>
+            )}
+            <List.Item>
+              was <strong>created</strong> by '{selectedTracker.createUser} '
+              {moment(selectedTracker.createdOn)
+                .calendar()
+                .toLowerCase()}
+            </List.Item>
+            <List.Item>
+              was <strong>updated</strong> by '{selectedTracker.updateUser} '
+              {moment(selectedTracker.updatedOn)
+                .calendar()
+                .toLowerCase()}
+            </List.Item>
+          </List>
+        </Grid.Column>
+      </Grid>
+    }
+    onClose={() => onHandleTrackerSelection(null)}
+    titleColumns={6}
+    menuColumns={10}
+    headerMenuItems={
+      <Checkbox
+        toggle
+        checked={selectedTracker.enabled}
+        onMouseDown={() => onHandleEnableToggle(selectedTracker.filterId, selectedTracker.enabled)}
+      />
+    }
+    headerSize="h3"
+  />
+);
 
 ProcessingDetails.propTypes = {
   selectedTracker: PropTypes.object.isRequired,
