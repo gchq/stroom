c4bdd2cd9decf1c164819c8c7b2e6c92d56888b6
GCHQ-11 <gchq.developer.11@gmail.com>
2018-09-14 15:14:26 +0100

    should use simple select box for permission inheritance picker instead of searchable dropdown

--- stroom-ui/src/components/PermissionInheritancePicker/PermissionInheritancePicker.js
+++ stroom-ui/src/components/PermissionInheritancePicker/PermissionInheritancePicker.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import DropdownSelect from 'components/DropdownSelect';
+import SelectBox from 'components/SelectBox';
 import permissionInheritanceValues from './permissionInheritanceValues';
 
 const piOptions = Object.values(permissionInheritanceValues).map(pi => ({
-  key: pi,
   value: pi,
   text: pi,
 }));
 
 const PermissionInheritancePicker = props => (
-  <DropdownSelect {...props} placeholder="Permission Inheritance" options={piOptions} />
+  <SelectBox {...props} placeholder="Permission Inheritance" options={piOptions} />
 );
 
 PermissionInheritancePicker.propTypes = {
