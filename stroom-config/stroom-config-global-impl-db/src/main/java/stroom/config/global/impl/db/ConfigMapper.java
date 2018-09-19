d97097fbc0890d41c33a83d6e47b562026519236
at055612 <at055612@gmail.com>
2018-09-19 12:39:34 +0100

    Add support for List and Map property types

--- stroom-config/stroom-config-global-impl-db/src/main/java/stroom/config/global/impl/db/ConfigMapper.java
+++ stroom-config/stroom-config-global-impl-db/src/main/java/stroom/config/global/impl/db/ConfigMapper.java
@@ -20,26 +20,37 @@ package stroom.config.global.impl.db;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.google.common.base.Splitter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import stroom.config.app.AppConfig;
-import stroom.config.global.impl.db.BeanUtil.Prop;
 import stroom.config.global.api.ConfigProperty;
+import stroom.config.global.impl.db.BeanUtil.Prop;
+import stroom.util.logging.LambdaLogger;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Singleton
 public class ConfigMapper {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigMapper.class);
+
+    private static final List<String> DELIMITERS = List.of(
+            "|", ":", ";", ",", "!", "/", "\\", "#", "@", "~", "-", "_", "=", "+", "?");
 
     private final List<ConfigProperty> globalProperties = new ArrayList<>();
     private final Map<String, Prop> propertyMap = new HashMap<>();
@@ -79,7 +90,9 @@ public class ConfigMapper {
                             type.equals(Double.class) ||
                             type.equals(Boolean.class) ||
                             type.equals(boolean.class) ||
-                            type.equals(Character.class)) {
+                            type.equals(Character.class) ||
+                            value instanceof List ||
+                            value instanceof Map) {
                         propertyMap.put(fullPath, v);
 
                         // Create global property.
@@ -124,11 +137,25 @@ public class ConfigMapper {
         return null;
     }
 
+    public static String convert(final Object value) {
+        if (value != null) {
+            if (value instanceof List) {
+                return listToString((List<?>) value);
+            } else if (value instanceof Map) {
+                return mapToString((Map<?, ?>) value);
+            } else {
+                return value.toString();
+            }
+        } else {
+            return null;
+        }
+    }
+
     private String getDefaultValue(final Object object, final Method method) {
         try {
-            final Object o = method.invoke(object, (Object[]) new Class[0]);
-            if (o != null) {
-                return o.toString();
+            final Object value = method.invoke(object, (Object[]) new Class[0]);
+            if (value != null) {
+                return convert(value);
             }
         } catch (final IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
             LOGGER.debug(e.getMessage(), e);
@@ -136,43 +163,213 @@ public class ConfigMapper {
         return null;
     }
 
-    public void update(final String key, final String value) {
+    public Object update(final String key, final String value) {
         try {
             final Prop prop = propertyMap.get(key);
             if (prop != null) {
-                final Class<?> type = prop.getSetter().getParameterTypes()[0];
-                prop.getSetter().invoke(prop.getObject(), convert(value, type));
+                final Type genericType = prop.getSetter().getGenericParameterTypes()[0];
+                final Object typedValue = convert(value, genericType);
+                prop.getSetter().invoke(prop.getObject(), typedValue);
+                return typedValue;
             }
         } catch (final IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+            // TODO why swallow these exceptions
             LOGGER.debug(e.getMessage(), e);
         }
+        return null;
     }
 
-    private Object convert(final String value, final Class<?> type) {
+    private static Object convert(final String value, final Type genericType) {
         if (value == null) {
             return null;
         }
 
+        Class<?> type = getDataType(genericType);
+
         if (type.equals(String.class)) {
             return value;
-        } else if (type.equals(Byte.class)) {
+        } else if (type.equals(Byte.class) || type.equals(byte.class)) {
             return Byte.valueOf(value);
-        } else if (type.equals(Integer.class)) {
+        } else if (type.equals(Integer.class) || type.equals(int.class)) {
             return Integer.valueOf(value);
-        } else if (type.equals(Long.class)) {
+        } else if (type.equals(Long.class) || type.equals(long.class)) {
             return Long.valueOf(value);
-        } else if (type.equals(Short.class)) {
+        } else if (type.equals(Short.class) || type.equals(short.class)) {
             return Short.valueOf(value);
-        } else if (type.equals(Float.class)) {
+        } else if (type.equals(Float.class) || type.equals(float.class)) {
             return Float.valueOf(value);
-        } else if (type.equals(Double.class)) {
+        } else if (type.equals(Double.class) || type.equals(double.class)) {
             return Double.valueOf(value);
         } else if (type.equals(Boolean.class) || type.equals(boolean.class)) {
             return Boolean.valueOf(value);
-        } else if (type.equals(Character.class) && value.length() > 0) {
+        } else if ((type.equals(Character.class) || type.equals(char.class)) && value.length() > 0) {
             return value.charAt(0);
+        } else if (type.isAssignableFrom(List.class)) {
+            // determine the type of the list items
+            Class<?> itemType = getDataType(getGenericTypes(genericType).get(0));
+            return stringToList(value, itemType);
+        } else if (type.isAssignableFrom(Map.class)) {
+            // determine the types of the keys and values
+            Class<?> keyType = getDataType(getGenericTypes(genericType).get(0));
+            Class<?> valueType = getDataType(getGenericTypes(genericType).get(1));
+            return stringToMap(value, keyType, valueType);
         }
 
         return null;
     }
+
+    private static String listToString(final List<?> list) {
+
+        if (list.isEmpty()) {
+            return "";
+        }
+        List<String> strList = list.stream()
+                .map(ConfigMapper::convert)
+                .collect(Collectors.toList());
+
+        String allText = String.join("", strList);
+
+        String delimiter = getDelimiter(allText);
+
+        // prefix the delimited form with the delimiter so when we deserialise
+        // we know what the delimiter is
+        return delimiter + String.join(delimiter, strList);
+    }
+
+    private static String mapToString(final Map<?, ?> map) {
+        if (map.isEmpty()) {
+            return "";
+        }
+        // convert keys/values to strings
+        final List<Map.Entry<String, String>> strEntries = map.entrySet().stream()
+                .map(entry -> {
+                    String key = ConfigMapper.convert(entry.getKey());
+                    String value = ConfigMapper.convert(entry.getValue());
+                    return Map.entry(key, value);
+                })
+                .collect(Collectors.toList());
+
+        // join all strings into one fat string
+        final String allText = strEntries.stream()
+                .map(entry -> entry.getKey() + entry.getValue())
+                .collect(Collectors.joining());
+
+        final String entryDelimiter = getDelimiter(allText);
+        final String keyValueDelimiter = getDelimiter(allText, entryDelimiter);
+
+        // prefix the delimited form with the delimiters so when we deserialise
+        // we know what the delimiters are
+        return entryDelimiter + keyValueDelimiter + strEntries.stream()
+                .map(entry ->
+                    entry.getKey() + keyValueDelimiter + entry.getValue())
+                .collect(Collectors.joining(entryDelimiter));
+    }
+
+    private static <T> List<T> stringToList(final String serialisedForm, final Class<T> type) {
+       if (serialisedForm == null || serialisedForm.isEmpty()) {
+           return Collections.emptyList();
+       }
+
+       String delimiter = String.valueOf(serialisedForm.charAt(0));
+       String delimitedValue = serialisedForm.substring(1);
+
+       return StreamSupport.stream(Splitter.on(delimiter).split(delimitedValue).spliterator(), false)
+               .map(str -> convert(str, type))
+               .map(type::cast)
+               .collect(Collectors.toList());
+    }
+
+    private static <K, V> Map<K, V> stringToMap(
+            final String serialisedForm,
+            final Class<K> keyType,
+            final Class<V> valueType) {
+
+        final String entryDelimiter = String.valueOf(serialisedForm.charAt(0));
+        final String keyValueDelimiter = String.valueOf(serialisedForm.charAt(1));
+
+        // now remove the delimiters from our value
+        final String delimitedValue = serialisedForm.substring(2);
+
+        return StreamSupport.stream(Splitter.on(entryDelimiter).split(delimitedValue).spliterator(), false)
+                .map(keyValueStr -> {
+                    final List<String> parts = Splitter.on(keyValueDelimiter).splitToList(keyValueStr);
+
+                    if (parts.size() < 1 || parts.size() > 2) {
+                        throw new RuntimeException(LambdaLogger.buildMessage("Too many parts [{}] in value [{}], whole value [{}]",
+                                parts.size(), keyValueStr, serialisedForm));
+                    }
+
+                    String keyStr = parts.get(0);
+                    String valueStr = parts.size() == 1 ? null : parts.get(1);
+
+                    K key = keyType.cast(convert(keyStr, keyType));
+                    V value = valueStr != null ? valueType.cast(convert(valueStr, valueType)) : null;
+
+                    return Map.entry(key, value);
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private static Class getDataType(Class clazz) {
+        if(clazz.isPrimitive()) {
+            return clazz;
+        }
+
+        if(clazz.isArray()) {
+            return getDataType(clazz.getComponentType());
+        }
+
+        return clazz;
+    }
+
+    private static Class getDataType(Type type) {
+        if(type instanceof Class) {
+            return getDataType((Class) type);
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType pt = (ParameterizedType) type;
+            return getDataType(pt.getRawType());
+        } else {
+            throw new RuntimeException(LambdaLogger.buildMessage("Unexpected type of type {}",
+                    type.getClass().getName()));
+        }
+    }
+
+    private static List<Type> getGenericTypes(Type type) {
+        if(type instanceof Class) {
+            return Collections.emptyList();
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType pt = (ParameterizedType) type;
+            Type[] specificTypes = pt.getActualTypeArguments();
+
+            return Arrays.asList(specificTypes);
+        } else {
+            throw new RuntimeException(LambdaLogger.buildMessage("Unexpected type of type {}",
+                    type.getClass().getName()));
+        }
+    }
+
+    private static String getDelimiter(final String allText) {
+        return getDelimiter(allText, "");
+    }
+
+    private static String getDelimiter(final String allText, final String... blackListedDelimiters) {
+        // remove already used delimiters from the available candidates
+        final List<String> delimiterCandidates;
+        if (blackListedDelimiters!= null && blackListedDelimiters.length > 0) {
+            delimiterCandidates = new ArrayList<>(DELIMITERS);
+            for (String blackListedDelimiter : blackListedDelimiters) {
+                delimiterCandidates.remove(blackListedDelimiter);
+            }
+        } else {
+            delimiterCandidates = DELIMITERS;
+        }
+
+        // find the first delimiter that does not appear in the text
+        return delimiterCandidates.stream()
+                .filter(delimiter -> !allText.contains(delimiter))
+                .findFirst()
+                .orElseThrow(() ->
+                        new RuntimeException("Exhausted all delimiters"));
+    }
+
 }
--- stroom-config/stroom-config-global-impl-db/src/test/java/stroom/config/global/impl/db/TestConfigMapper.java
+++ stroom-config/stroom-config-global-impl-db/src/test/java/stroom/config/global/impl/db/TestConfigMapper.java
@@ -1,6 +1,6 @@
 package stroom.config.global.impl.db;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 import io.dropwizard.configuration.ConfigurationException;
 import io.dropwizard.configuration.ConfigurationFactory;
@@ -18,9 +18,16 @@ import org.slf4j.LoggerFactory;
 import stroom.config.app.AppConfig;
 import stroom.config.global.api.ConfigProperty;
 
-import javax.validation.Validator;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BooleanSupplier;
+import java.util.function.IntSupplier;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class TestConfigMapper {
 
@@ -42,60 +49,171 @@ class TestConfigMapper {
     void update_string() throws IOException, ConfigurationException {
         AppConfig appConfig = getAppConfig();
 
-        String initialValue = appConfig.getCoreConfig().getTemp();
+        Supplier<String> getter = () -> appConfig.getCoreConfig().getTemp();
+        String initialValue = getter.get();
         String newValue = initialValue + "/xxx";
 
         ConfigMapper configMapper = new ConfigMapper(appConfig);
         configMapper.update("stroom.core.temp", newValue);
 
-        Assertions.assertThat(appConfig.getCoreConfig().getTemp()).isEqualTo(newValue);
+        Assertions.assertThat(getter.get()).isEqualTo(newValue);
     }
 
     @Test
     void update_boolean() throws IOException, ConfigurationException {
         AppConfig appConfig = getAppConfig();
 
-        boolean initialValue = appConfig.getRefDataStoreConfig().isReadAheadEnabled();
+        BooleanSupplier getter = () -> appConfig.getRefDataStoreConfig().isReadAheadEnabled();
+        boolean initialValue = getter.getAsBoolean();
         boolean newValue = !initialValue;
 
         ConfigMapper configMapper = new ConfigMapper(appConfig);
         configMapper.update("stroom.refdata.readAheadEnabled", Boolean.valueOf(newValue).toString().toLowerCase());
 
-        Assertions.assertThat(appConfig.getRefDataStoreConfig().isReadAheadEnabled()).isEqualTo(newValue);
+        Assertions.assertThat(getter.getAsBoolean()).isEqualTo(newValue);
     }
 
+    @Test
+    void update_int() throws IOException, ConfigurationException {
+        AppConfig appConfig = getAppConfig();
 
+        IntSupplier getter = () -> appConfig.getRefDataStoreConfig().getMaxPutsBeforeCommit();
+        int initialValue = getter.getAsInt();
+        int newValue = initialValue + 1;
+
+        ConfigMapper configMapper = new ConfigMapper(appConfig);
+        configMapper.update("stroom.refdata.maxPutsBeforeCommit", Integer.toString(newValue));
+
+        Assertions.assertThat(getter.getAsInt()).isEqualTo(newValue);
+    }
+
+//    @Test
+//    void update_long() throws IOException, ConfigurationException {
+//        AppConfig appConfig = getAppConfig();
+//
+//        LongSupplier getter = () -> appConfig.getRefDataStoreConfig().getPurgeAgeMs();
+//        long initialValue = getter.getAsLong();
+//        long newValue = initialValue + 1;
+//
+//        ConfigMapper configMapper = new ConfigMapper(appConfig);
+//        configMapper.update("stroom.refdata.purgeAgeMs", Long.toString(newValue));
+//
+//        Assertions.assertThat(getter.getAsLong()).isEqualTo(newValue);
+//    }
+
+
+    @Test
+    void testGetGlobalProperties2() {
+
+        ExtendedAppConfig extendedAppConfig = new ExtendedAppConfig();
+        ConfigMapper configMapper = new ConfigMapper(extendedAppConfig);
+
+        List<ConfigProperty> configProperties = configMapper.getGlobalProperties();
+
+        configProperties.forEach(configProperty ->
+                LOGGER.debug("{} - {}", configProperty.getName(), configProperty.getValue()));
+    }
+
+    @Test
+    void update_string2() {
+        ExtendedAppConfig extendedAppConfig = new ExtendedAppConfig();
+
+        Supplier<String> getter = () -> extendedAppConfig.getTestConfig().getStringProp();
+        String initialValue = getter.get();
+        String newValue = initialValue + "xxx";
+
+        ConfigMapper configMapper = new ConfigMapper(extendedAppConfig);
+        configMapper.update("stroom.test.stringProp", newValue);
+
+        Assertions.assertThat(getter.get()).isEqualTo(newValue);
+    }
+
+    @Test
+    void update_primitiveInt() {
+        ExtendedAppConfig extendedAppConfig = new ExtendedAppConfig();
+
+        IntSupplier getter = () -> extendedAppConfig.getTestConfig().getTestPrimitiveConfig().getIntProp();
+        int initialValue = getter.getAsInt();
+        int newValue = initialValue + 1;
+
+        ConfigMapper configMapper = new ConfigMapper(extendedAppConfig);
+        configMapper.update("stroom.test.primitive.intProp", Integer.toString(newValue));
+
+        Assertions.assertThat(getter.getAsInt()).isEqualTo(newValue);
+    }
+
+    @Test
+    void update_boxedInt() {
+        ExtendedAppConfig extendedAppConfig = new ExtendedAppConfig();
+
+        Supplier<Integer> getter = () -> extendedAppConfig.getTestConfig().getTestBoxedConfig().getIntProp();
+        Integer initialValue = getter.get();
+        Integer newValue = initialValue + 1;
+
+        ConfigMapper configMapper = new ConfigMapper(extendedAppConfig);
+        configMapper.update("stroom.test.boxed.intProp", Integer.toString(newValue));
+
+        Assertions.assertThat(getter.get()).isEqualTo(newValue);
+    }
+
+    @Test
+    void update_list() {
+        ExtendedAppConfig extendedAppConfig = new ExtendedAppConfig();
+        ConfigMapper configMapper = new ConfigMapper(extendedAppConfig);
+
+        Supplier<List<String>> getter = () -> extendedAppConfig.getTestConfig().getStringListProp();
+        List<String> initialValue = getter.get();
+        List<String> newValue = Stream.of(initialValue, initialValue)
+                .flatMap(List::stream)
+                .map(str -> str + "x")
+                .collect(Collectors.toList());
+        String newValueStr = ConfigMapper.convert(newValue);
+
+        configMapper.update("stroom.test.stringListProp", newValueStr);
+
+        Assertions.assertThat(getter.get()).isEqualTo(newValue);
+    }
+
+    @Test
+    void update_map() {
+        ExtendedAppConfig extendedAppConfig = new ExtendedAppConfig();
+        ConfigMapper configMapper = new ConfigMapper(extendedAppConfig);
+
+        Supplier<Map<String, Long>> getter = () -> extendedAppConfig.getTestConfig().getStringLongMapProp();
+        Map<String, Long> initialValue = getter.get();
+        Map<String, Long> newValue = new HashMap<>();
+        initialValue.forEach((k, v) ->
+                newValue.put(k, v + 10));
+        newValue.put("k4", 14L);
+
+        String newValueStr = ConfigMapper.convert(newValue);
+
+        configMapper.update("stroom.test.stringLongMapProp", newValueStr);
+
+        Assertions.assertThat(getter.get()).isEqualTo(newValue);
+    }
 
     private AppConfig getAppConfig() throws IOException, ConfigurationException{
         ConfigurationSourceProvider configurationSourceProvider = new SubstitutingSourceProvider(
                 new FileConfigurationSourceProvider(),
                 new EnvironmentVariableSubstitutor(false));
 
-            Config config = parseConfiguration(
-                    new DefaultConfigurationFactoryFactory<>(),
-                    configurationSourceProvider,
-                    io.dropwizard.jersey.validation.Validators.newValidator(),
-//                    "../../stroom-app/dev.yml",
-                    "../../local.yml",
-                    Config.class,
-                    Jackson.newObjectMapper());
+            ConfigurationFactoryFactory<Config> configurationFactoryFactory = new DefaultConfigurationFactoryFactory<>();
+
+        final ConfigurationFactory<Config> configurationFactory = configurationFactoryFactory
+                .create(
+                        Config.class,
+                        io.dropwizard.jersey.validation.Validators.newValidator(),
+                        Jackson.newObjectMapper(),
+                        "dw");
+            Config config = configurationFactory.build(configurationSourceProvider, "../../stroom-app/dev.yml");
 
            return config.getAppConfig();
     }
 
-    private Config parseConfiguration(ConfigurationFactoryFactory<Config> configurationFactoryFactory,
-                                 ConfigurationSourceProvider provider,
-                                 Validator validator,
-                                 String path,
-                                 Class<Config> klass,
-                                 ObjectMapper objectMapper) throws IOException, ConfigurationException {
-        final ConfigurationFactory<Config> configurationFactory = configurationFactoryFactory
-                .create(klass, validator, objectMapper, "dw");
-        if (path != null) {
-            return configurationFactory.build(provider, path);
-        }
-        return configurationFactory.build();
-    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
     private static class Config extends Configuration {
         private AppConfig appConfig;
@@ -106,6 +224,193 @@ class TestConfigMapper {
 
         void setAppConfig(final AppConfig appConfig) {
             this.appConfig = appConfig;
+        }
+    }
+
+    private static class ExtendedAppConfig extends AppConfig {
+
+        private TestConfig testConfig = new TestConfig();
+
+        @JsonProperty("test")
+        TestConfig getTestConfig() {
+            return testConfig;
+        }
+
+        void setTestConfig(final TestConfig testConfig) {
+            this.testConfig = testConfig;
+        }
+    }
+
+    private static class TestConfig {
+
+        private String stringProp = "initial value";
+        private List<String> stringListProp = new ArrayList<>();
+        private List<Integer> intListProp = new ArrayList<>();
+        private Map<String, Long> stringLongMapProp = new HashMap<>();
+        private TestPrimitiveConfig testPrimitiveConfig = new TestPrimitiveConfig();
+        private TestBoxedConfig testBoxedConfig = new TestBoxedConfig();
+
+        TestConfig() {
+            stringListProp.add("item 1");
+            stringListProp.add("item 2");
+            stringListProp.add("item 3");
+
+            intListProp.add(1);
+            intListProp.add(2);
+            intListProp.add(3);
+
+            stringLongMapProp.put("k1", 1L);
+            stringLongMapProp.put("k2", 2L);
+            stringLongMapProp.put("k3", 3L);
+        }
+
+        String getStringProp() {
+            return stringProp;
+        }
+
+        void setStringProp(final String stringValue) {
+            this.stringProp = stringValue;
+        }
+
+        @JsonProperty("primitive")
+        TestPrimitiveConfig getTestPrimitiveConfig() {
+            return testPrimitiveConfig;
+        }
+
+        void setTestPrimitiveConfig(final TestPrimitiveConfig testPrimitiveConfig) {
+            this.testPrimitiveConfig = testPrimitiveConfig;
+        }
+
+        @JsonProperty("boxed")
+        TestBoxedConfig getTestBoxedConfig() {
+            return testBoxedConfig;
+        }
+
+        void setTestBoxedConfig(final TestBoxedConfig testBoxedConfig) {
+            this.testBoxedConfig = testBoxedConfig;
+        }
+
+        List<String> getStringListProp() {
+            return stringListProp;
+        }
+
+        void setStringListProp(final List<String> stringListProp) {
+            this.stringListProp = stringListProp;
+        }
+
+        List<Integer> getIntListProp() {
+            return intListProp;
+        }
+
+        void setIntListProp(final List<Integer> intListProp) {
+            this.intListProp = intListProp;
+        }
+
+        Map<String, Long> getStringLongMapProp() {
+            return stringLongMapProp;
+        }
+
+        void setStringLongMapProp(final Map<String, Long> stringLongMapProp) {
+            this.stringLongMapProp = stringLongMapProp;
+        }
+    }
+
+
+    private static class TestPrimitiveConfig {
+
+        private boolean booleanProp = false;
+        private int intProp = 123;
+        private long longProp = 123L;
+        private double doubleProp = 1.23;
+        private short shortProp = 123;
+
+        boolean isBooleanProp() {
+            return booleanProp;
+        }
+
+        void setBooleanProp(final boolean booleanProp) {
+            this.booleanProp = booleanProp;
+        }
+
+        int getIntProp() {
+            return intProp;
+        }
+
+        void setIntProp(final int intProp) {
+            this.intProp = intProp;
+        }
+
+        long getLongProp() {
+            return longProp;
+        }
+
+        void setLongProp(final long longProp) {
+            this.longProp = longProp;
+        }
+
+        double getDoubleProp() {
+            return doubleProp;
+        }
+
+        void setDoubleProp(final double doubleProp) {
+            this.doubleProp = doubleProp;
+        }
+
+        short getShortProp() {
+            return shortProp;
+        }
+
+        void setShortProp(final short shortProp) {
+            this.shortProp = shortProp;
+        }
+    }
+
+    private static class TestBoxedConfig {
+
+        private Boolean booleanProp = false;
+        private Integer intProp = 123;
+        private Long longProp = 123L;
+        private Double doubleProp = 1.23;
+        private Short shortProp = 123;
+
+        Boolean getBooleanProp() {
+            return booleanProp;
+        }
+
+        void setBooleanProp(final Boolean booleanProp) {
+            this.booleanProp = booleanProp;
+        }
+
+        Integer getIntProp() {
+            return intProp;
+        }
+
+        void setIntProp(final Integer intProp) {
+            this.intProp = intProp;
+        }
+
+        Long getLongProp() {
+            return longProp;
+        }
+
+        void setLongProp(final Long longProp) {
+            this.longProp = longProp;
+        }
+
+        Double getDoubleProp() {
+            return doubleProp;
+        }
+
+        void setDoubleProp(final Double doubleProp) {
+            this.doubleProp = doubleProp;
+        }
+
+        Short getShortProp() {
+            return shortProp;
+        }
+
+        void setShortProp(final Short shortProp) {
+            this.shortProp = shortProp;
         }
     }
 }
