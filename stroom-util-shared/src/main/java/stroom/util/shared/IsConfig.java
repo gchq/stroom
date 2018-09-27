342df2bf60786b1add297bae960e69e251415b25
at055612 <at055612@users.noreply.github.com>
2018-09-27 14:49:38 +0100

    Add more javadoc to IsConfig

--- stroom-util-shared/src/main/java/stroom/util/shared/IsConfig.java
+++ stroom-util-shared/src/main/java/stroom/util/shared/IsConfig.java
@@ -1,7 +1,8 @@
 package stroom.util.shared;
 
 /**
- * Marker interface for java bean classes used to provide configuration properties. Implementing classes
+ * Marker interface for java bean classes used to provide configuration properties.
+ * This includes AppConfig and the classes that sit beneath it.  Implementing classes
  * are expected to be (de)serialised from/to YAML configuration files.
  */
 public interface IsConfig {
