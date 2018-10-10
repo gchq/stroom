785ffcd1f3eaa3c266064b77ece9b4bd3da666c0
stroomdev66 <stroomdev66@gmail.com>
2018-10-10 13:10:29 +0100

    Remove console redirect to log file

--- stroom-app/src/dist/stroom.sh
+++ stroom-app/src/dist/stroom.sh
@@ -12,8 +12,7 @@ YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
 NC='\033[0m' # No Colour
 
-
-NO_ARGUMENT_MESSAGE="Please supply an argument: either ${YELLOW}start${NC}, ${YELLOW}stop${NC}, ${YELLOW}log${NC} or ${YELLOW}errors${NC}."
+NO_ARGUMENT_MESSAGE="Please supply an argument: either ${YELLOW}start${NC} or ${YELLOW}stop${NC}."
 # Check script's params
 if [ $# -ne 1 ]; then
     echo -e $NO_ARGUMENT_MESSAGE
@@ -26,8 +25,6 @@ mkdir -p log
 SERVICE_NAME=Stroom
 PATH_TO_JAR=stroom-app-all.jar
 PID_PATH_NAME=log/stroom.pid
-LOG_FILE=log/stroom.log
-ERROR_LOG_FILE=log/stroom_error.log
 JAVA_OPTS="-Xms512m -Xmx2048m"
 
 startStroom(){
@@ -47,17 +44,15 @@ startStroom(){
     # Run Stroom
     echo -e " - Running ${GREEN}Stroom${NC}"
 #    java -jar stroom-app-all.jar server config.yml
-    nohup java $JAVA_OPTS -jar $PATH_TO_JAR server config.yml > $LOG_FILE 2> $ERROR_LOG_FILE &
+    nohup java $JAVA_OPTS -jar $PATH_TO_JAR server config.yml &
     echo $! > $PID_PATH_NAME
-    echo -e ""
-    echo -e "Run '${YELLOW}./stroom.sh log${NC}' to see what's happening, or '${YELLOW}./stroom.sh errors${NC}' for errors."
     echo -e ""
 }
 
 stopStroom(){
     PID=$(cat $PID_PATH_NAME);
     kill $PID;
-    echo -e "Stopping ${GREEN}Stroom${NC} gracefully. Run '${YELLOW}./stroom.sh log${NC}' to check for progress."
+    echo -e "Stopping ${GREEN}Stroom${NC} gracefully."
     echo -e ""
     rm $PID_PATH_NAME
 }
@@ -77,16 +72,6 @@ case $1 in
         else
             echo -e "${RED}Cannot${NC} stop ${GREEN}Stroom${NC} because it does not appear to be running (there is no PID file)."
         fi
-    ;;
-    log)
-        echo -e "Tailing ${GREEN}Stroom's${NC} log file at ${YELLOW}$LOG_FILE${NC}. Use ${YELLOW}Ctrl+C${NC} to stop tailing."
-        echo -e ""
-        tail -f $LOG_FILE
-    ;;
-    errors)
-        echo -e "Tailing ${GREEN}Stroom's${NC} error log at ${YELLOW}$ERROR_LOG_FILE${NC}. Use ${YELLOW}Ctrl+C${NC} to stop tailing."
-        echo -e ""
-        tail -f $ERROR_LOG_FILE
     ;;
     help)
         echo -e $NO_ARGUMENT_MESSAGE
