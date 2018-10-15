41332845a787d905c2715f31c9426da30a357614
at055612 <at055612@users.noreply.github.com>
2018-10-15 16:12:13 +0100

    Improve Dockerfiles

--- runProxyDocker.sh
+++ runProxyDocker.sh
@@ -2,17 +2,33 @@
 
 set -e
 
-docker rm stroom-proxy || echo "No container to delete"
+main() {
+    local -r VOLUMES=( \
+        stroom-proxy_config \
+        stroom-proxy_content \
+        stroom-proxy_logs \
+        stroom-proxy_repo)
 
-docker volume rm stroom-proxy_state || echo "No volume to delete"
+    docker rm stroom-proxy || echo "No container to delete"
 
-docker volume create stroom-proxy_state
+    for volume in "${VOLUMES[@]}"; do
+        docker volume rm $"{volume}" || echo "Volume ${volume} doesn't exist to delete"
+    done
 
-docker \
-    run --name stroom-proxy \
-    --mount source=stroom-proxy_state,target=/stroom-proxy/state/ \
-    --publish 9090:9090 \
-    --publish 9091:9091 \
-    gchq/stroom-proxy:dev-SNAPSHOT
-    #gchq/stroom-proxy:dev-SNAPSHOT bash -c "cat /stroom-proxy/state/config.yml"
+    for volume in "${VOLUMES[@]}"; do
+        docker volume create "${volume}"
+    done
+
+    docker \
+        run --name stroom-proxy \
+        --mount source=stroom-proxy_config,target=/stroom-proxy/config/ \
+        --mount source=stroom-proxy_content,target=/stroom-proxy/content/ \
+        --mount source=stroom-proxy_logs,target=/stroom-proxy/logs/ \
+        --mount source=stroom-proxy_repo,target=/stroom-proxy/repo/ \
+        --publish 9090:9090 \
+        --publish 9091:9091 \
+        gchq/stroom-proxy:dev-SNAPSHOT
+        #gchq/stroom-proxy:dev-SNAPSHOT bash -c "cat /stroom-proxy/state/config.yml"
+}
+
 
