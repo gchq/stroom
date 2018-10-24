95a2574fa23ebf902be59f15a8706a257ef911e0
at055612 <at055612@gmail.com>
2018-10-25 00:13:18 +0100

    Add log shipping to proxy docker, fix stroom log shipping

--- stroom-app/docker/docker-entrypoint.sh
+++ stroom-app/docker/docker-entrypoint.sh
@@ -6,6 +6,20 @@ set -e
 #if [ "$1" = 'stroom' -a "$(id -u)" = '0' ]; then
 if [ "$(id -u)" = '0' ]; then
     chown -R stroom:stroom .
+    
+    # This is a bit of a cludge to get round "Text file in use" errors
+    # See: https://github.com/moby/moby/issues/9547
+    # sync ensures all disk writes are persisted
+    sync
+
+    # Build the crontab file to send logs to stroom
+    echo "Creating the crontab file"
+    ./create_crontab.sh
+
+    # Start the cron daemon
+    echo "Starting cron in the background"
+    crond
+    
     #su-exec is the alpine equivalent of gosu
     #runs all args as user stroom, rather than as root
     exec su-exec stroom "$@"
