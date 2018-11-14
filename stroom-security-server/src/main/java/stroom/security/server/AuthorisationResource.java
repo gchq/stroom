57038bc7bece352ec6228551e507c048c8528288
jc064522 <jc064522@gmail.com>
2018-11-14 08:18:05 +0000

    Add an endpoint for the creation of internal users

--- stroom-security-server/src/main/java/stroom/security/server/AuthorisationResource.java
+++ stroom-security-server/src/main/java/stroom/security/server/AuthorisationResource.java
@@ -3,17 +3,18 @@ package stroom.security.server;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import stroom.security.SecurityContext;
 
-import javax.validation.constraints.NotNull;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -24,11 +25,15 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 @Component
 public class AuthorisationResource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorisationResource.class);
+
     private final SecurityContext securityContext;
+    private final UserService userService;
 
     @Inject
-    public AuthorisationResource(final SecurityContext securityContext) {
+    public AuthorisationResource(final SecurityContext securityContext, UserService userService) {
         this.securityContext = securityContext;
+        this.userService = userService;
     }
 
     /**
@@ -66,5 +71,24 @@ public class AuthorisationResource {
         boolean result = securityContext.hasAppPermission(userPermissionRequest.getPermission());
         // The user here will be the one logged in by the JWT.
         return result ? Response.ok().build() : Response.status(Response.Status.UNAUTHORIZED).build();
+    }
+
+    /**
+     * This function is used by the Users UI to create a Stroom user for authorisation purposes.
+     * It solves the problem of Users having to log in before they're available to assign permissions to.
+     */
+    @POST
+    @Path("createUser")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createUser(@QueryParam("id") String userId) {
+        try{
+            userService.createUser(userId);
+            return Response.ok().build();
+        }
+        catch(Exception e){
+            LOGGER.error("Unable to create user: {}", e);
+            return Response.serverError().build();
+        }
     }
 }
