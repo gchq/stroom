f803be5461277a90c74f42eecec01215fb0d69ba
at055612 <at055612@users.noreply.github.com>
2018-11-19 11:36:14 +0000

    Remove log sending process from docker images
    Now handled by stroom-log-sender

--- stroom-app/docker/docker-entrypoint.sh
+++ stroom-app/docker/docker-entrypoint.sh
@@ -12,14 +12,6 @@ if [ "$(id -u)" = '0' ]; then
     # sync ensures all disk writes are persisted
     sync
 
-    # Build the crontab file to send logs to stroom
-    echo "Creating the crontab file"
-    ./create_crontab.sh
-
-    # Start the cron daemon
-    echo "Starting cron in the background"
-    crond
-    
     #su-exec is the alpine equivalent of gosu
     #runs all args as user stroom, rather than as root
     exec su-exec stroom "$@"
