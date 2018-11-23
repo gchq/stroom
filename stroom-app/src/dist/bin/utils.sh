b6831330464660709317e47cae722f9b0ead419e
jc064522 <jc064522@gmail.com>
2018-11-23 13:59:40 +0000

    Improve logging and error checking in the stand-alone stack

--- stroom-app/src/dist/bin/utils.sh
+++ stroom-app/src/dist/bin/utils.sh
@@ -14,7 +14,7 @@ BLUE='\033[1;34m'
 BLUE2='\033[1;34m'
 NC='\033[0m' # No Colour
 
-mkdir -p log
+mkdir -p logs
 
 greeting() {
   echo -e "${GREEN}                    ╔╬═${NC}"
@@ -42,10 +42,62 @@ ask_about_logs() {
   fi
 }
 
+error() {
+    echo -e "${RED}Error:${NC} $1"
+}
+
 warn() {
-    echo -e "${RED}Warning:${NC} $1"
+    echo -e "${YELLOW}Warning:${NC} $1"
 }
 
 info() {
     echo -e "${GREEN}Info:${NC} $1"
+}
+
+wait_for_200_response() {
+    if [[ $# -ne 1 ]]; then
+        error "Invalid arguments to wait_for_200_response(), expecting a URL to wait for."
+        exit 1
+    fi
+
+    local -r url=$1
+    local -r maxWaitSecs=120
+    echo
+
+    n=0
+    # Keep retrying for maxWaitSecs
+    until [ $n -ge ${maxWaitSecs} ]
+    do
+        # Check for a configuration parsing error
+        LOG_ERROR_PATTERN="io.dropwizard.configuration.ConfigurationParsingException"
+        SCRIPT_LOG_LOCATION="logs/start.sh.log"
+
+        if grep -q "${LOG_ERROR_PATTERN}" "${SCRIPT_LOG_LOCATION}"; then
+            echo -e
+            error "It looks like you have a problem with something in ${BLUE}config/config.yml${NC}. Look in ${BLUE}logs/start.sh.log${NC} for the details.${NC}"
+            break
+            exit 1
+        fi
+
+        # OR with true to prevent the non-zero exit code from curl from stopping our script
+        responseCode=$(curl -sL -w "%{http_code}\\n" "${url}" -o /dev/null || true)
+        #echo "Response code: ${responseCode}"
+        if [[ "${responseCode}" = "200" ]]; then
+            break
+        fi
+        # print a simple unbounded progress bar, increasing every 2s
+        mod=$(($n%2))
+        if [[ ${mod} -eq 0 ]]; then
+            printf '.'
+        fi
+
+        n=$[$n+1]
+        # sleep for two secs
+        sleep 1
+    done
+    printf "\n"
+
+    if [[ $n -ge ${maxWaitSecs} ]]; then
+        echo -e "${RED}Gave up wating for stroom to start up, check the logs: (${BLUE}./logs${NC}, or ${BLUE}logs/start.sh.log${NC}${RED})${NC}"
+    fi
 }
