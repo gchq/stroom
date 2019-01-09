318911621e412a941ee0986027325695e2d200a0
jc064522 <jc064522@gmail.com>
2019-01-09 15:35:39 +0000

    gh-1063 - Annotations -> Functions for statistics jobs

--- stroom-statistics/stroom-statistics-server/src/main/java/stroom/statistics/sql/SQLStatisticsJobs.java
+++ stroom-statistics/stroom-statistics-server/src/main/java/stroom/statistics/sql/SQLStatisticsJobs.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.List;
 
+import static stroom.util.lifecycle.jobmanagement.Schedule.ScheduleType.CRON;
 import static stroom.util.lifecycle.jobmanagement.Schedule.ScheduleType.PERIODIC;
 import static stroom.util.lifecycle.jobmanagement.ScheduledJob.ScheduledJobBuilder.jobBuilder;
 
@@ -14,10 +15,17 @@ import static stroom.util.lifecycle.jobmanagement.ScheduledJob.ScheduledJobBuild
 public class SQLStatisticsJobs implements ScheduledJobs {
 
     private SQLStatisticEventStore sqlStatisticEventStore;
+    private SQLStatisticCache sqlStatisticCache;
+    private SQLStatisticAggregationManager sqlStatisticAggregationManager;
 
     @Inject
-    public SQLStatisticsJobs(SQLStatisticEventStore sqlStatisticEventStore) {
+    public SQLStatisticsJobs(
+            SQLStatisticEventStore sqlStatisticEventStore,
+            SQLStatisticCache sqlStatisticCache,
+            SQLStatisticAggregationManager sqlStatisticAggregationManager) {
         this.sqlStatisticEventStore = sqlStatisticEventStore;
+        this.sqlStatisticCache = sqlStatisticCache;
+        this.sqlStatisticAggregationManager = sqlStatisticAggregationManager;
     }
 
     @Override
@@ -29,6 +37,18 @@ public class SQLStatisticsJobs implements ScheduledJobs {
                         .managed(false)
                         .schedule(PERIODIC, "1m")
                         .method((task) -> this.sqlStatisticEventStore.evict())
+                        .build(),
+                jobBuilder()
+                        .name("SQL Stats In Memory Flush")
+                        .description("SQL Stats In Memory Flush (Cache to DB)")
+                        .schedule(CRON, "0,10,20,30,40,50 * *")
+                        .method((task) -> this.sqlStatisticCache.execute())
+                        .build(),
+                jobBuilder()
+                        .name("SQL Stats Database Aggregation")
+                        .description("Run SQL stats database aggregation")
+                        .schedule(CRON, "5,15,25,35,45,55 * *")
+                        .method((task) -> this.sqlStatisticAggregationManager.aggregate())
                         .build()
         );
     }
