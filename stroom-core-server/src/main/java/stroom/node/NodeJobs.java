83066ddd33c5384776f32df2c185b5299605675a
jc064522 <jc064522@gmail.com>
2019-01-09 15:49:47 +0000

    gh-1063 - Annotations -> Functions for node jobs

--- stroom-core-server/src/main/java/stroom/node/NodeJobs.java
+++ stroom-core-server/src/main/java/stroom/node/NodeJobs.java
@@ -14,24 +14,35 @@ import static stroom.util.lifecycle.jobmanagement.ScheduledJob.ScheduledJobBuild
 public class NodeJobs implements ScheduledJobs {
 
     private HeapHistogramStatisticsExecutor heapHistogramStatisticsExecutor;
+    private NodeStatusExecutor nodeStatusExecutor;
 
     @Inject
-    public NodeJobs(HeapHistogramStatisticsExecutor heapHistogramStatisticsExecutor){
+    public NodeJobs(
+            HeapHistogramStatisticsExecutor heapHistogramStatisticsExecutor,
+            NodeStatusExecutor nodeStatusExecutor) {
         this.heapHistogramStatisticsExecutor = heapHistogramStatisticsExecutor;
+        this.nodeStatusExecutor = nodeStatusExecutor;
     }
 
     @Override
     public List<ScheduledJob> getJobs() {
         return List.of(
                 jobBuilder()
-                .name("Java Heap Histogram Statistics")
-                .description("Generate Java heap map histogram ('jmap -histo:live') and record statistic events " +
-                    "for the entries. CAUTION: this will pause the JVM, only enable this if you understand the " +
-                    "consequences!")
-                .schedule(CRON, "0 * *")
-                .method((task) -> this.heapHistogramStatisticsExecutor.exec())
-                .enabled(false)
-                .build()
+                        .name("Java Heap Histogram Statistics")
+                        .description("Generate Java heap map histogram ('jmap -histo:live') and record statistic events " +
+                                "for the entries. CAUTION: this will pause the JVM, only enable this if you understand the " +
+                                "consequences!")
+                        .schedule(CRON, "0 * *")
+                        .method((task) -> this.heapHistogramStatisticsExecutor.exec())
+                        .enabled(false)
+                        .build(),
+                jobBuilder()
+                        .name("Node Status")
+                        .description("Job to record status of node (CPU and Memory usage)")
+                        .schedule(CRON, "* * *")
+                        .method((task) -> this.nodeStatusExecutor.exec())
+                        .advanced(false)
+                        .build()
         );
     }
 }
