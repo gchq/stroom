46346d162e8818998273b4492dea1638f0908bad
stroomdev66 <stroomdev66@gmail.com>
2019-01-25 11:12:09 +0000

    Fix config

--- stroom-config/stroom-config-global-impl-db/src/main/resources/stroom/config/global/impl/db/migration/V7_0_0_1__config.sql
+++ stroom-config/stroom-config-global-impl-db/src/main/resources/stroom/config/global/impl/db/migration/V7_0_0_1__config.sql
@@ -1,21 +1,17 @@
 --
--- Create the property table
+-- Create the config table
 --
 CREATE TABLE IF NOT EXISTS config (
   id 				    int(11) NOT NULL AUTO_INCREMENT,
+  version               int(11) NOT NULL,
+  create_time_ms        bigint(20) NOT NULL,
+  create_user           varchar(255) NOT NULL,
+  update_time_ms        bigint(20) NOT NULL,
+  update_user           varchar(255) NOT NULL,
   name				    varchar(255) NOT NULL,
   val 				    longtext NOT NULL,
   PRIMARY KEY           (id),
   UNIQUE KEY            name (name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS config_history (
-  id 				    int(11) NOT NULL AUTO_INCREMENT,
-  update_time        	bigint(20) DEFAULT NULL,
-  update_user 			varchar(255) DEFAULT NULL,
-  name				    varchar(255) NOT NULL,
-  val 				    longtext NOT NULL,
-  PRIMARY KEY           (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -25,28 +21,21 @@ DROP PROCEDURE IF EXISTS copy;
 DELIMITER //
 CREATE PROCEDURE copy ()
 BEGIN
-    IF (SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'GLOB_PROP' > 0) THEN
-        INSERT INTO config (name, val)
-        SELECT NAME, VAL
-        FROM GLOB_PROP
-        ORDER BY NAME;
+  IF (SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'GLOB_PROP' > 0) THEN
+    INSERT INTO config (id, version, create_time_ms, create_user, update_time_ms, update_user, name, val)
+    SELECT ID, VER, CRT_MS, CRT_USER, UPD_MS, UPD_USER, NAME, VAL
+    FROM GLOB_PROP
+    WHERE ID > (SELECT COALESCE(MAX(id), 0) FROM activity)
+    ORDER BY ID;
 
-        INSERT INTO config_history (update_time, update_user, name, val)
-        SELECT UPD_MS, UPD_USER, NAME, VAL
-        FROM GLOB_PROP
-        ORDER BY NAME;
-    END IF;
-    IF (SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'OLD_GLOB_PROP' > 0) THEN
-      INSERT INTO config (name, val)
-      SELECT NAME, VAL
-      FROM OLD_GLOB_PROP
-      ORDER BY NAME;
+    -- Work out what to set our auto_increment start value to
+    SELECT CONCAT('ALTER TABLE config AUTO_INCREMENT = ', COALESCE(MAX(id) + 1, 1))
+    INTO @alter_table_sql
+    FROM config;
 
-      INSERT INTO config_history (update_time, update_user, name, val)
-      SELECT UPD_MS, UPD_USER, NAME, VAL
-      FROM OLD_GLOB_PROP
-      ORDER BY NAME;
-    END IF;
+    PREPARE alter_table_stmt FROM @alter_table_sql;
+    EXECUTE alter_table_stmt;
+  END IF;
 END//
 DELIMITER ;
 CALL copy();
