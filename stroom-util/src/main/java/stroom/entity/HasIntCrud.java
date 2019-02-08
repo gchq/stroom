47e622b303cff89ced7b64b26e086aed5f3a1944
at055612 <at055612@gmail.com>
2019-02-08 12:03:23 +0000

    Rename Basic(Int)?CrudDao to Has(Int)?Crud

--- stroom-util/src/main/java/stroom/entity/HasIntCrud.java
+++ stroom-util/src/main/java/stroom/entity/HasIntCrud.java
@@ -6,16 +6,16 @@ import java.util.Optional;
 public interface BasicIntCrudDao<EntityType> {
 
     /**
-     * Persist the passes record object
+     * Creates the passes record object in the persistence implementation
      * @param record Object to persist.
      * @return The persisted object including any changes such as auto IDs
      */
     EntityType create(@Nonnull final EntityType record);
 
     /**
-     * Update the passed record in the database
+     * Update the passed record in the persistence implementation
      * @param record The record to update.
-     * @return The record as it now appears in the database
+     * @return The record as it now appears in the persistence implementation
      */
     EntityType update(@Nonnull final EntityType record);
 
@@ -24,10 +24,10 @@ public interface BasicIntCrudDao<EntityType> {
      * @param id The unique identifier for the record to delete.
      * @return The number of records deleted.
      */
-    int delete(final int id);
+    boolean delete(final int id);
 
     /**
-     * Fetch a record from the database using its unique id value.
+     * Fetch a record from the persistence implementation using its unique id value.
      * @param id The id to uniquely identify the required record with
      * @return The record associated with the id in the database, if it exists.
      */
