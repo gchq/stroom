6b1ae94cd587a2de626e97e0ea07a52d562ebb78
stroomdev66 <stroomdev66@gmail.com>
2019-04-15 11:51:23 +0100

    Sproc for SQL statistics upsert that works across MySQL versions

--- stroom-statistics/stroom-statistics-impl-sql/src/main/java/stroom/statistics/impl/sql/SQLStatisticAggregationTransactionHelper.java
+++ stroom-statistics/stroom-statistics-impl-sql/src/main/java/stroom/statistics/impl/sql/SQLStatisticAggregationTransactionHelper.java
@@ -34,14 +34,14 @@ import java.sql.Statement;
 import java.sql.Types;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Singleton
 public class SQLStatisticAggregationTransactionHelper {
     public static final long NEWEST_SENSIBLE_STAT_AGE = DateUtil.parseNormalDateTimeString("9999-01-01T00:00:00.000Z");
-    // /**
-    // * The number of records to add to the aggregate from the aggregate source
-    // * table on each pass
-    // */
+    // The number of records to add to the aggregate from the aggregate source
+    // table on each pass
+    //
     // private static final int AGGREGATE_INCREMENT = 10000;
     public static final byte DEFAULT_PRECISION = 0;
     public static final long MS_HOUR = 1000 * 60 * 60;
@@ -94,93 +94,6 @@ public class SQLStatisticAggregationTransactionHelper {
     // then insert or update the values back into SV
     // relies on SV having a unique key index on FK_SQL_STAT_KEY_ID, TIME_MS,
     // PRES, VAL_TP
-//    private static final String STAGE1_UPSERT = "" +
-//            "INSERT INTO SQL_STAT_VAL (FK_SQL_STAT_KEY_ID, TIME_MS, PRES, VAL_TP, VAL, CT) " +
-//            "SELECT " +
-//            "   AGG.FK_SQL_STAT_KEY_ID, " +
-//            "   AGG.TIME_MS, " +
-//            "   AGG.PRES, " +
-//            "   AGG.VAL_TP, " +
-//            "   AGG.VAL, " +
-//            "   AGG.CT " +
-//            "FROM ( " +
-//            "   SELECT " +
-//            "       SSVT.FK_SQL_STAT_KEY_ID as FK_SQL_STAT_KEY_ID, " +
-//            "       SSVT.TIME_MS_RND as TIME_MS, " +
-//            "       SSVT.PRES as PRES, " +
-//            "       SSVT.VAL_TP as VAL_TP, " +
-//            "       @aggregateValue:=(COALESCE(SSVT.VAL,0) + COALESCE(SSV.VAL, 0)) as VAL, " +
-//            "       @aggregateCount:=(COALESCE(SSVT.CT,0) + COALESCE(SSV.CT, 0)) as CT " +
-//            "   FROM ( " +
-//            "       SELECT " +
-//            "           ROUND(SSVS.TIME_MS, ?) AS TIME_MS_RND, " +
-//            "           ? as PRES, " +
-//            "           ? as VAL_TP, " +
-//            "           SUM(SSVS.VAL) as VAL, " +
-//            "           SUM(CASE SSVS.VAL_TP WHEN " + StatisticType.COUNT.getPrimitiveValue() + " THEN SSVS.VAL ELSE 1 END) as CT, " +
-//            "           SSK.ID as FK_SQL_STAT_KEY_ID " +
-//            "           FROM SQL_STAT_VAL_SRC SSVS " +
-//            "           JOIN SQL_STAT_KEY SSK ON (SSK.NAME = SSVS.NAME) " +
-//            "           WHERE SSVS.TIME_MS < ? " +
-//            "           AND SSVS.VAL_TP = ? " +
-//            "       AND SSVS.PROCESSING = 1 " +
-//            "       GROUP BY FK_SQL_STAT_KEY_ID, TIME_MS_RND, SSVS.VAL_TP, PRES " +
-//            "       HAVING COUNT(*) > 0 " +
-//            "   ) SSVT " +
-//            "   LEFT JOIN SQL_STAT_VAL SSV ON (" +
-//            "       SSV.FK_SQL_STAT_KEY_ID = SSVT.FK_SQL_STAT_KEY_ID AND " +
-//            "       SSV.TIME_MS = SSVT.TIME_MS_RND AND " +
-//            "       SSV.PRES = SSVT.PRES AND SSV.VAL_TP = SSVT.VAL_TP" +
-//            "   ) " +
-//            ") AGG " +
-//            "WHERE @aggregateCount > 0 " +
-//            "ON DUPLICATE KEY UPDATE " +
-//            "   VAL = @aggregateValue, " +
-//            "   CT = @aggregateCount ";
-//
-//    private static final String STAGE1_UPSERT = "" +
-//            "INSERT INTO SQL_STAT_VAL (FK_SQL_STAT_KEY_ID, TIME_MS, PRES, VAL_TP, VAL, CT) " +
-//            "SELECT " +
-//            "   AGG.FK_SQL_STAT_KEY_ID, " +
-//            "   AGG.TIME_MS, " +
-//            "   AGG.PRES, " +
-//            "   AGG.VAL_TP, " +
-//            "   AGG.VAL, " +
-//            "   AGG.CT " +
-//            "FROM ( " +
-//            "   SELECT " +
-//            "       SSVT.FK_SQL_STAT_KEY_ID as FK_SQL_STAT_KEY_ID, " +
-//            "       SSVT.TIME_MS_RND as TIME_MS, " +
-//            "       SSVT.PRES as PRES, " +
-//            "       SSVT.VAL_TP as VAL_TP, " +
-//            "       @aggregateValue:=(COALESCE(SSVT.VAL,0) + COALESCE(SSV.VAL, 0)) as VAL, " +
-//            "       @aggregateCount:=(COALESCE(SSVT.CT,0) + COALESCE(SSV.CT, 0)) as CT " +
-//            "   FROM ( " +
-//            "       SELECT " +
-//            "           ROUND(SSVS.TIME_MS, ?) AS TIME_MS_RND, " +
-//            "           ? as PRES, " +
-//            "           ? as VAL_TP, " +
-//            "           SUM(SSVS.VAL) as VAL, " +
-//            "           SUM(CASE SSVS.VAL_TP WHEN " + StatisticType.COUNT.getPrimitiveValue() + " THEN SSVS.VAL ELSE 1 END) as CT, " +
-//            "           SSK.ID as FK_SQL_STAT_KEY_ID " +
-//            "           FROM SQL_STAT_VAL_SRC SSVS " +
-//            "           JOIN SQL_STAT_KEY SSK ON (SSK.NAME = SSVS.NAME) " +
-//            "           WHERE SSVS.TIME_MS < ? " +
-//            "           AND SSVS.VAL_TP = ? " +
-//            "       AND SSVS.PROCESSING = 1 " +
-//            "       GROUP BY FK_SQL_STAT_KEY_ID, TIME_MS_RND, SSVS.VAL_TP, PRES " +
-//            "       HAVING COUNT(*) > 0 " +
-//            "   ) SSVT " +
-//            "   LEFT JOIN SQL_STAT_VAL SSV ON (" +
-//            "       SSV.FK_SQL_STAT_KEY_ID = SSVT.FK_SQL_STAT_KEY_ID AND " +
-//            "       SSV.TIME_MS = SSVT.TIME_MS_RND AND " +
-//            "       SSV.PRES = SSVT.PRES AND SSV.VAL_TP = SSVT.VAL_TP" +
-//            "   ) " +
-//            ") AGG " +
-//            "WHERE @aggregateCount > 0 " +
-//            "ON DUPLICATE KEY UPDATE " +
-//            "   VAL = @aggregateValue, " +
-//            "   CT = @aggregateCount ";
     private static final String SPROC_STAGE1_UPSERT = "{call stage1Upsert (?, ?, ?, ?, ?)}";
     private static final String STAGE1_AGGREGATE_DELETE_SOURCE = "" +
             "DELETE FROM SQL_STAT_VAL_SRC " +
@@ -202,92 +115,6 @@ public class SQLStatisticAggregationTransactionHelper {
     // Copy stats from one precision to a coarser precision, aggregating them
     // with any stats
     // in the target precision if there are any
-//    private static final String STAGE2_UPSERT = "" +
-//            "INSERT INTO SQL_STAT_VAL (FK_SQL_STAT_KEY_ID, TIME_MS, PRES, VAL_TP, VAL, CT) " +
-//            "SELECT " +
-//            "   AGG.FK_SQL_STAT_KEY_ID, " +
-//            "   AGG.TIME_MS, " +
-//            "   ? as PRES, " + // target pres
-//            "   ? as VAL_TP, " +
-//            "   AGG.VAL AS VAL, " +
-//            "   AGG.CT AS CT " +
-//            "FROM (" +
-//            "   SELECT " +
-//            "      FK_SQL_STAT_KEY_ID, " +
-//            "      TIME_MS, " +
-//            "      @aggregateValue:=SUM(VAL) AS VAL, " +
-//            "      @aggregateCount:=SUM(CT) AS CT " +
-//            "   FROM ( " +
-//            "      SELECT " + // existing values at correct precision
-//            "         SSVO.TIME_MS, " +
-//            "         SSVO.VAL, " +
-//            "         SSVO.CT, " +
-//            "         SSVO.FK_SQL_STAT_KEY_ID " +
-//            "      FROM SQL_STAT_VAL SSVO " +
-//            "      WHERE SSVO.PRES = ? " + // target pres
-//            "      AND SSVO.VAL_TP = ? " +
-//            "      AND SSVO.TIME_MS <= ROUND(?, ?) " + // only pick up records up to the point we are interested in
-//            "      UNION ALL " +
-//            "      SELECT " +
-//            "         ROUND(SSVN.TIME_MS, ?) AS TIME_MS, " + // target pres, e.g. -9
-//            "         SSVN.VAL AS VAL, " +
-//            "         SSVN.CT AS CT, " +
-//            "         SSVN.FK_SQL_STAT_KEY_ID AS FK_SQL_STAT_KEY_ID " +
-//            "      FROM SQL_STAT_VAL SSVN " +
-//            "      WHERE SSVN.TIME_MS < ? " + // stat age threshold to change precision
-//            "      AND SSVN.PRES = ? " + // old PRES
-//            "      AND SSVN.VAL_TP = ? " +
-//            "      GROUP BY FK_SQL_STAT_KEY_ID, TIME_MS " +
-//            "   ) ROUNDED " +
-//            "   GROUP BY FK_SQL_STAT_KEY_ID, TIME_MS " +
-//            ") AGG " +
-//            "ON DUPLICATE KEY UPDATE " +
-//            "VAL = @aggregateValue, " +
-//            "CT =  @aggregateCount ";
-//
-//    private static final String STAGE2_UPSERT = new StringBuilder()
-//            .append("INSERT INTO SQL_STAT_VAL (FK_SQL_STAT_KEY_ID, TIME_MS, PRES, VAL_TP, VAL, CT) ")
-//            .append("SELECT ")
-//            .append("   AGG.FK_SQL_STAT_KEY_ID,  ")
-//            .append("   AGG.TIME_MS,  ")
-//            .append("   ? as PRES,  ") // target pres
-//            .append("   ? as VAL_TP,  ")
-//            .append("   AGG.VAL AS VAL,  ")
-//            .append("   AGG.CT AS CT ")
-//            .append("FROM ( ")
-//            .append("   SELECT ")
-//            .append("       FK_SQL_STAT_KEY_ID,  ")
-//            .append("       TIME_MS,  ")
-//            .append("       SUM(VAL) AS VAL,  ")
-//            .append("       SUM(CT) AS CT ")
-//            .append("   FROM ( ")
-//            .append("       SELECT ") // existing values at correct precision
-//            .append("           SSVO.TIME_MS, ")
-//            .append("           SSVO.VAL, ")
-//            .append("           SSVO.CT, ")
-//            .append("           SSVO.FK_SQL_STAT_KEY_ID ")
-//            .append("       FROM SQL_STAT_VAL SSVO ")
-//            .append("       WHERE SSVO.PRES = ? ") // target pres
-//            .append("       AND SSVO.VAL_TP = ? ")
-//            .append("       AND SSVO.TIME_MS <= ROUND(?, ?) ") // only pick up records up to the point we are interested in
-//            .append("       UNION ALL ")
-//            .append("       SELECT ")
-//            .append("           ROUND(SSVN.TIME_MS, ?), ") // target pres, e.g. -9
-//            .append("           SSVN.VAL, ")
-//            .append("           SSVN.CT, ")
-//            .append("           SSVN.FK_SQL_STAT_KEY_ID ")
-//            .append("       FROM SQL_STAT_VAL SSVN ")
-//            .append("       WHERE SSVN.TIME_MS < ? ") // stat age threshold to change precision
-//            .append("       AND SSVN.PRES = ? ") // old PRES
-//            .append("       AND SSVN.VAL_TP = ? ")
-//            .append("   ) ROUNDED ")
-//            .append("   GROUP BY ROUNDED.FK_SQL_STAT_KEY_ID, ROUNDED.TIME_MS ")
-//            .append(") AGG ")
-//            .append("ON DUPLICATE KEY UPDATE ")
-//            .append("   VAL = AGG.VAL, ")
-//            .append("   CT = AGG.CT ")
-//            .toString();
-
     private static final String SPROC_STAGE2_UPSERT = "{call stage2Upsert (?, ?, ?, ?, ?, ?)}";
 
     // Delete records from STAT_VAL older than a certain threshold for a given
@@ -329,7 +156,6 @@ public class SQLStatisticAggregationTransactionHelper {
 
             new AggregateConfig(StatisticType.VALUE, MS_DAY, DAY_PRECISION, DEFAULT_PRECISION, "0 * *"),
             new AggregateConfig(StatisticType.VALUE, -1, DEFAULT_PRECISION, DEFAULT_PRECISION, "* * *"),
-
     };
 
     @Inject
@@ -343,32 +169,6 @@ public class SQLStatisticAggregationTransactionHelper {
         final long scale = (long) Math.pow(10, precision);
         return ((timeMs) / scale) * scale;
     }
-
-//    int doUpdate(final Connection connection, final TaskContext taskContext, final String prefix,
-//                 String sql, final List<Object> args) throws SQLException {
-//        final LogExecutionTime time = new LogExecutionTime();
-//        final String trace = buildSQLTrace(sql, args);
-//
-//        taskContext.info("{}\n {}", prefix, trace);
-//
-//        for (final Object arg : args) {
-//            sql = sql.replaceFirst("\\?", String.valueOf(arg));
-//        }
-//
-//
-//        try (final Statement statement = connection.createStatement()) {
-//            statement.execute(sql);
-//        } catch (final SQLException sqlException) {
-//            throw sqlException;
-//        }
-//        return 1;
-//
-////        final int count = ConnectionUtil.executeUpdate(connection, sql, args);
-////
-////        logDebug("doAggretateSQL - {} - {} in {} - {}", prefix, ModelStringUtil.formatCsv(count), time, trace);
-////        return count;
-//    }
-
 
     protected int doAggregateSQL_Update(final Connection connection, final TaskContext taskContext, final String prefix,
                                         final String sql, final List<Object> args) throws SQLException {
@@ -499,12 +299,6 @@ public class SQLStatisticAggregationTransactionHelper {
                             level.getPrecision(),
                             level.getValueType().getPrimitiveValue(),
                             aggregateToMs);
-
-//                    final int rowsAffectedOnUpsert = doAggregateSQL_Update(connection, taskContext, newPrefix,
-//                            STAGE1_UPSERT,
-//                            Arrays.asList(level.getSQLPrecision(), level.getPrecision(),
-//                                    level.getValueType().getPrimitiveValue(), aggregateToMs,
-//                                    level.getValueType().getPrimitiveValue()));
 
                     // delete records from STAT_VAL_SRC up to maxSrcId and below
                     // aggregateToMs,
@@ -641,66 +435,6 @@ public class SQLStatisticAggregationTransactionHelper {
                         // required. Does an update or
                         // insert depending on if the target precision has a
                         // record or not.
-
-
-//                        LOGGER.info("--------BEFORE--------");
-//                        report(connection);
-//
-//
-//                        final String createTempTable = "    CREATE TEMPORARY TABLE TEMP_AGG AS (\n" +
-//                                "        SELECT\n" +
-//                                "            FK_SQL_STAT_KEY_ID,\n" +
-//                                "            TIME_MS,\n" +
-//                                "            SUM(VAL) AS VAL,\n" +
-//                                "            SUM(CT) AS CT\n" +
-//                                "        FROM (\n" +
-//                                "            SELECT \n" +
-//                                "                SSVO.TIME_MS,\n" +
-//                                "                SSVO.VAL,\n" +
-//                                "                SSVO.CT,\n" +
-//                                "                SSVO.FK_SQL_STAT_KEY_ID\n" +
-//                                "            FROM SQL_STAT_VAL SSVO\n" +
-//                                "            WHERE SSVO.PRES = " + targetPrecision + " \n" +
-//                                "            AND SSVO.VAL_TP = " + valueType + "\n" +
-//                                "            AND SSVO.TIME_MS <= ROUND(" + aggregateToMs + ", " + targetSqlPrecision + ") \n" +
-//                                "            UNION ALL\n" +
-//                                "            SELECT\n" +
-//                                "                ROUND(SSVN.TIME_MS, " + targetSqlPrecision + "), \n" +
-//                                "                SSVN.VAL,\n" +
-//                                "                SSVN.CT,\n" +
-//                                "                SSVN.FK_SQL_STAT_KEY_ID\n" +
-//                                "            FROM SQL_STAT_VAL SSVN\n" +
-//                                "            WHERE SSVN.TIME_MS < " + aggregateToMs + "\n" +
-//                                "            AND SSVN.PRES = " + lastPrecision + " \n" +
-//                                "            AND SSVN.VAL_TP = " + valueType + "\n" +
-//                                "        ) ROUNDED\n" +
-//                                "        GROUP BY ROUNDED.FK_SQL_STAT_KEY_ID, ROUNDED.TIME_MS\n" +
-//                                "    );\n";
-//
-//                        final String insert = "    INSERT INTO SQL_STAT_VAL (FK_SQL_STAT_KEY_ID, TIME_MS, PRES, VAL_TP, VAL, CT)\n" +
-//                                "    SELECT\n" +
-//                                "        TEMP_AGG.FK_SQL_STAT_KEY_ID,\n" +
-//                                "        TEMP_AGG.TIME_MS,\n" +
-//                                "        " + targetPrecision + " as PRES, \n" +
-//                                "        " + valueType + " as VAL_TP,\n" +
-//                                "        TEMP_AGG.VAL AS VAL,\n" +
-//                                "        TEMP_AGG.CT AS CT\n" +
-//                                "    FROM TEMP_AGG\n" +
-//                                "    ON DUPLICATE KEY UPDATE\n" +
-//                                "        VAL = TEMP_AGG.VAL,\n" +
-//                                "        CT = TEMP_AGG.CT;\n";
-//
-//                        final String dropTempTable = "    DROP TEMPORARY TABLE TEMP_AGG;";
-
-
-//                        int upsertCount;
-//                        ConnectionUtil.executeStatement(connection, createTempTable);
-//                        try (final Statement statement = connection.createStatement()) {
-//                            upsertCount = statement.executeUpdate(insert);
-//                        }
-//                        ConnectionUtil.executeStatement(connection, dropTempTable);
-
-
                         final int upsertCount = callUpsert2(connection,
                                 taskContext,
                                 newPrefix,
@@ -709,15 +443,6 @@ public class SQLStatisticAggregationTransactionHelper {
                                 lastPrecision,
                                 valueType,
                                 aggregateToMs);
-
-
-//                        final int upsertCount = doAggregateSQL_Update(connection, taskContext, newPrefix, STAGE2_UPSERT,
-//                                Arrays.asList(targetPrecision, valueType, targetPrecision, valueType,
-//                                        aggregateToMs, targetSqlPrecision, targetSqlPrecision, aggregateToMs,
-//                                        lastPrecision, valueType));
-
-//                        LOGGER.info("--------AFTER--------");
-//                        report(connection);
 
                         if (upsertCount > 0) {
                             // now delete the old stats that we just copied up
@@ -733,34 +458,6 @@ public class SQLStatisticAggregationTransactionHelper {
             }
         }
     }
-
-//    private void report(final Connection connection) {
-//        try {
-//            final StringBuilder sb = new StringBuilder();
-//            try (final Statement statement = connection.createStatement()) {
-//                try (final ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM SQL_STAT_VAL")) {
-//                    if (resultSet.next()) {
-//                        sb.append("SQL_STAT_VAL RECORD COUNT = " + resultSet.getInt(1) + "\n");
-//                    }
-//                }
-//            }
-//            try (final Statement statement = connection.createStatement()) {
-//                try (final ResultSet resultSet = statement.executeQuery("SELECT TIME_MS, PRES, VAL_TP, VAL, CT, FK_SQL_STAT_KEY_ID FROM SQL_STAT_VAL")) {
-//                    if (resultSet.next()) {
-//                        sb.append(resultSet.getLong(1) + "|" +
-//                                resultSet.getByte(2) + "|" +
-//                                resultSet.getByte(3) + "|" +
-//                                resultSet.getLong(4) + "|" +
-//                                resultSet.getLong(5) + "|" +
-//                                resultSet.getLong(6) + "\n");
-//                    }
-//                }
-//            }
-//            LOGGER.info("\n" + sb.toString());
-//        } catch (final SQLException e) {
-//            LOGGER.error(e.getMessage(), e);
-//        }
-//    }
 
     private Long getStatsRetentionAgeMs() {
         final String propVal = config.getMaxProcessingAge();
