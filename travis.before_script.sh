afd017b7ac2752aff391c7f63847bbfa0190dda3
at055612 <at055612@users.noreply.github.com>
2019-05-17 11:11:16 +0100

    Remove popd call that should have gone in previous commit

--- travis.before_script.sh
+++ travis.before_script.sh
@@ -45,7 +45,7 @@ else
 
     echo -e "${GREEN}Start all the services we need to run the integration tests in stroom${NC}"
     ./bounceIt.sh 'up -d --build' -d -e -y -x kafka stroom-all-dbs zookeeper
-    popd
+
     popd
 
     echo -e "${GREEN}Configure our plugins directory so stroom can find the kafka jar${NC}"
