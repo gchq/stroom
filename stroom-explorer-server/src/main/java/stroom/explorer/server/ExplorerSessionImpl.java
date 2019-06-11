28a24c032d71e04cef4841084472a18900785722
stroomdev66 <stroomdev66@gmail.com>
2019-06-11 11:03:32 +0100

    #1153 The explorer tree is now built with fewer DB queries

--- stroom-explorer-server/src/main/java/stroom/explorer/server/ExplorerSessionImpl.java
+++ stroom-explorer-server/src/main/java/stroom/explorer/server/ExplorerSessionImpl.java
@@ -2,35 +2,26 @@ package stroom.explorer.server;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.stereotype.Component;
 import stroom.servlet.HttpServletRequestHolder;
 
-import javax.inject.Inject;
-import javax.inject.Provider;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Component
-class ExplorerSessionImpl implements ExplorerSession {
+class ExplorerSessionImpl implements ExplorerSession, BeanFactoryAware {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExplorerSessionImpl.class);
     private static final String MIN_EXPLORER_TREE_MODEL_BUILD_TIME = "MIN_EXPLORER_TREE_MODEL_BUILD_TIME";
 
-    private final Provider<HttpServletRequestHolder> httpServletRequestHolderProvider;
-
-    @Inject
-    ExplorerSessionImpl(final Provider<HttpServletRequestHolder> httpServletRequestHolderProvider) {
-        this.httpServletRequestHolderProvider = httpServletRequestHolderProvider;
-    }
+    private BeanFactory beanFactory;
 
     @Override
     public Optional<Long> getMinExplorerTreeModelBuildTime() {
-        final HttpServletRequestHolder httpServletRequestHolder = httpServletRequestHolderProvider.get();
-        if (httpServletRequestHolder == null) {
-            return Optional.empty();
-        }
-
-        final HttpServletRequest request = httpServletRequestHolder.get();
+        final HttpServletRequest request = getRequest();
         if (request == null) {
             LOGGER.debug("Request holder has no current request");
             return Optional.empty();
@@ -43,16 +34,32 @@ class ExplorerSessionImpl implements ExplorerSession {
 
     @Override
     public void setMinExplorerTreeModelBuildTime(final long buildTime) {
-        final HttpServletRequestHolder httpServletRequestHolder = httpServletRequestHolderProvider.get();
-        if (httpServletRequestHolder != null) {
-            final HttpServletRequest request = httpServletRequestHolder.get();
-            if (request == null) {
-                LOGGER.debug("Request holder has no current request");
+        final HttpServletRequest request = getRequest();
+        if (request == null) {
+            LOGGER.debug("Request holder has no current request");
 
-            } else {
-                final HttpSession session = request.getSession();
-                session.setAttribute(MIN_EXPLORER_TREE_MODEL_BUILD_TIME, buildTime);
+        } else {
+            final HttpSession session = request.getSession();
+            session.setAttribute(MIN_EXPLORER_TREE_MODEL_BUILD_TIME, buildTime);
+        }
+    }
+
+    private HttpServletRequest getRequest() {
+        if (beanFactory != null) {
+            try {
+                final HttpServletRequestHolder holder = beanFactory.getBean(HttpServletRequestHolder.class);
+                if (holder != null) {
+                    return holder.get();
+                }
+            } catch (final RuntimeException e) {
+                // Ignore.
             }
         }
+        return null;
+    }
+
+    @Override
+    public void setBeanFactory(final BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
     }
 }
