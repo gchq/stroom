11369b4dc7439b0d9e32671b755b4cebcc01c9c0
at055612 <at055612@gmail.com>
2019-07-10 18:33:39 +0100

    Add kafka 2.1.1 client and uplift hbase to 2.1.5 (WIP)

---
+++ stroom-kafka-client-impl_2_1_1/src/main/java/stroom/connectors/kafka/StroomKafkaProducerFactoryImpl.java
@@ -0,0 +1,21 @@
+package stroom.connectors.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import stroom.connectors.ConnectorProperties;
+
+public class StroomKafkaProducerFactoryImpl implements StroomKafkaProducerFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StroomKafkaProducerFactoryImpl.class);
+    private static final String VERSION = "2.1.1";
+
+    @Override
+    public StroomKafkaProducer createConnector(final String version, final ConnectorProperties properties) {
+
+        if (VERSION.equals(version)) {
+            return new StroomKafkaProducerImpl(properties);
+        } else {
+            LOGGER.debug("Requested version [{}] doesn't match my version [{}]", version, VERSION);
+        }
+        return null;
+    }
+}
