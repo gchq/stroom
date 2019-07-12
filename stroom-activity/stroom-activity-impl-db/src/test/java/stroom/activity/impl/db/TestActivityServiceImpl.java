d32e89b35c497ebf1d964defc6f6691cc61838c6
stroomdev66 <stroomdev66@gmail.com>
2019-07-12 10:21:45 +0100

    Merge branch '6.0' of github.com:gchq/stroom
    # Conflicts:
    #	CHANGELOG.md
    #	stroom-core-client/src/main/java/stroom/activity/client/ActivityEditPresenter.java
    #	stroom-core-server/src/main/java/stroom/activity/server/ActivityService.java
    #	stroom-core-server/src/main/java/stroom/activity/server/ActivityServiceImpl.java
    #	stroom-core-server/src/main/java/stroom/entity/server/DocumentEntityServiceImpl.java
    #	stroom-core-server/src/main/java/stroom/node/server/DefaultProperties.java
    #	stroom-core-server/src/main/java/stroom/task/server/ExecutorProviderImpl.java
    #	stroom-core-shared/src/main/java/stroom/task/shared/FindTaskProgressCriteria.java
    #	stroom-core/src/main/java/stroom/activity/server/ValidateActivityHandler.java
    #	stroom-index-server/src/main/java/stroom/index/server/IndexServiceImpl.java
    #	stroom-integrationtest/src/integrationTest/java/stroom/activity/server/TestActivityServiceImpl.java
    #	stroom-statistics-server/src/main/java/stroom/statistics/server/sql/search/SqlStatisticStoreFactory.java
    #	stroom-statistics-server/src/main/java/stroom/statistics/server/sql/search/SqlStatisticsStore.java
    #	stroom-statistics-server/src/main/java/stroom/statistics/server/sql/search/StatisticsSearchServiceImpl.java

--- stroom-activity/stroom-activity-impl-db/src/test/java/stroom/activity/impl/db/TestActivityServiceImpl.java
+++ stroom-activity/stroom-activity-impl-db/src/test/java/stroom/activity/impl/db/TestActivityServiceImpl.java
@@ -24,6 +24,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import stroom.activity.api.ActivityService;
 import stroom.activity.impl.ActivityDao;
 import stroom.activity.impl.ActivityServiceImpl;
@@ -39,6 +41,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class TestActivityServiceImpl {
     private ActivityService activityService;
 
