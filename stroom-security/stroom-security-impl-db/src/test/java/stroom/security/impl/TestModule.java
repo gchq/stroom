0a2b9eec7186d1207c9b96e76b72279339bb87de
stroomdev66 <stroomdev66@gmail.com>
2019-08-21 17:59:45 +0100

    Merge branch 'master' of github.com:gchq/stroom into SimplifySecurity
    # Conflicts:
    #	stroom-index/stroom-index-app/src/test/java/stroom/index/impl/TestIndexShardPoolImpl2.java
    #	stroom-index/stroom-index-impl/src/main/java/stroom/index/impl/service/IndexVolumeGroupServiceImpl.java

--- stroom-security/stroom-security-impl-db/src/test/java/stroom/security/impl/TestModule.java
+++ stroom-security/stroom-security-impl-db/src/test/java/stroom/security/impl/TestModule.java
@@ -1,15 +1,13 @@
 package stroom.security.impl;
 
 import com.google.inject.AbstractModule;
-import org.mockito.Mockito;
 import stroom.cache.impl.CacheModule;
 import stroom.entity.shared.EntityEventBus;
 import stroom.explorer.api.ExplorerService;
-import stroom.security.api.SecurityContext;
 import stroom.security.impl.db.SecurityDbModule;
+import stroom.security.mock.MockSecurityContextModule;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class TestModule extends AbstractModule {
     @Override
@@ -17,17 +15,10 @@ public class TestModule extends AbstractModule {
         // We want the 'real' security DB Module
         install(new CacheModule());
         install(new SecurityDbModule());
+        install(new MockSecurityContextModule());
 
         bind(UserService.class).to(UserServiceImpl.class);
         bind(UserAppPermissionService.class).to(UserAppPermissionServiceImpl.class);
-
-        final SecurityContext securityContext = mock(SecurityContext.class);
-        when(securityContext.getUserId()).thenReturn("admin");
-        when(securityContext.isLoggedIn()).thenReturn(true);
-        when(securityContext.hasAppPermission(Mockito.any())).thenReturn(true);
-        when(securityContext.hasDocumentPermission(Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(true);
-        bind(SecurityContext.class).toInstance(securityContext);
-
         bind(ExplorerService.class).toInstance(mock(ExplorerService.class));
         bind(EntityEventBus.class).toInstance(mock(EntityEventBus.class));
     }
