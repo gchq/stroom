eb2b06250a6b5bdc5b0cebff33ed507fd4a63e6a
at055612 <at055612@users.noreply.github.com>
2019-10-14 10:54:51 +0100

    Add USE_TEST_CONTAINERS env var to DbTestUtil

--- stroom-test-common/src/main/java/stroom/test/common/util/db/DbTestUtil.java
+++ stroom-test-common/src/main/java/stroom/test/common/util/db/DbTestUtil.java
@@ -28,6 +28,14 @@ public class DbTestUtil {
 
     private static final String TESTCONTAINERS_JDBC_CONTAINER_DATABASE_DRIVER = "org.testcontainers.jdbc.ContainerDatabaseDriver";
 
+    private static final String USE_TEST_CONTAINERS_ENV_VAR = "USE_TEST_CONTAINERS";
+    public static final String TEST_CONTAINERS_DB_VERSION = "5.5.52";
+//    public static final String TEST_CONTAINERS_DB_VERSION = "5.6.43";
+//    public static final String TEST_CONTAINERS_DB_VERSION = "5.7.25";
+//    public static final String TEST_CONTAINERS_DB_VERSION = "8.0.15";
+    public static final String TEST_CONTAINERS_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    public static final String TEST_CONTAINERS_DRIVER_URL = "jdbc:tc:mysql:" + TEST_CONTAINERS_DB_VERSION + "://localhost:3306/test";
+
     private DbTestUtil() {
     }
 
@@ -60,23 +68,32 @@ public class DbTestUtil {
     }
 
     public static void applyTestContainersConfig(final ConnectionConfig connectionConfig) {
-        try {
-            final Class<?> clazz = Class.forName(TESTCONTAINERS_JDBC_CONTAINER_DATABASE_DRIVER);
-            if (clazz != null) {
-                LOGGER.info("Using test container DB connection config");
+        String useTestContainersEnvVarVal = System.getenv(USE_TEST_CONTAINERS_ENV_VAR);
+        // This env var allows a dev to use their own stroom-resources DB instead of test containers.
+        // This is useful when debugging a test that needs to access the DB as test containers has to
+        // migrate the DB from scratch on each run which is very time consuming
+        if (useTestContainersEnvVarVal == null || !useTestContainersEnvVarVal.toLowerCase().equals("false")) {
+            try {
+                final Class<?> clazz = Class.forName(TESTCONTAINERS_JDBC_CONTAINER_DATABASE_DRIVER);
+                if (clazz != null) {
+                    LOGGER.info("Using Test Containers v{} DB connection config", TEST_CONTAINERS_DB_VERSION);
 
-                connectionConfig.setJdbcDriverClassName("com.mysql.cj.jdbc.Driver");
-                connectionConfig.setJdbcDriverUrl("jdbc:tc:mysql:5.5.52://localhost:3306/test");
+                    connectionConfig.setJdbcDriverClassName(TEST_CONTAINERS_CLASS_NAME);
+                    connectionConfig.setJdbcDriverUrl(TEST_CONTAINERS_DRIVER_URL);
 //                    connectionConfig.setJdbcDriverUrl("jdbc:tc:mysql:5.6.43://localhost:3306/test");
 //                    connectionConfig.setJdbcDriverUrl("jdbc:tc:mysql:5.7.25://localhost:3306/test");
 //                    connectionConfig.setJdbcDriverUrl("jdbc:tc:mysql:8.0.15://localhost:3306/test");
-                connectionConfig.setJdbcDriverPassword("test");
-                connectionConfig.setJdbcDriverUsername("test");
+                    connectionConfig.setJdbcDriverPassword("test");
+                    connectionConfig.setJdbcDriverUsername("test");
 
+                }
+            } catch (final ClassNotFoundException e) {
+                LOGGER.debug("Can't find class {} for Test Containers, falling back to standard DB connection",
+                        TESTCONTAINERS_JDBC_CONTAINER_DATABASE_DRIVER);
             }
-        } catch (final ClassNotFoundException e) {
-            LOGGER.debug("Can't find class {} for Test Containers, falling back to standard DB connection",
-                    TESTCONTAINERS_JDBC_CONTAINER_DATABASE_DRIVER);
+        } else {
+            LOGGER.info("{}={} so using standard DB connection",
+                    USE_TEST_CONTAINERS_ENV_VAR, useTestContainersEnvVarVal);
         }
     }
 
