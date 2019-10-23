f748e9e735921aaf5e3f00908ce65081e559b55d
at055612 <at055612@gmail.com>
2019-10-23 11:15:15 +0100

    Change travis gwt mem settings

--- travis.script.sh
+++ travis.script.sh
@@ -238,14 +238,16 @@ else
     echo -e "extraBuildArgs:                [${GREEN}${extraBuildArgs[*]}${NC}]"
 
     # Do the gradle build
-    # Use 1 local worker to avoid using too much memory as each worker will
-    # chew up ~500Mb ram
+    # Use custom gwt compile jvm settings to avoid blowing the ram limit in 
+    # travis. At time of writing a sudo VM in travis has 7.5gb ram.
+    # Each work will chew up the maxHeap value and we have to allow for
+    # our docker services as well.
     ./gradlew \
       -PdumpFailedTestXml=true \
       -Pversion="${TRAVIS_TAG}" \
-      -PgwtCompilerWorkers=1 \
+      -PgwtCompilerWorkers=2 \
       -PgwtCompilerMinHeap=50M \
-      -PgwtCompilerMaxHeap=500M \
+      -PgwtCompilerMaxHeap=750M \
       clean \
       build \
       buildDistribution \
