faffc6d5be4ec84decbd33a86c8212a9d97a8bb9
at055612 <at055612@gmail.com>
2019-10-23 12:00:04 +0100

    Increase gwt compile travis mem settings

--- travis.script.sh
+++ travis.script.sh
@@ -247,7 +247,7 @@ else
       -Pversion="${TRAVIS_TAG}" \
       -PgwtCompilerWorkers=2 \
       -PgwtCompilerMinHeap=50M \
-      -PgwtCompilerMaxHeap=750M \
+      -PgwtCompilerMaxHeap=1000M \
       clean \
       build \
       buildDistribution \
