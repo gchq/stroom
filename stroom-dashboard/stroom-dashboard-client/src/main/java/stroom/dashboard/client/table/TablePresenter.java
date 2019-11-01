37a5635217e9ceb52def97912bd8ebd9583a2e20
stroomdev66 <stroomdev66@gmail.com>
2019-11-01 15:11:18 +0000

    Merge branch '6.1' of github.com:gchq/stroom

--- stroom-dashboard/stroom-dashboard-client/src/main/java/stroom/dashboard/client/table/TablePresenter.java
+++ stroom-dashboard/stroom-dashboard-client/src/main/java/stroom/dashboard/client/table/TablePresenter.java
@@ -125,6 +125,7 @@ public class TablePresenter extends AbstractComponentPresenter<TableView>
                           final ClientSecurityContext securityContext,
                           final LocationManager locationManager,
                           final MenuListPresenter menuListPresenter,
+                          final Provider<RenameFieldPresenter> renameFieldPresenterProvider,
                           final Provider<ExpressionPresenter> expressionPresenterProvider,
                           final FormatPresenter formatPresenter,
                           final FilterPresenter filterPresenter,
@@ -152,7 +153,12 @@ public class TablePresenter extends AbstractComponentPresenter<TableView>
         downloadButton = dataGrid.addButton(SvgPresets.DOWNLOAD);
         downloadButton.setVisible(securityContext.hasAppPermission(PermissionNames.DOWNLOAD_SEARCH_RESULTS_PERMISSION));
 
-        fieldsManager = new FieldsManager(this, menuListPresenter, expressionPresenterProvider, formatPresenter,
+        fieldsManager = new FieldsManager(
+                this,
+                menuListPresenter,
+                renameFieldPresenterProvider,
+                expressionPresenterProvider,
+                formatPresenter,
                 filterPresenter);
         dataGrid.setHeadingListener(fieldsManager);
 
@@ -176,8 +182,8 @@ public class TablePresenter extends AbstractComponentPresenter<TableView>
         super.onBind();
         registerHandler(dataGrid.getSelectionModel().addSelectionHandler(event -> performRowAction(dataGrid.getSelectionModel().getSelected())));
         registerHandler(dataGrid.addRangeChangeHandler(event -> {
-                final com.google.gwt.view.client.Range range = event.getNewRange();
-                tableResultRequest.setRange(range.getStart(), range.getLength());
+            final com.google.gwt.view.client.Range range = event.getNewRange();
+            tableResultRequest.setRange(range.getStart(), range.getLength());
             if (!ignoreRangeChange) {
                 refresh();
             }
@@ -455,6 +461,10 @@ public class TablePresenter extends AbstractComponentPresenter<TableView>
 
         dataGrid.addResizableColumn(column, fieldHeader, field.getWidth());
         existingColumns.add(column);
+    }
+
+    public void redrawHeaders() {
+        dataGrid.redrawHeaders();
     }
 
     private void performRowAction(final Row result) {
