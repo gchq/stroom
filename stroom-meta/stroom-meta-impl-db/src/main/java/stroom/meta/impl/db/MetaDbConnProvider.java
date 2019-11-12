c82668bf04a9f004ab6c016d97506fd377550345
stroomdev66 <stroomdev66@gmail.com>
2019-11-12 19:33:25 +0000

    Merge branch 'master' of github.com:gchq/stroom into merge_annotations

--- stroom-meta/stroom-meta-impl-db/src/main/java/stroom/meta/impl/db/MetaDbConnProvider.java
+++ stroom-meta/stroom-meta-impl-db/src/main/java/stroom/meta/impl/db/MetaDbConnProvider.java
@@ -3,7 +3,7 @@ package stroom.meta.impl.db;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-class MetaDbConnProvider extends HikariDataSource {
+public class MetaDbConnProvider extends HikariDataSource {
     MetaDbConnProvider(final HikariConfig configuration) {
         super(configuration);
     }
