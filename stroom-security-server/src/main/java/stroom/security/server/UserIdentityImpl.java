61d4e4927cbe1850af93ee7b0a22bef0924d0a3e
stroomdev66 <stroomdev66@gmail.com>
2020-01-13 13:23:01 +0000

    #1352 Fixed Hessian serialisation of user identities on tasks
    (cherry picked from commit a2d24c362d40dd87544a03cd315a8023ac15be0e)

--- stroom-security-server/src/main/java/stroom/security/server/UserIdentityImpl.java
+++ stroom-security-server/src/main/java/stroom/security/server/UserIdentityImpl.java
@@ -3,6 +3,8 @@ package stroom.security.server;
 import stroom.security.shared.UserIdentity;
 import stroom.security.shared.UserRef;
 
+import java.util.Objects;
+
 class UserIdentityImpl implements UserIdentity {
     private final UserRef userRef;
     private final String name;
@@ -33,6 +35,22 @@ class UserIdentityImpl implements UserIdentity {
     @Override
     public String getSessionId() {
         return sessionId;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserIdentityImpl)) return false;
+        final UserIdentityImpl that = (UserIdentityImpl) o;
+        return Objects.equals(userRef, that.userRef) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(jws, that.jws) &&
+                Objects.equals(sessionId, that.sessionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userRef, name, jws, sessionId);
     }
 
     @Override
