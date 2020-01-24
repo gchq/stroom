bd2d04cb8e3ba20720d9f365caa97521121c3cc1
gcdev373 <gcdev373@users.noreply.github.com>
2020-01-24 15:44:23 +0000

    Issue GH-1363

--- stroom-app/src/main/java/stroom/app/guice/CoreModule.java
+++ stroom-app/src/main/java/stroom/app/guice/CoreModule.java
@@ -10,6 +10,7 @@ public class CoreModule extends AbstractModule {
     protected void configure() {
         install(new stroom.activity.impl.db.ActivityDbModule());
         install(new stroom.annotation.impl.db.AnnotationDbModule());
+        install(new stroom.annotation.pipeline.AnnotationPipelineModule());
         install(new stroom.cache.impl.CacheHandlerModule());
         install(new stroom.cache.impl.CacheModule());
         install(new stroom.cluster.lock.impl.db.ClusterLockDbModule());
