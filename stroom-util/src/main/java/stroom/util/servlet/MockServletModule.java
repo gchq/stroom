5165d94a0c141ab4be8592ef9b9cc5da0f3faec5
stroomdev66 <stroomdev66@gmail.com>
2020-01-27 15:07:55 +0000

    Fix test

--- stroom-util/src/main/java/stroom/util/servlet/MockServletModule.java
+++ stroom-util/src/main/java/stroom/util/servlet/MockServletModule.java
@@ -8,7 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 public class MockServletModule extends AbstractModule {
     @Override
     protected void configure() {
+        final SessionIdProvider sessionIdProvider = () -> "mockSessionId";
         bind(HttpServletRequest.class).toProvider(HttpServletRequestHolder.class);
-        bind(SessionIdProvider.class).toProvider(Providers.of(null));
+        bind(SessionIdProvider.class).toInstance(sessionIdProvider);
     }
 }
