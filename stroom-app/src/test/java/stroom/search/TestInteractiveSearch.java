46a29e733e2ae9980d1320621f19cf22bbfa2584
stroomdev66 <stroomdev66@gmail.com>
2020-01-29 23:25:37 +0000

    Temporarily disable test

--- stroom-app/src/test/java/stroom/search/TestInteractiveSearch.java
+++ stroom-app/src/test/java/stroom/search/TestInteractiveSearch.java
@@ -17,8 +17,8 @@
 
 package stroom.search;
 
-import net.sf.saxon.lib.Logger;
 import org.apache.hadoop.util.ThreadUtil;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import stroom.annotation.api.AnnotationDataSource;
 import stroom.dictionary.impl.DictionaryStore;
@@ -52,6 +52,7 @@ import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Disabled
 class TestInteractiveSearch extends AbstractSearchTest {
     @Inject
     private CommonIndexingTestHelper commonIndexingTestHelper;
