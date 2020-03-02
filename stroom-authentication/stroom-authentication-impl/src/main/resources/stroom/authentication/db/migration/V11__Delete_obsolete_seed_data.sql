1f9c64cc10b05cb90adb4916938f782fa6af395d
jc064522 <jc064522@gmail.com>
2020-03-02 10:09:42 +0000

    auth-into-stroom: added cleaning up api keys

--- stroom-authentication/stroom-authentication-impl/src/main/resources/stroom/authentication/db/migration/V11__Delete_obsolete_seed_data.sql
+++ stroom-authentication/stroom-authentication-impl/src/main/resources/stroom/authentication/db/migration/V11__Delete_obsolete_seed_data.sql
@@ -8,3 +8,6 @@ DELETE FROM users WHERE email='statsServiceUser';
 
 DELETE FROM tokens WHERE user_id=(SELECT id FROM users WHERE email="authenticationResourceUser");
 DELETE FROM users WHERE email='authenticationResourceUser';
+
+-- We don't need admin's key any more, but we do need the user.
+DELETE FROM tokens WHERE user_id=(SELECT id FROM users WHERE email="admin");
