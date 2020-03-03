288d1fc7b714c0da5199160f89ac92ff80bb841b
gcdev373 <gcdev373@users.noreply.github.com>
2020-03-03 18:16:40 +0000

    Change presenter

--- stroom-core-client/src/main/java/stroom/kafkaConfig/client/presenter/KafkaConfigPresenter.java
+++ stroom-core-client/src/main/java/stroom/kafkaConfig/client/presenter/KafkaConfigPresenter.java
@@ -17,39 +17,57 @@
 
 package stroom.kafkaConfig.client.presenter;
 
+import com.google.gwt.core.client.GWT;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import edu.ycp.cs.dh.acegwt.client.ace.AceEditorMode;
+import stroom.core.client.LocationManager;
+import stroom.dispatch.client.ExportFileCompleteUtil;
+import stroom.dispatch.client.Rest;
+import stroom.dispatch.client.RestFactory;
 import stroom.docref.DocRef;
 import stroom.editor.client.presenter.EditorPresenter;
 import stroom.entity.client.presenter.ContentCallback;
 import stroom.entity.client.presenter.DocumentEditTabPresenter;
 import stroom.entity.client.presenter.LinkTabPanelView;
 import stroom.kafkaConfig.shared.KafkaConfigDoc;
+import stroom.kafkaConfig.shared.KafkaConfigResource;
 import stroom.security.client.api.ClientSecurityContext;
+import stroom.svg.client.SvgPresets;
+import stroom.util.shared.ResourceGeneration;
+import stroom.widget.button.client.ButtonView;
 import stroom.widget.tab.client.presenter.TabData;
 import stroom.widget.tab.client.presenter.TabDataImpl;
 
 import javax.inject.Provider;
 
 public class KafkaConfigPresenter extends DocumentEditTabPresenter<LinkTabPanelView, KafkaConfigDoc> {
+    private static final KafkaConfigResource KAFKA_CONFIG_RESOURCE = GWT.create(KafkaConfigResource.class);
     private static final TabData SETTINGS_TAB = new TabDataImpl("Settings");
     private static final TabData CONFIG_TAB = new TabDataImpl("Config");
 
     private final KafkaConfigSettingsPresenter settingsPresenter;
     private final Provider<EditorPresenter> editorPresenterProvider;
 
+    private final ButtonView downloadButton;
+    private final RestFactory restFactory;
+    private final LocationManager locationManager;
+
     private EditorPresenter editorPresenter;
     private boolean readOnly = true;
 
-    private int loadCount;
+    private DocRef docRef;
 
     @Inject
     public KafkaConfigPresenter(final EventBus eventBus, final LinkTabPanelView view,
-                           final KafkaConfigSettingsPresenter settingsPresenter, final ClientSecurityContext securityContext, final Provider<EditorPresenter> editorPresenterProvider) {
+                           final KafkaConfigSettingsPresenter settingsPresenter, final ClientSecurityContext securityContext, final Provider<EditorPresenter> editorPresenterProvider,
+                                final RestFactory restFactory,
+                                final LocationManager locationManager) {
         super(eventBus, view, securityContext);
         this.settingsPresenter = settingsPresenter;
         this.editorPresenterProvider = editorPresenterProvider;
+        this.restFactory = restFactory;
+        this.locationManager = locationManager;
 
         settingsPresenter.addDirtyHandler(event -> {
             if (event.isDirty()) {
@@ -57,10 +75,26 @@ public class KafkaConfigPresenter extends DocumentEditTabPresenter<LinkTabPanelV
             }
         });
 
+        downloadButton = addButtonLeft(SvgPresets.DOWNLOAD);
+
         addTab(CONFIG_TAB);
         addTab(SETTINGS_TAB);
         selectTab(CONFIG_TAB);
     }
+
+    @Override
+    protected void onBind() {
+        super.onBind();
+
+        registerHandler(downloadButton.addClickHandler(clickEvent -> {
+            final Rest<ResourceGeneration> rest = restFactory.create();
+            rest
+                    .onSuccess(result -> ExportFileCompleteUtil.onSuccess(locationManager, null, result))
+                    .call(KAFKA_CONFIG_RESOURCE)
+                    .download(docRef);
+        }));
+    }
+
 
     @Override
     protected void getContent(final TabData tab, final ContentCallback callback) {
@@ -76,7 +110,9 @@ public class KafkaConfigPresenter extends DocumentEditTabPresenter<LinkTabPanelV
     @Override
     public void onRead(final DocRef docRef, final KafkaConfigDoc doc) {
         super.onRead(docRef, doc);
-        loadCount++;
+        this.docRef = docRef;
+        downloadButton.setEnabled(true);
+
         settingsPresenter.read(docRef, doc);
 
         if (editorPresenter != null && doc.getData() != null) {
