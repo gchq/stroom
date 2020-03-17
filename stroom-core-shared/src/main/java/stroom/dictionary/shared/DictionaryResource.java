36a159b6aef375f3c38986699a825926333ceafd
jc064522 <jc064522@gmail.com>
2020-03-17 14:59:38 +0000

    Merge resources

--- stroom-core-shared/src/main/java/stroom/dictionary/shared/DictionaryResource.java
+++ stroom-core-shared/src/main/java/stroom/dictionary/shared/DictionaryResource.java
@@ -18,23 +18,33 @@ package stroom.dictionary.shared;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.fusesource.restygwt.client.DirectRestService;
 import stroom.docref.DocRef;
+import stroom.importexport.shared.Base64EncodedDocumentData;
 import stroom.util.shared.ResourceGeneration;
 import stroom.util.shared.ResourcePaths;
 import stroom.util.shared.RestResource;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Set;
 
 @Api(value = "dictionary - /v1")
 @Path("/dictionary" + ResourcePaths.V1)
 @Produces(MediaType.APPLICATION_JSON)
 public interface DictionaryResource extends RestResource, DirectRestService {
+    ///////////////////////
+    // GWT UI end points //
+    ///////////////////////
+
     @POST
     @Path("/read")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -61,4 +71,48 @@ public interface DictionaryResource extends RestResource, DirectRestService {
             value = "Download a dictionary doc",
             response = ResourceGeneration.class)
     ResourceGeneration download(DocRef dictionaryRef);
+
+
+
+    ////////////////////////
+    // React UI endpoints //
+    ////////////////////////
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @javax.ws.rs.Path("/list")
+    @ApiOperation(
+            value = "Submit a request for a list of doc refs held by this service",
+            response = Set.class)
+    Set<DocRef> listDocuments();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @javax.ws.rs.Path("/import")
+    @ApiOperation(
+            value = "Submit an import request",
+            response = DocRef.class)
+    DocRef importDocument(@ApiParam("DocumentData") final Base64EncodedDocumentData encodedDocumentData);
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @javax.ws.rs.Path("/export")
+    @ApiOperation(
+            value = "Submit an export request",
+            response = Base64EncodedDocumentData.class)
+    Base64EncodedDocumentData exportDocument(@ApiParam("DocRef") final DocRef docRef);
+
+    @GET
+    @javax.ws.rs.Path("/{dictionaryUuid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    Response fetch(@PathParam("dictionaryUuid") final String dictionaryUuid);
+
+    @POST
+    @javax.ws.rs.Path("/{dictionaryUuid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    Response save(@PathParam("dictionaryUuid") final String dictionaryUuid,
+                         final DictionaryDTO updates);
 }
