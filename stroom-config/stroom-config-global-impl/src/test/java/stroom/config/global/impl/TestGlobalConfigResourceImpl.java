bb6ee4654d8de727d3dcbe0690197e4765b08bfe
at055612 <at055612@users.noreply.github.com>
2020-03-17 13:57:30 +0000

    Remove SecurityContext from GlobalConfigResourceImpl

---
+++ stroom-config/stroom-config-global-impl/src/test/java/stroom/config/global/impl/TestGlobalConfigResourceImpl.java
@@ -0,0 +1,40 @@
+package stroom.config.global.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestGlobalConfigResourceImpl {
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void listByNode() {
+    }
+
+    @Test
+    void getPropertyByName() {
+    }
+
+    @Test
+    void getYamlValueByName() {
+    }
+
+    @Test
+    void getYamlValueByNodeAndName() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void fetchUiConfig() {
+    }
+}
