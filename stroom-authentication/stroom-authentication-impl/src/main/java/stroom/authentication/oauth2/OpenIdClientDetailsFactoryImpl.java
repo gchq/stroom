340fcc0797f40b5fd197f46c076c5f8040d95e57
at055612 <at055612@users.noreply.github.com>
2020-04-22 11:49:31 +0100

    gh-1465 Format code, add suffix to client id

--- stroom-authentication/stroom-authentication-impl/src/main/java/stroom/authentication/oauth2/OpenIdClientDetailsFactoryImpl.java
+++ stroom-authentication/stroom-authentication-impl/src/main/java/stroom/authentication/oauth2/OpenIdClientDetailsFactoryImpl.java
@@ -1,5 +1,6 @@
 package stroom.authentication.oauth2;
 
+import org.apache.commons.codec.binary.Hex;
 import stroom.security.impl.OpenIdClientDetailsFactory;
 
 import javax.inject.Inject;
@@ -17,7 +18,12 @@ public class OpenIdClientDetailsFactoryImpl implements OpenIdClientDetailsFactor
     public OpenIdClientDetailsFactoryImpl(final OAuth2ClientDao dao) {
         Optional<OAuth2Client> optionalClient = dao.getClientByName(INTERNAL_STROOM_CLIENT);
         if (optionalClient.isEmpty()) {
-            final OAuth2Client client = new OAuth2Client(INTERNAL_STROOM_CLIENT, createRandomCode(40), createRandomCode(20), ".*");
+            // Generate new randomised client details and persist them
+            final OAuth2Client client = new OAuth2Client(
+                    INTERNAL_STROOM_CLIENT,
+                    createRandomCode(40) + ".apps.stroom-idp",
+                    createRandomCode(20),
+                    ".*");
             dao.create(client);
             optionalClient = dao.getClientByName(INTERNAL_STROOM_CLIENT);
         }
@@ -45,5 +51,12 @@ public class OpenIdClientDetailsFactoryImpl implements OpenIdClientDetailsFactor
         final byte[] bytes = new byte[length];
         secureRandom.nextBytes(bytes);
         return Base64.getUrlEncoder().encodeToString(bytes);
+    }
+
+    private static String createRandomHexCode(final int length) {
+        final SecureRandom secureRandom = new SecureRandom();
+        final byte[] bytes = new byte[length];
+        secureRandom.nextBytes(bytes);
+        return Hex.encodeHexString(bytes, true);
     }
 }
