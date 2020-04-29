069538f2e8c2ea075dc640a496469304b9257bb0
at055612 <at055612@users.noreply.github.com>
2020-04-29 21:21:54 +0100

    gh-1508 Update prop descriptions for CacheConfig

--- stroom-util/src/main/java/stroom/util/cache/CacheConfig.java
+++ stroom-util/src/main/java/stroom/util/cache/CacheConfig.java
@@ -1,13 +1,15 @@
 package stroom.util.cache;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import stroom.util.shared.AbstractConfig;
 import stroom.util.shared.NotInjectableConfig;
 import stroom.util.time.StroomDuration;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+
 import javax.validation.constraints.Min;
 
+// The descriptions have mostly been taken from the Caffine javadoc
 @NotInjectableConfig
 public class CacheConfig extends AbstractConfig {
 
@@ -29,7 +31,13 @@ public class CacheConfig extends AbstractConfig {
         this.expireAfterWrite = expireAfterWrite;
     }
 
-    @JsonPropertyDescription("The maximum number of entries in the cache")
+    @JsonPropertyDescription("Specifies the maximum number of entries the cache may contain. Note that the cache " +
+            "may evict an entry before this limit is exceeded or temporarily exceed the threshold while evicting. " +
+            "As the cache size grows close to the maximum, the cache evicts entries that are less likely to be used " +
+            "again. For example, the cache may evict an entry because it hasn't been used recently or very often. " +
+            "When size is zero, elements will be evicted immediately after being loaded into the cache. This can " +
+            "be useful in testing, or to disable caching temporarily without a code change. If no value is set then " +
+            "no size limit will be applied.")
     @JsonProperty(PROP_NAME_MAXIMUM_SIZE)
     @Min(0)
     public Long getMaximumSize() {
@@ -41,8 +49,9 @@ public class CacheConfig extends AbstractConfig {
     }
 
     @JsonPropertyDescription("Specifies that each entry should be automatically removed from the cache once " +
-        "a this duration has elapsed after the entry's creation, the most recent replacement of " +
-        "its value, or its last read. In ISO-8601 duration format, e.g. 'PT10M'")
+            "this duration has elapsed after the entry's creation, the most recent replacement of " +
+            "its value, or its last read. In ISO-8601 duration format, e.g. 'PT10M'. If no value is set then " +
+            " entries will not be aged out based these criteria.")
     @JsonProperty(PROP_NAME_EXPIRE_AFTER_ACCESS)
     public StroomDuration getExpireAfterAccess() {
         return expireAfterAccess;
@@ -53,8 +62,9 @@ public class CacheConfig extends AbstractConfig {
     }
 
     @JsonPropertyDescription("Specifies that each entry should be automatically removed from the cache once " +
-        "a fixed duration has elapsed after the entry's creation, or the most recent replacement of its value. " +
-        "In ISO-8601 duration format, e.g. 'PT5M'")
+            "a fixed duration has elapsed after the entry's creation, or the most recent replacement of its value. " +
+            "In ISO-8601 duration format, e.g. 'PT5M'. If no value is set then entries will not be aged out based on " +
+            " these criteria.")
     @JsonProperty(PROP_NAME_EXPIRE_AFTER_WRITE)
     public StroomDuration getExpireAfterWrite() {
         return expireAfterWrite;
