0bb93ed6233f538539cc65395e209ef4f2c9fac4
at055612 <at055612@users.noreply.github.com>
2020-05-04 22:37:34 +0100

    Change HasSystemInfo to use fully qualified name by default

--- stroom-util/src/main/java/stroom/util/sysinfo/HasSystemInfo.java
+++ stroom-util/src/main/java/stroom/util/sysinfo/HasSystemInfo.java
@@ -4,11 +4,11 @@ public interface HasSystemInfo {
 
     /**
      * @return The name for the system information being provided. Should be limited
-     * to [A-Za-z_-] to avoid URL encoding issues. By default the unqualified class
+     * to [A-Za-z_-] to avoid URL encoding issues. By default the qualified class
      * name will be used.
      */
     default String getSystemInfoName() {
-        return this.getClass().getSimpleName();
+        return this.getClass().getName();
     }
 
     SystemInfoResult getSystemInfo();
