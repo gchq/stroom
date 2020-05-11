716bc8278a1542a3a34f693b35a90e59aa2dafdc
at055612 <at055612@users.noreply.github.com>
2020-05-11 17:18:19 +0100

    gh-1508 Format code

--- stroom-util/src/main/java/stroom/util/config/PropertyUtil.java
+++ stroom-util/src/main/java/stroom/util/config/PropertyUtil.java
@@ -18,11 +18,12 @@
 package stroom.util.config;
 
 
+import stroom.util.logging.LogUtil;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.CaseFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import stroom.util.logging.LogUtil;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
