fef64c1e89b32b4231ed4ae8461a0672bac3bcc4
at055612 <at055612@users.noreply.github.com>
2020-05-11 17:18:37 +0100

    gh-1508 Format code, remove static imports

--- stroom-authentication/stroom-authentication-impl/src/main/java/stroom/authentication/authenticate/PasswordValidator.java
+++ stroom-authentication/stroom-authentication-impl/src/main/java/stroom/authentication/authenticate/PasswordValidator.java
@@ -3,34 +3,40 @@ package stroom.authentication.authenticate;
 
 import java.util.Optional;
 
-import static stroom.authentication.authenticate.LoginResult.*;
-import static stroom.authentication.authenticate.PasswordValidationFailureType.BAD_OLD_PASSWORD;
-import static stroom.authentication.authenticate.PasswordValidationFailureType.COMPLEXITY;
-import static stroom.authentication.authenticate.PasswordValidationFailureType.LENGTH;
-import static stroom.authentication.authenticate.PasswordValidationFailureType.REUSE;
 
 public class PasswordValidator {
 
-    static Optional<PasswordValidationFailureType> validateLength(String newPassword, int minimumLength) {
+    static Optional<PasswordValidationFailureType> validateLength(final String newPassword,
+                                                                  int minimumLength) {
         boolean isLengthValid = newPassword != null && (newPassword.length() >= minimumLength);
-        return isLengthValid ? Optional.empty() : Optional.of(LENGTH);
+        return isLengthValid
+                ? Optional.empty()
+                : Optional.of(PasswordValidationFailureType.LENGTH);
     }
 
-    static Optional<PasswordValidationFailureType> validateComplexity(String newPassword, String complexityRegex) {
+    static Optional<PasswordValidationFailureType> validateComplexity(final String newPassword,
+                                                                      final String complexityRegex) {
         boolean isPasswordComplexEnough = newPassword.matches(complexityRegex);
-        return isPasswordComplexEnough ? Optional.empty() : Optional.of(COMPLEXITY);
+        return isPasswordComplexEnough
+                ? Optional.empty()
+                : Optional.of(PasswordValidationFailureType.COMPLEXITY);
     }
 
-    static Optional<PasswordValidationFailureType> validateAuthenticity(LoginResult loginResult) {
-        boolean isPasswordValid = loginResult != BAD_CREDENTIALS
-                && loginResult != DISABLED_BAD_CREDENTIALS
-                && loginResult != LOCKED_BAD_CREDENTIALS
-                && loginResult != USER_DOES_NOT_EXIST;
-        return isPasswordValid ? Optional.empty() : Optional.of(BAD_OLD_PASSWORD);
+    static Optional<PasswordValidationFailureType> validateAuthenticity(final LoginResult loginResult) {
+        boolean isPasswordValid = loginResult != LoginResult.BAD_CREDENTIALS
+                && loginResult != LoginResult.DISABLED_BAD_CREDENTIALS
+                && loginResult != LoginResult.LOCKED_BAD_CREDENTIALS
+                && loginResult != LoginResult.USER_DOES_NOT_EXIST;
+        return isPasswordValid
+                ? Optional.empty()
+                : Optional.of(PasswordValidationFailureType.BAD_OLD_PASSWORD);
     }
 
-    static Optional<PasswordValidationFailureType> validateReuse(String oldPassword, String newPassword) {
+    static Optional<PasswordValidationFailureType> validateReuse(final String oldPassword,
+                                                                 final String newPassword) {
         boolean isPasswordReused = oldPassword.equalsIgnoreCase(newPassword);
-        return isPasswordReused ? Optional.of(REUSE) : Optional.empty();
+        return isPasswordReused
+                ? Optional.of(PasswordValidationFailureType.REUSE)
+                : Optional.empty();
     }
 }
