8529ac906b0ddbfa3d3b0edeb76b932a018e2321
at055612 <at055612@users.noreply.github.com>
2020-05-12 11:19:32 +0100

    gh-1508 Add prod descriptions to DataRetentionConfig

--- stroom-data/stroom-data-retention-impl/src/main/java/stroom/data/retention/impl/DataRetentionConfig.java
+++ stroom-data/stroom-data-retention-impl/src/main/java/stroom/data/retention/impl/DataRetentionConfig.java
@@ -2,16 +2,27 @@ package stroom.data.retention.impl;
 
 import stroom.util.shared.AbstractConfig;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import javax.inject.Singleton;
+import javax.validation.constraints.Min;
 
 @Singleton
+@JsonPropertyOrder(alphabetic = true)
 public class DataRetentionConfig extends AbstractConfig {
+
+    @Min(1)
+    @JsonProperty
+    @JsonPropertyDescription("The number of streams to delete in a single batch when applying data retention rules.")
     private int deleteBatchSize = 1000;
 
     public int getDeleteBatchSize() {
         return deleteBatchSize;
     }
 
+    @SuppressWarnings("unused")
     public void setDeleteBatchSize(final int deleteBatchSize) {
         this.deleteBatchSize = deleteBatchSize;
     }
