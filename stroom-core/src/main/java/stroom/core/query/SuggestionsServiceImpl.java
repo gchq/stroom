7efd197c41afa89881c8df1c5131fe241306429a
at055612 <at055612@users.noreply.github.com>
2020-05-18 12:25:11 +0100

    gh-1574 Refactor fuzzy match code to StringPredicateFactory
    Also make it work with camel case and brackets

--- stroom-core/src/main/java/stroom/core/query/SuggestionsServiceImpl.java
+++ stroom-core/src/main/java/stroom/core/query/SuggestionsServiceImpl.java
@@ -11,6 +11,7 @@ import stroom.query.shared.FetchSuggestionsRequest;
 import stroom.security.api.SecurityContext;
 import stroom.task.api.TaskContext;
 import stroom.task.api.TaskContextFactory;
+import stroom.util.string.StringPredicateFactory;
 
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -27,8 +28,6 @@ import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -37,8 +36,6 @@ import java.util.stream.Stream;
 public class SuggestionsServiceImpl implements SuggestionsService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SuggestionsServiceImpl.class);
-    private static final Pattern SEPARATOR_PATTERN = Pattern.compile("[ _\\-]");
-    private static final Pattern CASE_INSENS_WORD_LETTER_PATTERN = Pattern.compile("[a-z0-9]");
     private static final int LIMIT = 20;
 
     private final MetaService metaService;
@@ -68,7 +65,6 @@ public class SuggestionsServiceImpl implements SuggestionsService {
         this.taskContextFactory = taskContextFactory;
     }
 
-
     @Override
     public List<String> fetch(final FetchSuggestionsRequest request) {
         return securityContext.secureResult(() -> {
@@ -97,7 +93,7 @@ public class SuggestionsServiceImpl implements SuggestionsService {
         final List<String> result;
         result = pipelineStore.list().stream()
                 .map(DocRef::getName)
-                .filter(name -> userInput == null || name.contains(userInput))
+                .filter(StringPredicateFactory.createFuzzyMatchPredicate(userInput))
                 .sorted()
                 .limit(LIMIT)
                 .collect(Collectors.toList());
@@ -109,7 +105,7 @@ public class SuggestionsServiceImpl implements SuggestionsService {
         final List<String> result;
         result = Arrays.stream(Status.values())
                 .map(Status::getDisplayValue)
-                .filter(name -> userInput == null || name.contains(userInput))
+                .filter(StringPredicateFactory.createFuzzyMatchPredicate(userInput))
                 .sorted()
                 .limit(LIMIT)
                 .collect(Collectors.toList());
@@ -126,7 +122,6 @@ public class SuggestionsServiceImpl implements SuggestionsService {
     }
 
     private List<String> createFeedList(final TaskContext parentTaskContext, final String userInput) {
-        // TODO consider using isFuzzyMatch below
         // To get a list of feed names we need to combine the names from the meta service
         // and the feed store. Meta service only has feeds which have data, but may contain
         // feeds that have been deleted as docs.
@@ -150,7 +145,7 @@ public class SuggestionsServiceImpl implements SuggestionsService {
                     .thenCombine(docFeedsFuture, (metaFeedNames, docFeedNames) ->
                             Stream.concat(metaFeedNames.stream(), docFeedNames.stream())
                                     .parallel()
-                                    .filter(createFuzzyMatchPredicate(userInput))
+                                    .filter(StringPredicateFactory.createFuzzyMatchPredicate(userInput))
                                     .sorted(Comparator.naturalOrder())
                                     .distinct()
                                     .limit(LIMIT)
@@ -165,206 +160,16 @@ public class SuggestionsServiceImpl implements SuggestionsService {
         }
     }
 
-    static Predicate<String> createCaseInsensitiveContainsPredicate(final String userInput) {
-        if (userInput == null) {
-            return stringUnderTest -> true;
-        } else {
-            return stringUnderTest -> {
-                String lowerCaseInput = userInput.toLowerCase();
-                return stringUnderTest == null
-                        || stringUnderTest.toLowerCase().contains(lowerCaseInput);
-            };
-        }
-    }
-
     private List<String> createStreamTypeList(final String userInput) {
         // TODO this seems pretty inefficient as each call hits the db to get ALL feeds
         //   then limits/filters in java.  Needs to work off a cached feed name list
 
         return metaService.getTypes()
                 .parallelStream()
-                .filter(typeName -> typeName == null || userInput == null || typeName.startsWith(userInput))
+                .filter(StringPredicateFactory.createFuzzyMatchPredicate(userInput))
                 .sorted(Comparator.naturalOrder())
                 .limit(LIMIT)
                 .collect(Collectors.toList());
     }
 
-    static Predicate<String> createFuzzyMatchPredicate(final String userInput) {
-        if (userInput == null || userInput.isEmpty()) {
-            LOGGER.debug("Null input predicate");
-            // No input so get everything
-            return stringUnderTest -> true;
-        } else if (userInput.startsWith("^") && userInput.endsWith("$")) {
-            return createCaseInsensitiveExactMatchPredicate(userInput);
-        } else if (userInput.endsWith("$")) {
-            return createCaseInsensitiveEndsWithPredicate(userInput);
-        } else if (userInput.startsWith("^")) {
-            return createCaseInsensitiveStartsWithPredicate(userInput);
-        } else if (isAllLowerCase(userInput)) {
-            return createCharsAnywherePredicate(userInput);
-        } else {
-            return createWordBoundaryPredicate(userInput);
-        }
-    }
-
-    @NotNull
-    private static Predicate<String> createWordBoundaryPredicate(final String userInput) {
-        LOGGER.debug("Word boundary predicate");
-        // Has some uppercase so use word boundaries
-        // An upper case letter means the start of a word
-        // a lower case letter means the continuation of a word
-
-        final StringBuilder patternBuilder = new StringBuilder();
-        for (int i = 0; i < userInput.length(); i++) {
-            char chr = userInput.charAt(i);
-            char lastChr = 0;
-
-            if (Character.isUpperCase(chr)) {
-                if (i == 0) {
-                    // First letter so is either preceded by ^ or by a separator
-                    patternBuilder
-                            .append("(^|")
-                            .append(SEPARATOR_PATTERN)
-                            .append(")");
-                } else {
-                    // Not the first letter so need the end of the previous word
-                    // and a word separator
-                    patternBuilder
-                            .append(CASE_INSENS_WORD_LETTER_PATTERN)
-                            .append("*")
-                            .append(SEPARATOR_PATTERN);
-                }
-                // Start of a word
-                patternBuilder.append(chr);
-            } else {
-                // Continuation of a word
-                patternBuilder.append(chr);
-            }
-        }
-        final Pattern pattern = Pattern.compile(patternBuilder.toString(), Pattern.CASE_INSENSITIVE);
-        LOGGER.debug("Pattern: {}", pattern);
-        return toNullSafePredicate(pattern.asPredicate());
-    }
-
-    @NotNull
-    private static Predicate<String> createCharsAnywherePredicate(final String userInput) {
-        LOGGER.debug("Chars appear anywhere in correct order predicate");
-        // All lower case so match on each char appearing somewhere in the text
-        // in the correct order
-        final StringBuilder patternBuilder = new StringBuilder();
-        for (int i = 0; i < userInput.length(); i++) {
-                patternBuilder.append(".*?")
-                        .append(userInput.charAt(i));
-        }
-        patternBuilder.append(".*?");
-        final Pattern pattern = Pattern.compile(patternBuilder.toString(), Pattern.CASE_INSENSITIVE);
-        LOGGER.debug("Pattern: {}", pattern);
-        return toNullSafePredicate(pattern.asPredicate());
-    }
-
-    @NotNull
-    private static Predicate<String> createCaseInsensitiveStartsWithPredicate(final String userInput) {
-        LOGGER.debug("Case insensitive starts with predicate");
-        // remove the ^ marker char
-        final String lowerCaseInput = userInput.substring(1).toLowerCase();
-        return toNullSafePredicate(stringUnderTest ->
-                stringUnderTest.toLowerCase().startsWith(lowerCaseInput));
-    }
-
-    @NotNull
-    private static Predicate<String> createCaseInsensitiveEndsWithPredicate(final String userInput) {
-        LOGGER.debug("Case insensitive ends with predicate");
-        // remove the ^ marker char
-        final String lowerCaseInput = userInput.substring(0, userInput.length() - 1)
-                .toLowerCase();
-        return toNullSafePredicate(stringUnderTest ->
-                stringUnderTest.toLowerCase().endsWith(lowerCaseInput));
-    }
-
-    @NotNull
-    private static Predicate<String> createCaseInsensitiveExactMatchPredicate(final String userInput) {
-        LOGGER.debug("Case insensitive exact match predicate");
-        final String lowerCaseInput = userInput.substring(1)
-                .substring(0, userInput.length() - 2);
-        return toNullSafePredicate(stringUnderTest ->
-                stringUnderTest.toLowerCase().equalsIgnoreCase(lowerCaseInput));
-    }
-
-    /**
-     * Wraps the passes {@link Predicate} with one that returns false if the value under test is null
-     */
-    private static Predicate<String> toNullSafePredicate(final Predicate<String> predicate) {
-        return str -> {
-            if (str == null) {
-                return false;
-            } else {
-                return predicate.test(str);
-            }
-        };
-    }
-
-    private static boolean isAllLowerCase(final String text) {
-        for (int i = 0; i < text.length(); i++) {
-            if (Character.isUpperCase(text.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * See tests for an idea of how this is meant to work. This is unfinished and
-     * needs optimising.
-     */
-    static boolean isFuzzyMatch(final String userInput, final String text) {
-        final String inputStr = Objects.requireNonNullElse(userInput, "");
-        final String testStr = Objects.requireNonNullElse(text, "");
-
-        LOGGER.debug("Testing userInput: {} against text: {}", inputStr, testStr);
-        if (testStr.startsWith(inputStr)) {
-            LOGGER.info("Matched on startsWith");
-            return true;
-        } else if (testStr.contains(inputStr)) {
-            LOGGER.info("Matched on contains");
-            return true;
-        } else {
-            boolean hasMatched = false;
-            String patternStr = inputStr.chars().boxed()
-                    .map(charVal -> {
-                        char theChar = (char) charVal.intValue();
-                        if (Character.isLetterOrDigit(theChar)) {
-                            return String.valueOf(theChar);
-                        } else if (SEPARATOR_PATTERN.matcher(String.valueOf(theChar)).matches()) {
-                            return "[^_-]*" + theChar;
-                        } else {
-                            throw new RuntimeException("Unexpected char " + theChar);
-                        }
-                    })
-                    .collect(Collectors.joining());
-            patternStr = ".*" + patternStr + ".*";
-
-            if (Pattern.matches(patternStr, testStr)) {
-                LOGGER.info("Matched on pattern: {}", patternStr);
-                hasMatched = true;
-            } else {
-                LOGGER.info("Not Matched on pattern: {}", patternStr);
-            }
-
-            patternStr = inputStr.chars().boxed()
-                    .map(charVal -> {
-                        char theChar = (char) charVal.intValue();
-                        return String.valueOf(theChar);
-                    })
-                    .collect(Collectors.joining(".*"));
-            patternStr = ".*" + patternStr + ".*";
-
-            if (Pattern.matches(patternStr, testStr)) {
-                LOGGER.info("Matched on pattern: {}", patternStr);
-                hasMatched = true;
-            } else {
-                LOGGER.info("Not Matched on pattern: {}", patternStr);
-            }
-            return hasMatched;
-        }
-    }
 }
---
+++ stroom-util/src/main/java/stroom/util/string/StringPredicateFactory.java
@@ -0,0 +1,310 @@
+package stroom.util.string;
+
+import stroom.util.ConsoleColour;
+import stroom.util.logging.LogUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.validation.constraints.NotNull;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+/**
+ * Useful methods to create various {@link Predicate<String>}
+ */
+public class StringPredicateFactory {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StringPredicateFactory.class);
+
+    // Treat brackets as word separators, e.g. "Events (XML)"
+    private static final Pattern DEFAULT_SEPARATOR_CHAR_CLASS = Pattern.compile("[ _\\-()\\[\\]]");
+
+    private static final Pattern CASE_INSENS_WORD_LETTER_CHAR_CLASS = Pattern.compile("[a-z0-9]");
+
+    // Matches a whole string that is lowerCamelCase or UpperCamelCase
+    // It is debatable if we should instead look for the absence of a separator as that may
+    // be easier
+    private static final Pattern CAMEL_CASE_PATTERN = Pattern.compile(
+            "^([A-Z]+)?[a-z0-9]+(?:(?:\\d)|(?:[A-Z0-9]+[a-z0-9]+))*(?:[A-Z]+)?$");
+
+    // Matches positions in (C|c)amelCase to split into individual words
+    // Doesn't cope with abbreviations at the beginning/middle of the string,
+    // e.g. SQLScript or SomeSQLScript
+    // Pattern also splits on a space to allow us to pre-split the string a bit
+    private static final Pattern CAMEL_CASE_SPLIT_PATTERN = Pattern.compile(
+            "((?<=[a-z])(?=[A-Z])|(?<=[0-9])(?=[A-Z])|(?<=[a-zA-Z])(?=[0-9])| )");
+    private static final Pattern CAMEL_CASE_ABBREVIATIONS_PATTERN = Pattern.compile("([A-Z]+)([A-Z][a-z0-9])");
+
+    // Static util methods only
+    private StringPredicateFactory() {
+    }
+
+    /**
+     * @see StringPredicateFactory#createFuzzyMatchPredicate(String, Pattern)
+     */
+    public static Predicate<String> createFuzzyMatchPredicate(final String userInput) {
+        return createFuzzyMatchPredicate(userInput, DEFAULT_SEPARATOR_CHAR_CLASS);
+    }
+
+    /**
+     * Creates a fuzzy match {@link Predicate<String>} for userInput.
+     * Null userInput results in an always true predicate.
+     * Broadly it has five match modes:
+     * Starts with: "^prefix" matches "PrefixToSomeText" (case insensitive)
+     * Ends with "suffix$" matches "TextWithSuffix" (case insensitive)
+     * Exact match: "^sometext$" matches "sometext" (case insensitive)
+     * Chars anywhere (in order): "aid" matches "A big dog" (case insensitive)
+     * Word boundary match: "OTheM" matches "on the mat" in "the cat sat on the mat", but not "the cat sat on their mat"
+     * See TestStringPredicateFactory for more examples of how the
+     * matching works.
+     * @param separatorCharacterClass A regex character class, e.g. [ \-_] that defines the separators
+     *                                between words in the string(s) under test.
+     */
+    public static Predicate<String> createFuzzyMatchPredicate(final String userInput,
+                                                              final Pattern separatorCharacterClass) {
+        LOGGER.debug("Creating predicate for userInput [{}] and separators {}", userInput, separatorCharacterClass);
+        Predicate<String> predicate;
+        if (userInput == null || userInput.isEmpty()) {
+            LOGGER.debug("Creating null input predicate");
+            // No input so get everything
+            predicate = stringUnderTest -> true;
+        } else if (userInput.startsWith("^") && userInput.endsWith("$")) {
+            predicate = createCaseInsensitiveExactMatchPredicate(userInput);
+        } else if (userInput.endsWith("$")) {
+            // remove the $ marker char from the end
+            predicate = createCaseInsensitiveEndsWithPredicate(userInput.substring(0, userInput.length() - 1));
+        } else if (userInput.startsWith("^")) {
+            // remove the ^ marker char from the beginning
+            predicate = createCaseInsensitiveStartsWithPredicate(userInput.substring(1));
+        } else if (isAllLowerCase(userInput)) {
+            predicate = createCharsAnywherePredicate(userInput);
+        } else {
+            predicate = createWordBoundaryPredicate(userInput, separatorCharacterClass);
+        }
+
+        if (LOGGER.isDebugEnabled()) {
+            return toLoggingPredicate(predicate);
+        } else {
+            return predicate;
+        }
+    }
+
+    /**
+     * Wraps the passed {@link Predicate} with one that returns result
+     * if the value under test is null
+     */
+    public static Predicate<String> toNullSafePredicate(final boolean resultIfNull,
+                                                        final Predicate<String> predicate) {
+        return str -> {
+            if (str == null) {
+                return resultIfNull;
+            } else {
+                return predicate.test(str);
+            }
+        };
+    }
+
+    public static Predicate<String> toLoggingPredicate(final Predicate<String> predicate) {
+        return str -> {
+            boolean result = predicate.test(str);
+            final ConsoleColour colour = result ? ConsoleColour.GREEN : ConsoleColour.RED;
+
+            String msg = ConsoleColour.colourise(LogUtil.message("String under test [{}], result: {}",
+                    str, result), colour);
+            LOGGER.debug(msg);
+            return result;
+        };
+    }
+
+    @NotNull
+    public static Predicate<String> createCaseInsensitiveStartsWithPredicate(final String userInput) {
+        LOGGER.debug("Creating case insensitive starts with predicate");
+        // remove the ^ marker char
+        final String lowerCaseInput = userInput.toLowerCase();
+        return toNullSafePredicate(false, stringUnderTest ->
+                stringUnderTest.toLowerCase().startsWith(lowerCaseInput));
+    }
+
+    @NotNull
+    public static Predicate<String> createCaseInsensitiveEndsWithPredicate(final String userInput) {
+        LOGGER.debug("Creating case insensitive ends with predicate");
+        final String lowerCaseInput = userInput.toLowerCase();
+        return toNullSafePredicate(false, stringUnderTest ->
+                stringUnderTest.toLowerCase().endsWith(lowerCaseInput));
+    }
+
+    public static Predicate<String> createCaseInsensitiveContainsPredicate(final String userInput) {
+        if (userInput == null) {
+            return stringUnderTest -> true;
+        } else {
+            final String lowerCaseInput = userInput.toLowerCase();
+            return toNullSafePredicate(false, stringUnderTest ->
+                    stringUnderTest.toLowerCase().contains(lowerCaseInput));
+        }
+    }
+
+    @NotNull
+    private static Predicate<String> createWordBoundaryPredicate(final String userInput,
+                                                                 final Pattern separatorCharacterClass) {
+        LOGGER.debug("creating word boundary predicate");
+        // Has some uppercase so use word boundaries
+        // An upper case letter means the start of a word
+        // a lower case letter means the continuation of a word
+
+        // We can use the separator based predicate for both camel case and separated
+        // strings as long as we modify the camel case ones first.
+        final Predicate<String> separatorPredicate = createSeparatedWordBoundaryPredicate(
+                userInput, separatorCharacterClass);
+
+        return toNullSafePredicate(false, stringUnderTest -> {
+            if (CAMEL_CASE_PATTERN.matcher(stringUnderTest).matches()) {
+                LOGGER.debug("stringUnderTest [{}] is (C|c)amelCase", stringUnderTest);
+
+                // replace stuff like SQLScript with "SQL Script"
+                String separatedStringUnderTest = CAMEL_CASE_ABBREVIATIONS_PATTERN
+                        .matcher(stringUnderTest)
+                        .replaceAll("$1 $2");
+
+                LOGGER.debug("separatedStringUnderTest: [{}]", separatedStringUnderTest);
+
+                // Now split on camel case word boundaries (or spaces added above)
+                separatedStringUnderTest = CAMEL_CASE_SPLIT_PATTERN
+                        .matcher(separatedStringUnderTest)
+                        .replaceAll(" ");
+
+                LOGGER.debug("separatedStringUnderTest: [{}]", separatedStringUnderTest);
+
+                // Now we have split the words with spaces, use the separator predicate
+                return separatorPredicate.test(separatedStringUnderTest);
+            } else {
+                LOGGER.debug("stringUnderTest [{}] has word separators", stringUnderTest);
+                return separatorPredicate.test(stringUnderTest);
+            }
+        });
+    }
+
+    @NotNull
+    private static Predicate<String> createSeparatedWordBoundaryPredicate(final String userInput,
+                                                                          final Pattern separatorCharacterClass) {
+        // Has some uppercase so use word boundaries
+        // An upper case letter means the start of a word
+        // a lower case letter means the continuation of a word
+
+        final StringBuilder patternBuilder = new StringBuilder();
+        for (int i = 0; i < userInput.length(); i++) {
+            char chr = userInput.charAt(i);
+
+            if (Character.isUpperCase(chr)) {
+                if (i == 0) {
+                    // First letter so is either preceded by ^ or by a separator
+                    patternBuilder
+                            .append("(?:^|") // non-capturing
+                            .append(separatorCharacterClass)
+                            .append(")");
+                } else {
+                    // Not the first letter so need the end of the previous word
+                    // and a word separator
+                    patternBuilder
+                            .append(CASE_INSENS_WORD_LETTER_CHAR_CLASS)
+                            .append("*")
+                            .append(separatorCharacterClass)
+                            .append("+"); // one of more separators
+                }
+                // Start of a word
+            }
+
+            if (Character.isLetterOrDigit(chr)) {
+                patternBuilder.append(chr);
+            } else {
+                // Might be a special char so escape it
+                patternBuilder.append(Pattern.quote(String.valueOf(chr)));
+            }
+        }
+        final Pattern pattern = Pattern.compile(patternBuilder.toString(), Pattern.CASE_INSENSITIVE);
+        LOGGER.debug("Using separated word pattern: {} with separators {}", pattern, separatorCharacterClass);
+
+        return pattern.asPredicate();
+    }
+
+    @NotNull
+    private static Predicate<String> createCamelCaseWordBoundaryPredicate(final String userInput) {
+        // Has some uppercase so use word boundaries
+        // An upper case letter means the start of a word
+        // a lower case letter means the continuation of a word
+
+        final StringBuilder patternBuilder = new StringBuilder();
+        for (int i = 0; i < userInput.length(); i++) {
+            char chr = userInput.charAt(i);
+
+            if (Character.isUpperCase(chr)) {
+                if (i == 0) {
+                    // First letter so is either preceded by ^ or by the end of the previous word
+                    patternBuilder
+                            .append("(?:^|[a-z0-9]") // non-capturing, assume numbers part of prev word
+                            .append(")");
+                } else {
+                    // Not the first letter so need the end of the previous word
+                    // and a word separator
+                    patternBuilder
+                            .append("[a-z0-9]")
+                            .append("*");
+                }
+                // Start of a word
+            }
+
+            if (Character.isLetterOrDigit(chr)) {
+                patternBuilder.append(chr);
+            } else {
+                // Might be a special char so escape it
+                patternBuilder.append(Pattern.quote(String.valueOf(chr)));
+            }
+        }
+        final Pattern pattern = Pattern.compile(patternBuilder.toString());
+        LOGGER.debug("Using (C|c)amelCase separated pattern: {}", pattern);
+        return toNullSafePredicate(false, pattern.asPredicate());
+    }
+
+    @NotNull
+    private static Predicate<String> createCharsAnywherePredicate(final String userInput) {
+        LOGGER.debug("creating chars appear anywhere in correct order predicate");
+        // All lower case so match on each char appearing somewhere in the text
+        // in the correct order
+        final StringBuilder patternBuilder = new StringBuilder();
+        for (int i = 0; i < userInput.length(); i++) {
+            patternBuilder.append(".*?");
+
+            char chr = userInput.charAt(i);
+            if (Character.isLetterOrDigit(chr)) {
+                patternBuilder.append(chr);
+            } else {
+                // Might be a special char so escape it
+                patternBuilder.append(Pattern.quote(String.valueOf(chr)));
+            }
+        }
+        patternBuilder.append(".*?");
+        final Pattern pattern = Pattern.compile(patternBuilder.toString(), Pattern.CASE_INSENSITIVE);
+        LOGGER.debug("Using pattern: {}", pattern);
+        return toNullSafePredicate(false, pattern.asPredicate());
+    }
+
+
+    @NotNull
+    private static Predicate<String> createCaseInsensitiveExactMatchPredicate(final String userInput) {
+        LOGGER.debug("creating case insensitive exact match predicate");
+        final String lowerCaseInput = userInput.substring(1)
+                .substring(0, userInput.length() - 2);
+        return toNullSafePredicate(false, stringUnderTest ->
+                stringUnderTest.toLowerCase().equalsIgnoreCase(lowerCaseInput));
+    }
+
+
+    private static boolean isAllLowerCase(final String text) {
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
