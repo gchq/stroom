f74ed573fd689ccae4ec3b2a5aebd6abcf090092
stroomdev66 <stroomdev66@gmail.com>
2020-05-23 09:56:36 +0100

    Tidy up dependencies

--- travis.script.sh
+++ travis.script.sh
@@ -260,7 +260,7 @@ else
       -PgwtCompilerMaxHeap=1G \
       clean \
       build \
-      test --tests *.TestFullTranslationTask
+      test --tests *TestFullTranslationTask
       "${extraBuildArgs[@]}"
 
 # IF WE WANT TO SKIP SOME PARTS OF THE BUILD INCLUDE THESE LINES
