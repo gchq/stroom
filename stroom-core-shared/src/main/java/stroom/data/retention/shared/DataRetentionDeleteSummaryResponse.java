e67e9803893a1f7c517bde342026c17d4643ceb3
at055612 <at055612@users.noreply.github.com>
2020-06-09 10:28:44 +0100

    Fix failing test

--- stroom-core-shared/src/main/java/stroom/data/retention/shared/DataRetentionDeleteSummaryResponse.java
+++ stroom-core-shared/src/main/java/stroom/data/retention/shared/DataRetentionDeleteSummaryResponse.java
@@ -4,10 +4,13 @@ import stroom.util.shared.PageResponse;
 import stroom.util.shared.ResultPage;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+@JsonInclude(Include.NON_NULL)
 public class DataRetentionDeleteSummaryResponse extends ResultPage<DataRetentionDeleteSummary> {
 
     public DataRetentionDeleteSummaryResponse(final List<DataRetentionDeleteSummary> values) {
