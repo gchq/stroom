d9ed4ad478b55b8c1c13649a7b67fcd7a84c9d7b
stroomdev66 <stroomdev66@gmail.com>
2020-06-12 22:36:12 +0100

    Change storybook options

--- stroom-ui/.storybook/config.js
+++ stroom-ui/.storybook/config.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 import * as React from "react";
-import { addParameters, configure, addDecorator } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
 import StroomDecorator from "../src/testing/storybook/StroomDecorator";
 
 import "../src/styles/main.scss";
@@ -26,73 +26,5 @@ function loadStories() {
 }
 
 addDecorator(StroomDecorator);
-
-// Option defaults:
-addParameters({
-  options: {
-    /**
-     * show story component as full screen
-     * @type {Boolean}
-     */
-    isFullScreen: false,
-    /**
-     * display panel that shows a list of stories
-     * @type {Boolean}
-     */
-    showNav: true,
-    /**
-     * display panel that shows addon configurations
-     * @type {Boolean}
-     */
-    showPanel: false,
-    /**
-     * where to show the addon panel
-     * @type {('bottom'|'right')}
-     */
-    panelPosition: "bottom",
-    /**
-     * sorts stories
-     * @type {Boolean}
-     */
-    sortStoriesByKind: false,
-    /**
-     * regex for finding the hierarchy separator
-     * @example:
-     *   null - turn off hierarchy
-     *   /\// - split by `/`
-     *   /\./ - split by `.`
-     *   /\/|\./ - split by `/` or `.`
-     * @type {Regex}
-     */
-    hierarchySeparator: /\/|\./,
-    /**
-     * regex for finding the hierarchy root separator
-     * @example:
-     *   null - turn off multiple hierarchy roots
-     *   /\|/ - split by `|`
-     * @type {Regex}
-     */
-    hierarchyRootSeparator: /\|/,
-    /**
-     * sidebar tree animations
-     * @type {Boolean}
-     */
-    sidebarAnimations: true,
-    /**
-     * enable/disable shortcuts
-     * @type {Boolean}
-     */
-    enableShortcuts: true,
-    /**
-     * show/hide tool bar
-     * @type {Boolean}
-     */
-    isToolshown: true,
-    /**
-     * theme storybook, see link below
-     */
-    theme: undefined,
-  },
-});
 
 configure(loadStories, module);
---
+++ stroom-ui/.storybook/manager.js
@@ -0,0 +1,57 @@
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+
+addons.setConfig({
+  /**
+   * show story component as full screen
+   * @type {Boolean}
+   */
+  isFullscreen: false,
+
+  /**
+   * display panel that shows a list of stories
+   * @type {Boolean}
+   */
+  showNav: true,
+
+  /**
+   * display panel that shows addon configurations
+   * @type {Boolean}
+   */
+  showPanel: false,
+
+  /**
+   * where to show the addon panel
+   * @type {('bottom'|'right')}
+   */
+  panelPosition: 'bottom',
+
+  /**
+   * sidebar tree animations
+   * @type {Boolean}
+   */
+  sidebarAnimations: true,
+
+  /**
+   * enable/disable shortcuts
+   * @type {Boolean}
+   */
+  enableShortcuts: true,
+
+  /**
+   * show/hide tool bar
+   * @type {Boolean}
+   */
+  isToolshown: true,
+
+  /**
+   * theme storybook, see link below
+   */
+  theme: themes.dark,
+
+  /**
+   * id to select an addon panel
+   * @type {String}
+   */
+  selectedPanel: undefined,
+});
