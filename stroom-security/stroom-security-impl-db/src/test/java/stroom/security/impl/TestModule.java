6103c47321b1529c66ca671c00b20d72578985f9
stroomdev66 <stroomdev66@gmail.com>
2020-07-27 19:44:07 +0100

    Fix test

--- stroom-security/stroom-security-impl-db/src/test/java/stroom/security/impl/TestModule.java
+++ stroom-security/stroom-security-impl-db/src/test/java/stroom/security/impl/TestModule.java
@@ -2,6 +2,7 @@ package stroom.security.impl;
 
 import com.google.inject.AbstractModule;
 import stroom.cache.impl.CacheModule;
+import stroom.security.impl.event.PermissionChangeEventBus;
 import stroom.security.mock.MockProcessingUserIdentityProviderModule;
 import stroom.util.entityevent.EntityEventBus;
 import stroom.explorer.api.ExplorerService;
@@ -24,6 +25,7 @@ public class TestModule extends AbstractModule {
         bind(UserService.class).to(UserServiceImpl.class);
         bind(ExplorerService.class).toInstance(mock(ExplorerService.class));
         bind(EntityEventBus.class).toInstance(mock(EntityEventBus.class));
+        bind(PermissionChangeEventBus.class).toInstance(mock(PermissionChangeEventBus.class));
 
         // Not using all the DB modules so just bind to an empty anonymous class
         bind(ForceCoreMigration.class).toInstance(new ForceCoreMigration() {});
