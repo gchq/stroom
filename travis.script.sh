2f4091213ca9f52c683d3d318ddb81fe3451bf92
stroomdev66 <stroomdev66@gmail.com>
2020-08-05 10:43:25 +0100

    Changes to enable parallel builds

--- travis.script.sh
+++ travis.script.sh
@@ -265,7 +265,7 @@ else
       -x gwtCompile \
       -x copyYarnBuild \
       -x test \
-      --debug \
+      --info \
       --scan -s
 
 # IF WE WANT TO SKIP SOME PARTS OF THE BUILD INCLUDE THESE LINES
