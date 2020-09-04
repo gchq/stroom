aa566a3e9e8a577cf1ca04d140f8ddf85214ede9
stroomdev66 <stroomdev66@gmail.com>
2020-09-04 12:03:18 +0100

    #1755 Stepping now runs in a separate thread
    Stepping now runs in a separate thread to prevent interruption of DW
    threads when trying to terminate stepping early.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
 
+* Issue **#1755** : Stepping now runs in a separate thread to prevent interruption of DW threads when trying to terminate stepping early.
+
 * Issue **#1798** : Fixed REST serialisation issue that was preventing stepping XPath filters from being passed to the server.
 
 * Issue **#1666** : Stepping now loads element documents that use name patterns.
--- stroom-pipeline/src/main/java/stroom/pipeline/stepping/SteppingService.java
+++ stroom-pipeline/src/main/java/stroom/pipeline/stepping/SteppingService.java
@@ -2,37 +2,47 @@ package stroom.pipeline.stepping;
 
 import stroom.pipeline.shared.stepping.PipelineStepRequest;
 import stroom.pipeline.shared.stepping.SteppingResult;
+import stroom.task.api.ExecutorProvider;
 import stroom.task.api.TaskContext;
 import stroom.task.api.TaskContextFactory;
+import stroom.task.api.ThreadPoolImpl;
+import stroom.task.shared.ThreadPool;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class SteppingService {
+    static final ThreadPool THREAD_POOL = new ThreadPoolImpl(
+            "Stepping",
+            5,
+            0,
+            Integer.MAX_VALUE);
+
     private final TaskContextFactory taskContextFactory;
     private final Provider<SteppingRequestHandler> steppingRequestHandlerProvider;
+    private final ExecutorProvider executorProvider;
 
     @Inject
     public SteppingService(final TaskContextFactory taskContextFactory,
-                           final Provider<SteppingRequestHandler> steppingRequestHandlerProvider) {
+                           final Provider<SteppingRequestHandler> steppingRequestHandlerProvider,
+                           final ExecutorProvider executorProvider) {
         this.taskContextFactory = taskContextFactory;
         this.steppingRequestHandlerProvider = steppingRequestHandlerProvider;
+        this.executorProvider = executorProvider;
     }
 
     public SteppingResult step(final PipelineStepRequest request) {
-        // Make sure stepping can only happen on streams that are visible to
-        // the user.
-        // FIXME : Constrain available streams.
-        // folderValidator.constrainCriteria(task.getCriteria());
-
         // Execute the stepping task.
         final Function<TaskContext, SteppingResult> function = taskContext -> {
             final SteppingRequestHandler steppingRequestHandler = steppingRequestHandlerProvider.get();
             return steppingRequestHandler.exec(taskContext, request);
         };
         final Supplier<SteppingResult> supplier = taskContextFactory.contextResult("Translation stepping", function);
-        return supplier.get();
+        final Executor executor = executorProvider.get(THREAD_POOL);
+        return CompletableFuture.supplyAsync(supplier, executor).join();
     }
 }
