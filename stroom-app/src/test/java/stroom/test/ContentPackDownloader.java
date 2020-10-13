a5764523c11964b14fcbb56a601088e3f7d02dcd
at055612 <at055612@users.noreply.github.com>
2020-10-13 11:52:25 +0100

    Make downloading of content packs all use same dir
    This should reduce the number of downloads that actually happen

--- stroom-app/src/test/java/stroom/test/ContentPackDownloader.java
+++ stroom-app/src/test/java/stroom/test/ContentPackDownloader.java
@@ -1,10 +1,11 @@
 package stroom.test;
 
+import stroom.content.ContentPack;
+import stroom.util.io.FileUtil;
+
 import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import stroom.util.io.FileUtil;
-import stroom.util.shared.Version;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -17,30 +18,31 @@ public class ContentPackDownloader extends AbstractContentDownloader {
     public static final String URL_PREFIX = "https://github.com/gchq/stroom-content/releases/download/";
     private static final Logger LOGGER = LoggerFactory.getLogger(ContentPackDownloader.class);
 
-    public static Path downloadContentPack(final String contentPackName, final Version version, final Path destDir) {
-        return downloadContentPack(contentPackName, version, destDir, ConflictMode.KEEP_EXISTING);
+    public static Path downloadContentPack(final ContentPack contentPack, final Path destDir) {
+        return downloadContentPack(contentPack, destDir, ConflictMode.KEEP_EXISTING);
     }
 
-    public static Path downloadContentPack(final String contentPackName, final Version version, final Path destDir, final ConflictMode conflictMode) {
-        Preconditions.checkNotNull(contentPackName);
-        Preconditions.checkNotNull(version);
+    public static Path downloadContentPack(final ContentPack contentPack,
+                                           final Path destDir,
+                                           final ConflictMode conflictMode) {
+        Preconditions.checkNotNull(contentPack);
         Preconditions.checkNotNull(destDir);
         Preconditions.checkNotNull(conflictMode);
         Preconditions.checkArgument(Files.isDirectory(destDir));
 
-        Path destFilePath = buildDestFilePath(contentPackName, version, destDir);
+        Path destFilePath = buildDestFilePath(contentPack.getNameAsStr(), contentPack.getVersion(), destDir);
         boolean destFileExists = Files.isRegularFile(destFilePath);
 
         if (destFileExists && conflictMode.equals(ConflictMode.KEEP_EXISTING)) {
             LOGGER.debug("Requested contentPack {} already exists in {}, keeping existing",
-                    contentPackName,
+                    contentPack.getNameAsStr(),
                     FileUtil.getCanonicalPath(destFilePath));
             return destFilePath;
         }
 
         if (destFileExists && conflictMode.equals(ConflictMode.OVERWRITE_EXISTING)) {
             LOGGER.debug("Requested contentPack {} already exists in {}, overwriting existing",
-                    contentPackName,
+                    contentPack.getNameAsStr(),
                     FileUtil.getCanonicalPath(destFilePath));
             try {
                 Files.delete(destFilePath);
@@ -50,9 +52,9 @@ public class ContentPackDownloader extends AbstractContentDownloader {
             }
         }
 
-        URL fileUrl = buildFileUrl(contentPackName, version);
+        URL fileUrl = buildFileUrl(contentPack.getNameAsStr(), contentPack.getVersion());
         LOGGER.info("Downloading contentPack {} from {} to {}",
-                contentPackName,
+                contentPack.getNameAsStr(),
                 fileUrl.toString(),
                 FileUtil.getCanonicalPath(destFilePath));
 
