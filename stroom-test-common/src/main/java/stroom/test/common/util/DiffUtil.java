2d34a9a2c6cbeef64223ad2154a2b82d090cb541
at055612 <at055612@users.noreply.github.com>
2020-12-01 08:27:49 +0000

    Tidy up diffing code

--- stroom-test-common/src/main/java/stroom/test/common/util/DiffUtil.java
+++ stroom-test-common/src/main/java/stroom/test/common/util/DiffUtil.java
@@ -1,6 +1,7 @@
 package stroom.test.common.util;
 
 import stroom.util.ConsoleColour;
+import stroom.util.logging.LogUtil;
 
 import org.assertj.core.util.diff.DiffUtils;
 import org.assertj.core.util.diff.Patch;
@@ -14,23 +15,34 @@ import java.util.stream.Collectors;
 
 public class DiffUtil {
 
-    public static void diff(final Path file1,
-                     final Path file2,
-                     final Consumer<String> diffLineConsumer,
-                     final boolean colouredOutput,
-                     final int contextLines) throws IOException {
+    public static boolean unifiedDiff(final Path file1,
+                                   final Path file2,
+                                   final Consumer<String> diffLineConsumer,
+                                   final boolean colouredOutput,
+                                   final int contextLines) {
 
-        final String string1 = Files.readString(file1);
-        final String string2 = Files.readString(file2);
+        final String string1 = getFileAsString(file1);
+        final String string2 = getFileAsString(file2);
 
-        diff(string1, string2, diffLineConsumer, colouredOutput, contextLines);
+        return diff(file1, file2, string1, string2, diffLineConsumer, colouredOutput, contextLines);
     }
 
-    public static void diff(final String string1,
-                            final String string2,
-                            final Consumer<String> diffLineConsumer,
-                            final boolean colouredOutput,
-                            final int contextLines) {
+    private static String getFileAsString(final Path file) {
+        try {
+            return Files.readString(file);
+        } catch (IOException e) {
+            throw new RuntimeException(LogUtil.message("Error opening file {}: {}",
+                    file.toAbsolutePath().normalize(), e.getMessage()), e);
+        }
+    }
+
+    private static boolean diff(final Path file1,
+                             final Path file2,
+                             final String string1,
+                             final String string2,
+                             final Consumer<String> diffLineConsumer,
+                             final boolean colouredOutput,
+                             final int contextLines) {
 
         final List<String> lines1 = string1.lines().collect(Collectors.toList());
         final List<String> lines2 = string2.lines().collect(Collectors.toList());
@@ -38,8 +50,8 @@ public class DiffUtil {
         final Patch<String> patch = DiffUtils.diff(lines1, lines2);
 
         final List<String> unifiedDiff = DiffUtils.generateUnifiedDiff(
-                string1,
-                string2,
+                file1.toAbsolutePath().toString(),
+                file2.toAbsolutePath().toString(),
                 lines2,
                 patch,
                 contextLines);
@@ -58,9 +70,11 @@ public class DiffUtil {
 
                 if (colouredOutput) {
                     diffLineConsumer.accept(ConsoleColour.colourise(diffLine, lineColour));
+                } else {
+                    diffLineConsumer.accept(diffLine);
                 }
-                diffLineConsumer.accept(diffLine);
             });
         }
+        return !unifiedDiff.isEmpty();
     }
 }
