cf5d7c35d051ef902a14b2b0f8980deb43c02d94
at055612 <at055612@users.noreply.github.com>
2021-02-02 16:22:56 +0000

    Add MANUALLY_LOGGED type

--- stroom-util-shared/src/main/java/stroom/util/shared/AutoLogged.java
+++ stroom-util-shared/src/main/java/stroom/util/shared/AutoLogged.java
@@ -87,10 +87,19 @@ public @interface AutoLogged {
      *  ALLOCATE_AUTOMATICALLY - the system should determine event type based on resource method name and HTTP method.
      */
     enum OperationType {
-        //Special type - don't log at all
+        /**
+         * Special type - don't log at all
+         */
         UNLOGGED,
 
-        //Special type - system should determine event type based on resource method name and HTTP method
+        /**
+         * Special type - logging will be done via some other means, not the auto logger
+         */
+        MANUALLY_LOGGED,
+
+        /**
+         * Special type - system should determine event type based on resource method name and HTTP method
+         */
         ALLOCATE_AUTOMATICALLY,
 
         //Standard event types follow
