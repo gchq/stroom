e1989ffa44f96cd23cace27848332afc894d0a73
at055612 <at055612@users.noreply.github.com>
2021-02-05 13:33:37 +0000

    Fix bugs in TestRestResources

--- stroom-util/src/main/java/stroom/util/rest/RestUtil.java
+++ stroom-util/src/main/java/stroom/util/rest/RestUtil.java
@@ -5,8 +5,10 @@ import stroom.util.shared.HasIntegerId;
 import stroom.util.shared.HasUuid;
 
 import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public class RestUtil {
@@ -14,18 +16,27 @@ public class RestUtil {
     private RestUtil() {
     }
 
+    /**
+     * Used to validate a request argument and throw a {@link BadRequestException} if it is null
+     */
     public static void requireNonNull(final Object object) throws BadRequestException {
         if (object == null) {
             throw new BadRequestException();
         }
     }
 
+    /**
+     * Used to validate a request argument and throw a {@link BadRequestException} if it is null
+     */
     public static void requireNonNull(final Object object, String message) throws BadRequestException {
         if (object == null) {
             throw new BadRequestException(message);
         }
     }
 
+    /**
+     * Used to validate a request argument and throw a {@link BadRequestException} if it is null
+     */
     public static void requireNonNull(final Object object, Supplier<String> messageSupplier) throws BadRequestException {
         if (object == null) {
             throw new BadRequestException(messageSupplier != null
@@ -73,5 +84,31 @@ public class RestUtil {
         return Response
                 .ok(LogUtil.message(msg, args))
                 .build();
+    }
+
+    /**
+     * Ensure value is non null or throw a {@link NotFoundException}. For use in validating
+     * responses.
+     */
+    public static <T> T ensureNonNullResult(final T value,
+                                            final String msg,
+                                            final Object... args) {
+        if (value == null) {
+            throw new NotFoundException(LogUtil.message(msg, args));
+        }
+        return value;
+    }
+
+    /**
+     * Ensure value is not empty or throw a {@link NotFoundException}. For use in validating
+     * responses.
+     */
+    public static <T> T ensureNotEmptyResult(final Optional<T> optValue,
+                                             final String msg,
+                                             final Object... args) {
+        if (optValue == null || optValue.isEmpty()) {
+            throw new NotFoundException(LogUtil.message(msg, args));
+        }
+        return optValue.get();
     }
 }
