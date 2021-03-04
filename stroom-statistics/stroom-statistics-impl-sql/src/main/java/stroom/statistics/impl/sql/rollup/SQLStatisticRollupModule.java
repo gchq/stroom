71b0926e9ab0d0078eb21a95f1d2aa67f794b6be
gcdev373 <gcdev373@users.noreply.github.com>
2021-03-04 18:24:14 +0000

    Apply REST stds to StatisticRollupResourceImpl

--- stroom-statistics/stroom-statistics-impl-sql/src/main/java/stroom/statistics/impl/sql/rollup/SQLStatisticRollupModule.java
+++ stroom-statistics/stroom-statistics-impl-sql/src/main/java/stroom/statistics/impl/sql/rollup/SQLStatisticRollupModule.java
@@ -24,7 +24,8 @@ public class SQLStatisticRollupModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        RestResourcesBinder.create(binder())
-                .bind(StatisticRollupResourceImpl.class);
+        RestResourcesBinder.create(binder()).bind(StatisticRollupResourceImpl.class);
+
+        bind(StatisticRollupService.class).to(StatisticRollupServiceImpl.class);
     }
 }
