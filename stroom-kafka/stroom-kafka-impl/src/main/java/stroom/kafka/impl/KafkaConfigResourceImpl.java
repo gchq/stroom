48a93352222a7eef8587685a2285a6554bbeb9be
gcdev373 <gcdev373@users.noreply.github.com>
2021-03-04 12:09:33 +0000

    Apply Stroom standard for REST to KafkaConfigResourceImpl

--- stroom-kafka/stroom-kafka-impl/src/main/java/stroom/kafka/impl/KafkaConfigResourceImpl.java
+++ stroom-kafka/stroom-kafka-impl/src/main/java/stroom/kafka/impl/KafkaConfigResourceImpl.java
@@ -2,15 +2,17 @@ package stroom.kafka.impl;
 
 import stroom.docref.DocRef;
 import stroom.docstore.api.DocumentResourceHelper;
-import stroom.event.logging.api.DocumentEventLog;
+import stroom.event.logging.rs.api.AutoLogged;
 import stroom.kafka.shared.KafkaConfigDoc;
 import stroom.kafka.shared.KafkaConfigResource;
 import stroom.resource.api.ResourceStore;
-import stroom.security.api.SecurityContext;
 import stroom.util.io.StreamUtil;
 import stroom.util.shared.EntityServiceException;
 import stroom.util.shared.ResourceGeneration;
 import stroom.util.shared.ResourceKey;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -18,31 +20,28 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import javax.inject.Inject;
+import javax.inject.Provider;
 
+@AutoLogged
 public class KafkaConfigResourceImpl implements KafkaConfigResource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConfigResourceImpl.class);
 
-    private final KafkaConfigStore kafkaConfigStore;
-    private final DocumentResourceHelper documentResourceHelper;
-    private final ResourceStore resourceStore;
-    private final DocumentEventLog documentEventLog;
-    private final SecurityContext securityContext;
+    private final Provider<KafkaConfigStore> kafkaConfigStoreProvider;
+    private final Provider<DocumentResourceHelper> documentResourceHelperProvider;
+    private final Provider<ResourceStore> resourceStoreProvider;
 
     @Inject
-    KafkaConfigResourceImpl(final KafkaConfigStore kafkaConfigStore,
-                            final DocumentResourceHelper documentResourceHelper,
-                            final ResourceStore resourceStore,
-                            final DocumentEventLog documentEventLog,
-                            final SecurityContext securityContext) {
-        this.kafkaConfigStore = kafkaConfigStore;
-        this.documentResourceHelper = documentResourceHelper;
-        this.resourceStore = resourceStore;
-        this.documentEventLog = documentEventLog;
-        this.securityContext = securityContext;
+    KafkaConfigResourceImpl(final Provider<KafkaConfigStore> kafkaConfigStoreProvider,
+                            final Provider<DocumentResourceHelper> documentResourceHelperProvider,
+                            final Provider<ResourceStore> resourceStoreProvider) {
+        this.kafkaConfigStoreProvider = kafkaConfigStoreProvider;
+        this.documentResourceHelperProvider = documentResourceHelperProvider;
+        this.resourceStoreProvider = resourceStoreProvider;
     }
 
     @Override
     public KafkaConfigDoc fetch(final String uuid) {
-        return documentResourceHelper.read(kafkaConfigStore, getDocRef(uuid));
+        return documentResourceHelperProvider.get().read(kafkaConfigStoreProvider.get(), getDocRef(uuid));
     }
 
     @Override
@@ -50,7 +49,7 @@ public class KafkaConfigResourceImpl implements KafkaConfigResource {
         if (doc.getUuid() == null || !doc.getUuid().equals(uuid)) {
             throw new EntityServiceException("The document UUID must match the update UUID");
         }
-        return documentResourceHelper.update(kafkaConfigStore, doc);
+        return documentResourceHelperProvider.get().update(kafkaConfigStoreProvider.get(), doc);
     }
 
     private DocRef getDocRef(final String uuid) {
@@ -62,24 +61,20 @@ public class KafkaConfigResourceImpl implements KafkaConfigResource {
 
     @Override
     public ResourceGeneration download(final DocRef kafkaConfigDocRef) {
-        return securityContext.secureResult(() -> {
-            // Get dictionary.
-            final KafkaConfigDoc kafkaConfigDoc = kafkaConfigStore.readDocument(kafkaConfigDocRef);
-            if (kafkaConfigDoc == null) {
-                throw new EntityServiceException("Unable to find kafka config");
-            }
+        final KafkaConfigDoc kafkaConfigDoc = kafkaConfigStoreProvider.get().readDocument(kafkaConfigDocRef);
+        if (kafkaConfigDoc == null) {
+            throw new EntityServiceException("Unable to find kafka config");
+        }
 
-            try {
-                final ResourceKey resourceKey = resourceStore.createTempFile("kafkaConfig.properties");
-                final Path file = resourceStore.getTempFile(resourceKey);
-                Files.writeString(file, kafkaConfigDoc.getData(), StreamUtil.DEFAULT_CHARSET);
-                documentEventLog.download(kafkaConfigDoc, null);
-                return new ResourceGeneration(resourceKey, new ArrayList<>());
+        final ResourceKey resourceKey = resourceStoreProvider.get().createTempFile("kafkaConfig.properties");
+        final Path file = resourceStoreProvider.get().getTempFile(resourceKey);
+        try {
+            Files.writeString(file, kafkaConfigDoc.getData(), StreamUtil.DEFAULT_CHARSET);
+        } catch (IOException e) {
+            LOGGER.error("Unable to download KafkaConfig", e);
+            throw new UncheckedIOException(e);
+        }
 
-            } catch (final IOException e) {
-                documentEventLog.download(kafkaConfigDoc, null);
-                throw new UncheckedIOException(e);
-            }
-        });
+        return new ResourceGeneration(resourceKey, new ArrayList<>());
     }
 }
