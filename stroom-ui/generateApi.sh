89e8b0a8361fff314e9eebe9d5e7e84dab21490f
stroomdev66 <stroomdev66@gmail.com>
2021-04-13 15:33:58 +0100

    Fix `node` and `swagger-typescript-api` versions

--- stroom-ui/generateApi.sh
+++ stroom-ui/generateApi.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Generate typescript API from Swagger schema.
-echo "npx --version"
-npx --version
-echo "npx swagger-typescript-api@8.0.3 -p ../stroom-app/src/main/resources/ui/noauth/swagger/stroom.json -o ./src/api -n stroom.ts"
-npx swagger-typescript-api@8.0.3 -p ../stroom-app/src/main/resources/ui/noauth/swagger/stroom.json -o ./src/api -n stroom.ts
+# Latest version of `wagger-typescript-api` 8.0.3 but doesn't seem to work.
+echo "npx swagger-typescript-api@6.4.2 -p ../stroom-app/src/main/resources/ui/noauth/swagger/stroom.json -o ./src/api -n stroom.ts"
+npx swagger-typescript-api@6.4.2 -p ../stroom-app/src/main/resources/ui/noauth/swagger/stroom.json -o ./src/api -n stroom.ts
