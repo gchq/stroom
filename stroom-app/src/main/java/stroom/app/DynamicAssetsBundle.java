8347639abee7e2d615c04d5e5480922d3b91515b
stroomdev66 <stroomdev66@gmail.com>
2021-05-26 10:19:04 +0100

    #2113 Fix dynamic asset bundle for other devs

--- stroom-app/src/main/java/stroom/app/DynamicAssetsBundle.java
+++ stroom-app/src/main/java/stroom/app/DynamicAssetsBundle.java
@@ -1,9 +1,11 @@
 package stroom.app;
 
+import com.google.common.io.Resources;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.servlets.assets.AssetServlet;
 
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -12,6 +14,23 @@ import java.nio.file.Paths;
 import javax.annotation.Nullable;
 
 public class DynamicAssetsBundle extends AssetsBundle {
+
+    private static final Path assetPath;
+
+    static {
+        try {
+            Path path = Paths.get(Resources.getResource("").toURI()).toAbsolutePath();
+            while (path != null && !path.getFileName().toString().equals("stroom-app")) {
+                path = path.getParent();
+            }
+            if (path != null) {
+                path = path.resolve("src").resolve("main").resolve("resources");
+            }
+            assetPath = path;
+        } catch (final URISyntaxException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
 
     public DynamicAssetsBundle(final String resourcePath,
                                final String uriPath,
@@ -25,21 +44,23 @@ public class DynamicAssetsBundle extends AssetsBundle {
     }
 
     private static class DynamicAssetsServlet extends AssetServlet {
+
         public DynamicAssetsServlet(String resourcePath,
-                            String uriPath,
-                            @Nullable String indexFile,
-                            @Nullable Charset defaultCharset) {
+                                    String uriPath,
+                                    @Nullable String indexFile,
+                                    @Nullable Charset defaultCharset) {
             super(resourcePath, uriPath, indexFile, defaultCharset);
         }
 
         @Override
         protected URL getResourceUrl(final String absoluteRequestedResourcePath) {
-            final Path path = Paths.get("/home/stroomdev66/work/stroom-master-temp4/stroom-app/src/main/resources/" +
-                    absoluteRequestedResourcePath);
-            try {
-                return path.toUri().toURL();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
+            if (assetPath != null) {
+                final Path path = assetPath.resolve(absoluteRequestedResourcePath);
+                try {
+                    return path.toUri().toURL();
+                } catch (final MalformedURLException e) {
+                    e.printStackTrace();
+                }
             }
 
             return super.getResourceUrl(absoluteRequestedResourcePath);
