6d6c9f329e7801912ebd23a1b95de66ab2b13a98
stroomdev66 <stroomdev66@gmail.com>
2022-01-07 10:35:09 +0000

    Java 17

--- stroom-ui/src/api/stroom.ts
+++ stroom-ui/src/api/stroom.ts
@@ -6172,6 +6172,25 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
      * No description
      *
      * @tags Jobs
+     * @name SetNodeJobsEnabled
+     * @summary Sets the enabled state of jobs for the selected node. If both `includeJobs` and `excludeJobs` are unspecified or empty, this action will apply to ALL jobs.
+     * @request PUT:/job/v1/setJobsEnabled/{nodeName}
+     * @secure
+     */
+    setNodeJobsEnabled: (nodeName: string, data: NodeSetJobsEnabledRequest, params: RequestParams = {}) =>
+      this.request<any, NodeSetJobsEnabledResponse>({
+        path: `/job/v1/setJobsEnabled/${nodeName}`,
+        method: "PUT",
+        body: data,
+        secure: true,
+        type: ContentType.Json,
+        ...params,
+      }),
+
+    /**
+     * No description
+     *
+     * @tags Jobs
      * @name SetJobEnabled
      * @summary Sets the enabled status of the job
      * @request PUT:/job/v1/{id}/enabled
@@ -6548,25 +6567,6 @@ export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDa
     setNodePriority: (nodeName: string, data: number, params: RequestParams = {}) =>
       this.request<any, boolean>({
         path: `/node/v1/priority/${nodeName}`,
-        method: "PUT",
-        body: data,
-        secure: true,
-        type: ContentType.Json,
-        ...params,
-      }),
-
-    /**
-     * No description
-     *
-     * @tags Nodes
-     * @name SetNodeJobsEnabled
-     * @summary Sets the enabled state of jobs for the selected node. If both `includeJobs` and `excludeJobs` are unspecified or empty, this action will apply to ALL jobs.
-     * @request PUT:/node/v1/setJobsEnabled/{nodeName}
-     * @secure
-     */
-    setNodeJobsEnabled: (nodeName: string, data: NodeSetJobsEnabledRequest, params: RequestParams = {}) =>
-      this.request<any, NodeSetJobsEnabledResponse>({
-        path: `/node/v1/setJobsEnabled/${nodeName}`,
         method: "PUT",
         body: data,
         secure: true,
