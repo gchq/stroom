185a25687baf23612959a35d88bbd0e120943393
stroomdev66 <stroomdev66@gmail.com>
2022-01-27 11:51:02 +0000

    Improve padding

--- stroom-dashboard/stroom-dashboard-client/src/main/java/stroom/dashboard/client/flexlayout/TabLayout.java
+++ stroom-dashboard/stroom-dashboard-client/src/main/java/stroom/dashboard/client/flexlayout/TabLayout.java
@@ -82,12 +82,12 @@ public class TabLayout extends Composite implements RequiresResize, ProvidesResi
         barOuter.add(buttons);
 
         settings = new Button();
-        settings.setStyleName("fa-button face tabLayout-settingsButton");
+        settings.setStyleName("fa-button face tabLayout-button tabLayout-settingsButton");
         settings.setTitle("Settings");
         buttons.add(settings);
 
         close = new Button();
-        close.setStyleName("fa-button face tabLayout-closeButton");
+        close.setStyleName("fa-button face tabLayout-button tabLayout-closeButton");
         close.setTitle("Close");
         buttons.add(close);
 
