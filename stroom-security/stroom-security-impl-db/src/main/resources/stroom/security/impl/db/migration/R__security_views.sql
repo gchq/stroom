2fec6f8d60497873d6ea7dda03ddc90ef7a7b659
at055612 <at055612@users.noreply.github.com>
2022-07-06 11:13:38 +0100

    gh-2942 Remove doc tbl from v_permission

--- stroom-security/stroom-security-impl-db/src/main/resources/stroom/security/impl/db/migration/R__security_views.sql
+++ stroom-security/stroom-security-impl-db/src/main/resources/stroom/security/impl/db/migration/R__security_views.sql
@@ -5,26 +5,26 @@ SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0;
 
 -- View to show all the app/doc permissions for a user/group including ones
 -- inherited from group memberships
+-- Can't join to doc to get the name as the table is in another module and therefore
+-- potentially in a different db to this view.
 create or replace view v_permission as
 select
-    su.uuid,
-    su.name,
+    su.uuid user_or_group_uuid,
+    su.name user_or_group_name,
     su.is_group,
     null via_group,
-    null doc_name,
-    null doc_type,
+    null doc_uuid,
     ap.permission,
     'App' permission_type
 from app_permission ap
 inner join stroom_user su on su.uuid = ap.user_uuid
 union
 select
-    su_user.uuid,
-    su_user.name,
+    su_user.uuid user_or_group_uuid,
+    su_user.name user_or_group_name,
     su_user.is_group,
     su_grp.name via_group,
-    null doc_name,
-    null doc_type,
+    null doc_uuid,
     ap.permission,
     'App' permission_type
 from app_permission ap
@@ -33,12 +33,11 @@ inner join stroom_user_group sug on su_grp.uuid = sug.group_uuid
 inner join stroom_user su_user on su_user.uuid = sug.user_uuid
 union
 select
-    su.uuid,
-    su.name,
+    su.uuid user_or_group_uuid,
+    su.name user_or_group_name,
     su.is_group,
     null via_group,
-    d.name doc_name,
-    d.type doc_type,
+    dp.doc_uuid,
     dp.permission,
     'Document' permission_type
 from doc_permission dp
@@ -47,20 +46,17 @@ inner join doc d on d.uuid = dp.doc_uuid
 where d.ext = 'meta'
 union
 select
-    su_user.uuid,
-    su_user.name,
+    su_user.uuid user_or_group_uuid,
+    su_user.name user_or_group_name,
     su_user.is_group,
     su_grp.name via_group,
-    d.name doc_name,
-    d.type doc_type,
+    dp.doc_uuid,
     dp.permission,
     'Document' permission_type
 from doc_permission dp
 inner join stroom_user su_grp on su_grp.uuid = dp.user_uuid
 inner join stroom_user_group sug on su_grp.uuid = sug.group_uuid
-inner join stroom_user su_user on su_user.uuid = sug.user_uuid
-inner join doc d on d.uuid = dp.doc_uuid
-where d.ext = 'meta';
+inner join stroom_user su_user on su_user.uuid = sug.user_uuid;
 
 SET SQL_NOTES=@OLD_SQL_NOTES;
 
