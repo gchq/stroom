09b09ad8a135e8f3c34b47eb345cdca0552f4abc
at055612 <at055612@users.noreply.github.com>
2022-07-06 11:25:56 +0100

    gh-2942 Remove doc from view

--- stroom-security/stroom-security-impl-db/src/main/resources/stroom/security/impl/db/migration/R__security_views.sql
+++ stroom-security/stroom-security-impl-db/src/main/resources/stroom/security/impl/db/migration/R__security_views.sql
@@ -8,7 +8,7 @@ SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0;
 -- Can't join to doc to get the name as the table is in another module and therefore
 -- potentially in a different db to this view.
 create or replace view v_permission as
-select
+select -- Direct app perms
     su.uuid user_or_group_uuid,
     su.name user_or_group_name,
     su.is_group,
@@ -19,7 +19,7 @@ select
 from app_permission ap
 inner join stroom_user su on su.uuid = ap.user_uuid
 union
-select
+select -- App perms via group
     su_user.uuid user_or_group_uuid,
     su_user.name user_or_group_name,
     su_user.is_group,
@@ -32,7 +32,7 @@ inner join stroom_user su_grp on su_grp.uuid = ap.user_uuid
 inner join stroom_user_group sug on su_grp.uuid = sug.group_uuid
 inner join stroom_user su_user on su_user.uuid = sug.user_uuid
 union
-select
+select -- Direct doc perms
     su.uuid user_or_group_uuid,
     su.name user_or_group_name,
     su.is_group,
@@ -42,10 +42,8 @@ select
     'Document' permission_type
 from doc_permission dp
 inner join stroom_user su on su.uuid = dp.user_uuid
-inner join doc d on d.uuid = dp.doc_uuid
-where d.ext = 'meta'
 union
-select
+select -- Doc perms via group
     su_user.uuid user_or_group_uuid,
     su_user.name user_or_group_name,
     su_user.is_group,
