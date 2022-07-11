a6d5ec7604b3e1342097b144f79aee5588715707
at055612 <at055612@users.noreply.github.com>
2022-07-11 16:10:46 +0100

    gh-2942 Add v_doc_feed

--- stroom-docstore/stroom-docstore-impl-db/src/main/resources/stroom/docstore/impl/db/migration/R__docstore_views.sql
+++ stroom-docstore/stroom-docstore-impl-db/src/main/resources/stroom/docstore/impl/db/migration/R__docstore_views.sql
@@ -11,12 +11,12 @@ select
     v.uuid,
     v.name,
     v.content_extension,
-    json_extract(v.meta_data, '$.type') doc_type,
-    json_extract(v.meta_data, '$.version') version,
-    from_unixtime(json_extract(v.meta_data, '$.createTimeMs') / 1000) createTime,
-    json_extract(v.meta_data, '$.createUser') createUser,
-    from_unixtime(json_extract(v.meta_data, '$.updateTimeMs') / 1000) updateTime,
-    json_extract(v.meta_data, '$.updateUser') updateUser,
+    json_value(v.meta_data, '$.type') doc_type,
+    json_value(v.meta_data, '$.version') version,
+    from_unixtime(json_value(v.meta_data, '$.createTimeMs') / 1000) create_time,
+    json_value(v.meta_data, '$.createUser') create_user,
+    from_unixtime(json_value(v.meta_data, '$.updateTimeMs') / 1000) update_time,
+    json_value(v.meta_data, '$.updateUser') update_user,
     json_remove(
         v.meta_data,
         '$.uuid',
@@ -26,7 +26,7 @@ select
         '$.createTimeMs',
         '$.createUser',
         '$.updateTimeMs',
-        '$.updateUser') bespoke_meta_data,
+        '$.updateUser') bespoke_meta_data, -- The json without all the common stuff in it
     v.content_data
 from
     (select
@@ -41,6 +41,28 @@ from
         on dm.uuid = dd.uuid
         and dd.ext != dm.ext
     where dm.ext = 'meta') v;
+
+-- A view for just feed docs, extracting all the json keys to columns
+create or replace view v_doc_feed as
+select
+    vd.id,
+    vd.uuid,
+    vd.name,
+    vd.version,
+    vd.create_time,
+    vd.create_user,
+    vd.update_time,
+    vd.update_user,
+    json_value(vd.bespoke_meta_data, '$.description') description,
+    json_value(vd.bespoke_meta_data, '$.classification') classification,
+    json_value(vd.bespoke_meta_data, '$.encoding') encoding,
+    json_value(vd.bespoke_meta_data, '$.contextEncoding') context_encoding,
+    json_value(vd.bespoke_meta_data, '$.retentionDayAge') retention_day_ge,
+    json_value(vd.bespoke_meta_data, '$.reference') is_reference,
+    json_value(vd.bespoke_meta_data, '$.streamType') streamType,
+    json_value(vd.bespoke_meta_data, '$.status') status
+from v_doc vd
+where vd.doc_type = 'Feed';
 
 SET SQL_NOTES=@OLD_SQL_NOTES;
 
