486eb27bfd24219d6f6f75bf927cd20590cbfa13
at055612 <22818309+at055612@users.noreply.github.com>
2022-11-14 13:55:46 +0000

    gh-3076 Add standalone proxy build to debug

--- container_build/gradleBuild.sh
+++ container_build/gradleBuild.sh
@@ -89,6 +89,18 @@ main() {
     test_args=( "test" )
   fi
 
+  // TODO remove this block, just here to diagnose issues.
+  echo "::group::Proxy tests build"
+  echo -e "${GREEN}Do the basic java build${NC}"
+  ./gradlew \
+    "${GRADLE_ARGS[@]}" \
+    --info \
+    --scan \
+    --stacktrace \
+    -PdumpFailedTestXml=true \
+    :stroom-proxy:stroom-proxy-app:test
+  echo "::endgroup::"
+
   # Do the gradle build
   # Use custom gwt compile jvm settings to avoid blowing the ram limit in
   # travis. At time of writing a sudo VM in travis has 7.5gb ram.
