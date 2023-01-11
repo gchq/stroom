1eb5b305272b69e59fd3f61bad7240d672752e7a
p-kimberley <peter@gradata.com.au>
2023-01-11 23:49:29 +1100

    Restore deleted `StringUtil` methods

--- stroom-util-shared/src/main/java/stroom/util/shared/StringUtil.java
+++ stroom-util-shared/src/main/java/stroom/util/shared/StringUtil.java
@@ -52,6 +52,67 @@ public class StringUtil {
         }
     }
 
+    /**
+     * Converts user input, that has been double-quoted to include whitespace,
+     * into the un-quoted value. Any double quotes inside the outermost double
+     * quotes will be escaped with a single backslash. Any Unquoted whitespace
+     * or whitespace outside the outer double quotes will be trimmed.
+     * See the tests for examples.
+     */
+    public static String removeWhitespaceQuoting(final String userText) {
+        final String storedText;
+        if (userText == null || userText.isEmpty()) {
+            storedText = userText;
+        } else {
+            final String trimmedStr = userText.trim();
+            if (trimmedStr.startsWith("\"")
+                    && trimmedStr.endsWith("\"")) {
+
+                final int openQuoteIdx = trimmedStr.indexOf("\"");
+                final int endQuoteIdx = trimmedStr.lastIndexOf("\"");
+                if (openQuoteIdx == endQuoteIdx) {
+                    // '"'
+                    storedText = trimmedStr;
+                } else {
+                    // Everything inside the db quotes, un-escaping any dbl quote used
+                    storedText = trimmedStr.substring(openQuoteIdx + 1, endQuoteIdx)
+                            .replace("\\\"", "\"");
+                }
+            } else {
+                storedText = trimmedStr.replace("\\\"", "\"");
+            }
+        }
+        return storedText;
+    }
+
+    /**
+     * Converts a system stored value into one for display in the UI.
+     * If the value has leading or trailing whitespace it will be quoted and any
+     * double quotes escaped.
+     * See the tests for examples.
+     */
+    public static String addWhitespaceQuoting(final String storedText) {
+        String userText;
+        if (storedText == null || storedText.isEmpty()) {
+            userText = storedText;
+        } else {
+            final String escapedText = storedText.replace("\"", "\\\"");
+            if (storedText.startsWith(" ")
+                    || storedText.startsWith("\t")
+                    || storedText.endsWith(" ")
+                    || storedText.endsWith("\t")) {
+                // leading/trailing whitespace so dbl quote the whole thing
+                // ' he said "hello" ' => '" he said \"hello\" "'
+                userText = "\""
+                        + escapedText
+                        + "\"";
+            } else {
+                userText = escapedText;
+            }
+        }
+        return userText;
+    }
+
     public static String pluralSuffix(final int count) {
         return count > 1
                 ? "s"
