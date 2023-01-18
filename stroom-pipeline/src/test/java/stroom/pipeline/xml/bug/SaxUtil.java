f83fa156d5dccd8e92feb7389bc2454ee9b7deb1
stroomdev66 <stroomdev66@gmail.com>
2023-01-18 18:03:56 +0000

    #3194 Fix for JDK XML 1.1 parser bug

--- stroom-pipeline/src/test/java/stroom/pipeline/xml/bug/SaxUtil.java
+++ stroom-pipeline/src/test/java/stroom/pipeline/xml/bug/SaxUtil.java
@@ -1,43 +1,22 @@
 package stroom.pipeline.xml.bug;
 
+import stroom.util.xml.SAXParserFactoryFactory;
+
 import org.xml.sax.ContentHandler;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXNotRecognizedException;
-import org.xml.sax.SAXNotSupportedException;
 import org.xml.sax.XMLReader;
 
 import java.io.Reader;
 import javax.xml.XMLConstants;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 
 public class SaxUtil {
-
-    public static SAXParserFactory newParserFactory() {
-        final SAXParserFactory factory = SAXParserFactory.newInstance();
-        secureProcessing(factory);
-        factory.setNamespaceAware(true);
-        factory.setValidating(false);
-        return factory;
-    }
-
-    private static void secureProcessing(final SAXParserFactory factory) {
-        try {
-            factory.setFeature(
-                    XMLConstants.FEATURE_SECURE_PROCESSING,
-                    false);
-
-        } catch (final ParserConfigurationException | SAXNotRecognizedException | SAXNotSupportedException e) {
-            System.err.println(e.getMessage());
-        }
-    }
 
     public static void parse(final Reader input,
                              final ContentHandler contentHandler,
                              final EntityResolver resolver) throws Exception {
-        final SAXParser parser = newParserFactory().newSAXParser();
+        final SAXParser parser = SAXParserFactoryFactory.newInstance().newSAXParser();
 
         final XMLReader xmlReader = parser.getXMLReader();
         xmlReader.setEntityResolver(resolver);
