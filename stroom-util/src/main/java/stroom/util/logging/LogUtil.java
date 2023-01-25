82cdbbaeda51a36f2789b9772e6e60bd3e0674d7
at055612 <at055612@users.noreply.github.com>
2023-01-25 14:20:49 +0000

    Add inbox func from gh-3136-ref-no-eff-streams-mk3

--- stroom-util/src/main/java/stroom/util/logging/LogUtil.java
+++ stroom-util/src/main/java/stroom/util/logging/LogUtil.java
@@ -3,6 +3,8 @@ package stroom.util.logging;
 import com.google.common.base.Strings;
 import org.slf4j.helpers.MessageFormatter;
 
+import java.util.stream.Collectors;
+
 public final class LogUtil {
 
     private LogUtil() {
@@ -42,21 +44,56 @@ public final class LogUtil {
      * Constructs a formatted message string using a format string that takes
      * the same placeholders as SLF4J, e.g.
      * "Function called with name {} and value {}"
-     * This constructed message is placed inside a box after a line break
+     * This constructed message is placed inside a box after a line break.
      *
      * @param format SLF4J style format string
      * @param args   The values for any placeholders in the message format
      * @return A formatted message in a box on a new line
      */
+    public static String inBoxOnNewLine(final String format, final Object... args) {
+        return inBox(format, true, args);
+    }
+
+    /**
+     * Constructs a formatted message string using a format string that takes
+     * the same placeholders as SLF4J, e.g.
+     * "Function called with name {} and value {}"
+     * This constructed message is placed inside a box.
+     *
+     * @param format SLF4J style format string
+     * @param args   The values for any placeholders in the message format
+     * @return A formatted message in a box.
+     */
     public static String inBox(final String format, final Object... args) {
-        final String text = message(format, args);
+        return inBox(format, false, args);
+    }
 
-        final String line = Strings.repeat("-", text.length() + 6);
-        final String boxFormat = """
+    private static String inBox(final String format,
+                                final boolean addNewLine,
+                                final Object... args) {
+        if (format == null || format.isBlank()) {
+            return "";
+        } else {
+            final String text = message(format, args);
 
+            final int maxLineLen = text.lines()
+                    .mapToInt(String::length)
+                    .max()
+                    .orElse(0);
+
+            final String topBottomBorder = Strings.repeat("-", maxLineLen + 6);
+            final String boxText = text.lines()
+                    .map(line -> {
+                        final String padding = Strings.repeat(" ", maxLineLen - line.length());
+                        return "|  " + line + padding + "  |";
+                    })
+                    .collect(Collectors.joining("\n"));
+
+            final String boxFormat = """
+                {}{}
                 {}
-                |  {}  |
                 {}""";
-        return message(boxFormat, line, text, line);
+            return message(boxFormat, (addNewLine ? "\n" : ""), topBottomBorder, boxText, topBottomBorder);
+        }
     }
 }
