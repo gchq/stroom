bb752de90ae4c535f2dabcda373528f02e29b477
at055612 <at055612@users.noreply.github.com>
2023-03-14 17:40:58 +0000

    gh-3305 Change format of idx/data vol limit to 3 sig fig

---
+++ stroom-core-client-widget/src/main/java/stroom/cell/info/client/ToolTipCell.java
@@ -0,0 +1,48 @@
+package stroom.cell.info.client;
+
+import stroom.cell.info.client.ToolTipCell.ToolTipCellValue;
+
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+
+// TODO: 14/03/2023 This was the beginings of a cell for showing the byte values, e.g.
+//  volume/stream sizes, however it probably ought to become a ByteValueCell and ByteValueColumn
+//  with a standard approach for formatting the byte value and the tooltip. Leaving it here
+//  in case such a thing ever happens.
+/**
+ * A text cell with a tool tip
+ */
+public class ToolTipCell extends AbstractCell<ToolTipCellValue> {
+
+    @Override
+    public void render(Context context, ToolTipCellValue value, SafeHtmlBuilder sb) {
+        if (value != null) {
+            sb
+                    .appendHtmlConstant("<div title=\"")
+                    .appendEscaped(value.getToolTipValue())
+                    .appendHtmlConstant("\">")
+                    .append(SafeHtmlUtils.fromString(value.cellValue));
+        }
+    }
+
+    public static class ToolTipCellValue {
+        private final String cellValue;
+        private final String toolTipValue;
+
+        public static ToolTipCellValue EMPTY = new ToolTipCellValue("", "");
+
+        public ToolTipCellValue(final String cellValue, final String toolTipValue) {
+            this.cellValue = cellValue;
+            this.toolTipValue = toolTipValue;
+        }
+
+        public String getCellValue() {
+            return cellValue;
+        }
+
+        public String getToolTipValue() {
+            return toolTipValue;
+        }
+    }
+}
