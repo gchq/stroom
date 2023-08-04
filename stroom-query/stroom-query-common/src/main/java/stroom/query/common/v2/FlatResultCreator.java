c9599beae7663318cdf2436db265c76a2036df13
stroomdev66 <stroomdev66@gmail.com>
2023-08-04 14:56:31 +0100

    #3660 Fix high search memory usage

--- stroom-query/stroom-query-common/src/main/java/stroom/query/common/v2/FlatResultCreator.java
+++ stroom-query/stroom-query-common/src/main/java/stroom/query/common/v2/FlatResultCreator.java
@@ -44,7 +44,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 
 public class FlatResultCreator implements ResultCreator {
 
