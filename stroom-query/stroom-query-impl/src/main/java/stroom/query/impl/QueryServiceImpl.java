6f5c72b619a951208fedadfa1241e472d32509a7
stroomdev66 <stroomdev66@gmail.com>
2023-09-13 15:31:00 +0100

    #3746 Create streaming analytics by default

--- stroom-query/stroom-query-impl/src/main/java/stroom/query/impl/QueryServiceImpl.java
+++ stroom-query/stroom-query-impl/src/main/java/stroom/query/impl/QueryServiceImpl.java
@@ -24,14 +24,17 @@ import stroom.datasource.api.v2.DataSource;
 import stroom.docref.DocRef;
 import stroom.docstore.api.DocumentResourceHelper;
 import stroom.event.logging.rs.api.AutoLogged;
+import stroom.expression.api.DateTimeSettings;
 import stroom.node.api.NodeInfo;
 import stroom.query.api.v2.Field;
+import stroom.query.api.v2.Param;
 import stroom.query.api.v2.Query;
 import stroom.query.api.v2.QueryKey;
 import stroom.query.api.v2.ResultRequest;
 import stroom.query.api.v2.SearchRequest;
 import stroom.query.api.v2.SearchResponse;
 import stroom.query.api.v2.TableSettings;
+import stroom.query.api.v2.TimeRange;
 import stroom.query.common.v2.ResultStoreManager;
 import stroom.query.language.DataSourceResolver;
 import stroom.query.language.SearchRequestBuilder;
@@ -329,17 +332,26 @@ class QueryServiceImpl implements QueryService {
         final QueryKey queryKey = searchRequest.getQueryKey();
         final String query = searchRequest.getQuery();
         final QueryContext queryContext = searchRequest.getQueryContext();
+
+        List<Param> params = null;
+        TimeRange timeRange = null;
+        DateTimeSettings dateTimeSettings = null;
+        if (queryContext != null) {
+            params = queryContext.getParams();
+            timeRange = queryContext.getTimeRange();
+            dateTimeSettings = queryContext.getDateTimeSettings();
+        }
         final Query sampleQuery = Query
                 .builder()
-                .params(queryContext.getParams())
-                .timeRange(queryContext.getTimeRange())
+                .params(params)
+                .timeRange(timeRange)
                 .build();
         final SearchRequest sampleRequest = new SearchRequest(
                 searchRequest.getSearchRequestSource(),
                 queryKey,
                 sampleQuery,
                 null,
-                queryContext.getDateTimeSettings(),
+                dateTimeSettings,
                 searchRequest.isIncremental());
         SearchRequest mappedRequest = searchRequestBuilder.create(query, sampleRequest);
         mappedRequest = dataSourceResolver.resolveDataSource(mappedRequest);
