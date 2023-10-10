4642996e1ae676fb8485b502e769836aa7c8d1d5
stroomdev66 <stroomdev66@gmail.com>
2023-10-10 17:41:30 +0100

    #3830 Add S3 data storage option

--- stroom-data/stroom-data-store-impl-fs/src/main/java/stroom/data/store/impl/fs/S3Manager.java
+++ stroom-data/stroom-data-store-impl-fs/src/main/java/stroom/data/store/impl/fs/S3Manager.java
@@ -37,6 +37,8 @@ import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Request;
+import software.amazon.awssdk.services.s3.model.S3Response;
 import software.amazon.awssdk.services.s3.model.Tag;
 import software.amazon.awssdk.services.s3.model.Tagging;
 import software.amazon.awssdk.transfer.s3.S3TransferManager;
@@ -55,24 +57,13 @@ import java.time.Duration;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class S3Manager {
 
     private static final LambdaLogger LOGGER = LambdaLoggerFactory.getLogger(S3Manager.class);
-//
-//    private static final Map<String, String> EXTENSION_MAP = new HashMap<>();
-//
-//    static {
-//        EXTENSION_MAP.put(InternalStreamTypeNames.SEGMENT_INDEX, "seg");
-//        EXTENSION_MAP.put(InternalStreamTypeNames.BOUNDARY_INDEX, "bdy");
-//
-//        // Child types
-//        EXTENSION_MAP.put(InternalStreamTypeNames.MANIFEST, "mf");
-//        EXTENSION_MAP.put(StreamTypeNames.META, "meta");
-//        EXTENSION_MAP.put(StreamTypeNames.CONTEXT, "ctx");
-//    }
 
     private static final Pattern S3_NAME_PATTERN = Pattern.compile("[^a-z0-9]");
 
@@ -254,28 +245,18 @@ public class S3Manager {
             return tryUpload(bucketName, key, meta, attributeMap, source);
         } catch (final RuntimeException e) {
             if (s3ClientConfig.isCreateBuckets()) {
-                LOGGER.debug(e::getMessage, e);
+                debug("Error uploading: ", bucketName, key, e);
 
                 // If we are creating buckets then try to create the bucket and upload again.
                 try {
                     createBucket(bucketName);
                     return tryUpload(bucketName, key, meta, attributeMap, source);
                 } catch (final RuntimeException e2) {
-                    LOGGER.error(() -> "Error uploading: bucketName=" +
-                            bucketName +
-                            ", key=" +
-                            key +
-                            ", message=" +
-                            e2.getMessage(), e2);
+                    error("Error uploading: ", bucketName, key, e2);
                     throw e2;
                 }
             } else {
-                LOGGER.error(() -> "Error uploading: bucketName=" +
-                        bucketName +
-                        ", key=" +
-                        key +
-                        ", message=" +
-                        e.getMessage(), e);
+                error("Error uploading: ", bucketName, key, e);
                 throw e;
             }
         }
@@ -287,10 +268,9 @@ public class S3Manager {
                                        final AttributeMap attributeMap,
                                        final Path source) {
         final PutObjectRequest request = createPutObjectRequest(bucketName, key, meta, attributeMap);
-        final PutObjectResponse response;
-        LOGGER.debug(() -> "Uploading: bucketName=" + bucketName + ", key=" + key);
-        LOGGER.trace(() -> "Upload request: " + request);
+        logRequest("Uploading: ", bucketName, key, request);
 
+        final PutObjectResponse response;
         if (s3ClientConfig.isAsync()) {
             try (final S3AsyncClient s3AsyncClient = createAsyncClient(s3ClientConfig)) {
                 if (s3ClientConfig.isMultipart()) {
@@ -309,7 +289,10 @@ public class S3Manager {
                         final FileUpload fileUpload = transferManager.uploadFile(uploadFileRequest);
 
                         final CompletedFileUpload uploadResult = fileUpload.completionFuture().join();
-                        LOGGER.debug(() -> "Upload result: " + uploadResult);
+                        LOGGER.debug(() -> "Upload result: " +
+                                getDebugIdentity(bucketName, key) +
+                                ", result=" +
+                                uploadResult);
                         response = uploadResult.response();
                     }
 
@@ -323,34 +306,33 @@ public class S3Manager {
             }
         }
 
-        LOGGER.debug(() -> "Uploaded: bucketName=" + bucketName + ", key=" + key);
-        LOGGER.trace(() -> "Upload response: " + response);
+        logResponse("Uploaded: ", bucketName, key, response);
         return response;
     }
 
     private void createBucket(final String bucketName) {
         final CreateBucketRequest request = CreateBucketRequest.builder().bucket(bucketName).build();
+        logRequest("Creating bucket: ", bucketName, null, request);
+
         final CreateBucketResponse response;
-        LOGGER.debug(() -> "Creating bucket: " + bucketName);
-        LOGGER.trace(() -> "Create bucket request: " + request);
         if (s3ClientConfig.isAsync()) {
             try (final S3AsyncClient s3AsyncClient = createAsyncClient(s3ClientConfig)) {
                 response = s3AsyncClient.createBucket(
                         request).join();
             } catch (final S3Exception e) {
-                LOGGER.error(e::getMessage, e);
+                error("Error creating bucket: ", bucketName, null, e);
                 throw e;
             }
         } else {
             try (final S3Client s3Client = createClient(s3ClientConfig)) {
                 response = s3Client.createBucket(request);
             } catch (final S3Exception e) {
-                LOGGER.error(e::getMessage, e);
+                error("Error creating bucket: ", bucketName, null, e);
                 throw e;
             }
         }
-        LOGGER.debug(() -> "Created bucket: " + bucketName);
-        LOGGER.trace(() -> "Created bucket response: " + response);
+
+        logResponse("Created bucket: ", bucketName, null, response);
     }
 
     public GetObjectResponse download(final Meta meta,
@@ -361,10 +343,9 @@ public class S3Manager {
                 .bucket(bucketName)
                 .key(key)
                 .build();
-        final GetObjectResponse response;
-        LOGGER.debug(() -> "Downloading: bucketName=" + bucketName + ", key=" + key);
-        LOGGER.trace(() -> "Download request: " + request);
+        logRequest("Downloading: ", bucketName, key, request);
 
+        final GetObjectResponse response;
         if (s3ClientConfig.isAsync()) {
             try (final S3AsyncClient s3AsyncClient = createAsyncClient(s3ClientConfig)) {
                 if (s3ClientConfig.isMultipart()) {
@@ -383,7 +364,10 @@ public class S3Manager {
                         final FileDownload downloadFile = transferManager.downloadFile(downloadFileRequest);
 
                         final CompletedFileDownload downloadResult = downloadFile.completionFuture().join();
-                        LOGGER.debug(() -> "Download result: " + downloadResult);
+                        LOGGER.debug(() -> "Download result: " +
+                                getDebugIdentity(bucketName, key) +
+                                ", result=" +
+                                downloadResult);
                         response = downloadResult.response();
                     }
 
@@ -391,25 +375,19 @@ public class S3Manager {
                     response = s3AsyncClient.getObject(request, dest).join();
                 }
             } catch (final RuntimeException e) {
-                LOGGER.error(e::getMessage, e);
+                error("Error downloading: ", bucketName, key, e);
                 throw e;
             }
         } else {
             try (final S3Client s3Client = createClient(s3ClientConfig)) {
                 response = s3Client.getObject(request, dest);
             } catch (final RuntimeException e) {
-                LOGGER.error(() -> "Error downloading: bucketName=" +
-                        bucketName +
-                        ", key=" +
-                        key +
-                        ", message=" +
-                        e.getMessage(), e);
+                error("Error downloading: ", bucketName, key, e);
                 throw e;
             }
         }
 
-        LOGGER.debug(() -> "Downloaded: bucketName=" + bucketName + ", key=" + key);
-        LOGGER.trace(() -> "Download response: " + response);
+        logResponse("Downloaded: ", bucketName, key, response);
         return response;
     }
 
@@ -420,28 +398,26 @@ public class S3Manager {
                 .bucket(bucketName)
                 .key(key)
                 .build();
-        final DeleteObjectResponse response;
-        LOGGER.debug(() -> "Deleting: bucketName=" + bucketName + ", key=" + key);
-        LOGGER.trace(() -> "Delete request: " + request);
+        logRequest("Deleting: ", bucketName, key, request);
 
+        final DeleteObjectResponse response;
         if (s3ClientConfig.isAsync()) {
             try (final S3AsyncClient s3AsyncClient = createAsyncClient(s3ClientConfig)) {
                 response = s3AsyncClient.deleteObject(request).join();
             } catch (final S3Exception e) {
-                LOGGER.error(e::getMessage, e);
+                error("Error deleting: ", bucketName, key, e);
                 throw e;
             }
         } else {
             try (final S3Client s3Client = createClient(s3ClientConfig)) {
                 response = s3Client.deleteObject(request);
             } catch (final S3Exception e) {
-                LOGGER.error(e::getMessage, e);
+                error("Error deleting: ", bucketName, key, e);
                 throw e;
             }
         }
 
-        LOGGER.debug(() -> "Deleted: bucketName=" + bucketName + ", key=" + key);
-        LOGGER.trace(() -> "Delete response: " + response);
+        logResponse("Deleted: ", bucketName, key, response);
         return response;
     }
 
@@ -495,5 +471,48 @@ public class S3Manager {
                 .tagging(createTags(meta))
                 .metadata(metadata)
                 .build();
+    }
+
+    private void logRequest(final String message,
+                            final String bucketName,
+                            final String key,
+                            final S3Request request) {
+        LOGGER.debug(() -> message + getDebugIdentity(bucketName, key));
+        LOGGER.trace(() -> message + getDebugIdentity(bucketName, key) + ", request=" + request);
+    }
+
+    private void logResponse(final String message,
+                             final String bucketName,
+                             final String key,
+                             final S3Response response) {
+        LOGGER.debug(() -> message + getDebugIdentity(bucketName, key));
+        LOGGER.trace(() -> message + getDebugIdentity(bucketName, key) + ", response=" + response);
+    }
+
+    private void debug(final String message,
+                       final String bucketName,
+                       final String key,
+                       final Exception e) {
+        LOGGER.debug(() -> message +
+                getDebugIdentity(bucketName, key) +
+                ", message=" +
+                e.getMessage(), e);
+    }
+
+    private void error(final String message,
+                       final String bucketName,
+                       final String key,
+                       final Exception e) {
+        LOGGER.error(() -> message +
+                getDebugIdentity(bucketName, key) +
+                ", message=" +
+                e.getMessage(), e);
+    }
+
+    private String getDebugIdentity(final String bucketName,
+                                    final String key) {
+        return "bucketName=" +
+                bucketName +
+                Optional.ofNullable(key).map(k -> ", key=" + k).orElse("");
     }
 }
