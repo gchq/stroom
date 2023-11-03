2230512486bf97304e317badba2539f184b9dfe5
at055612 <22818309+at055612@users.noreply.github.com>
2023-11-03 14:29:46 +0000

    Change the snippets files into json format
    XML snippets are broken for some reason

--- stroom-core-client-widget/src/main/java/edu/ycp/cs/dh/acegwt/public/ace/snippets/markdown.js
+++ stroom-core-client-widget/src/main/java/edu/ycp/cs/dh/acegwt/public/ace/snippets/markdown.js
@@ -1,146 +1,180 @@
 define("ace/snippets/markdown",["require","exports","module"], function(require, exports, module) {
 "use strict";
 
+// AT 23/08/23 Changed this file as the default markdown snippets are not useful and geared
+// towards github markdown
 
-// AT 23/08/23 Commented the default snippets out as the are for github markdown I think, so have crafted my
-// own
-exports.snippetText = "# Markdown\n\
-snippet h1\n\
-\t# ${1:heading}\n\
-\t\n\
-\t${0}\n\
-snippet h2\n\
-\t## ${1:heading}\n\
-\t\n\
-\t${0}\n\
-snippet h3\n\
-\t### ${1:heading}\n\
-\t\n\
-\t${0}\n\
-snippet h4\n\
-\t#### ${1:heading}\n\
-\t\n\
-\t${0}\n\
-snippet h5\n\
-\t##### ${1:heading}\n\
-\t\n\
-\t${0}\n\
-snippet fence\n\
-\t```${1:language}\n\
-\t${2}\n\
-\t```${0}\n\
-snippet xml\n\
-\t```xml\n\
-\t${2}\n\
-\t```${0}\n\
-snippet text\n\
-\t```text\n\
-\t${2}\n\
-\t```${0}\n\
-snippet inline\n\
-\t`${1:code}`${0}\n\
-snippet b\n\
-\t**${1:bold_text}**${0}\n\
-snippet i\n\
-\t_${1:italic_text}_${0}\n\
-snippet s\n\
-\t~~${1:strikethrough_text}~~${0}\n\
-snippet bold-italic\n\
-\t***${1:bold_italic_text}***${0}\n\
-snippet link\n\
-\t[${1:title}](${2:url})${0}\n\
-";
+// $1 / ${1} is numbered tab position (with 1 being first) with no default text.
+// ${1:defaultText} is a numbered tab position with default text that is replaced on typing
+// $0 is final tab position
+// tabTrigger is the bit you type then hit tab to trigger the snippet.
 
-//exports.snippetText = "# Markdown\n\
-//\n\
-//# Includes octopress (http://octopress.org/) snippets\n\
-//\n\
-//snippet [\n\
-//	[${1:text}](http://${2:address} \"${3:title}\")\n\
-//snippet [*\n\
-//	[${1:link}](${2:`@*`} \"${3:title}\")${4}\n\
-//\n\
-//snippet [:\n\
-//	[${1:id}]: http://${2:url} \"${3:title}\"\n\
-//snippet [:*\n\
-//	[${1:id}]: ${2:`@*`} \"${3:title}\"\n\
-//\n\
-//snippet ![\n\
-//	![${1:alttext}](${2:/images/image.jpg} \"${3:title}\")\n\
-//snippet ![*\n\
-//	![${1:alt}](${2:`@*`} \"${3:title}\")${4}\n\
-//\n\
-//snippet ![:\n\
-//	![${1:id}]: ${2:url} \"${3:title}\"\n\
-//snippet ![:*\n\
-//	![${1:id}]: ${2:`@*`} \"${3:title}\"\n\
-//\n\
-//snippet ===\n\
-//regex /^/=+/=*//\n\
-//	${PREV_LINE/./=/g}\n\
-//	\n\
-//	${0}\n\
-//snippet ---\n\
-//regex /^/-+/-*//\n\
-//	${PREV_LINE/./-/g}\n\
-//	\n\
-//	${0}\n\
-//snippet blockquote\n\
-//	{% blockquote %}\n\
-//	${1:quote}\n\
-//	{% endblockquote %}\n\
-//\n\
-//snippet blockquote-author\n\
-//	{% blockquote ${1:author}, ${2:title} %}\n\
-//	${3:quote}\n\
-//	{% endblockquote %}\n\
-//\n\
-//snippet blockquote-link\n\
-//	{% blockquote ${1:author} ${2:URL} ${3:link_text} %}\n\
-//	${4:quote}\n\
-//	{% endblockquote %}\n\
-//\n\
-//snippet bt-codeblock-short\n\
-//	```\n\
-//	${1:code_snippet}\n\
-//	```\n\
-//\n\
-//snippet bt-codeblock-full\n\
-//	``` ${1:language} ${2:title} ${3:URL} ${4:link_text}\n\
-//	${5:code_snippet}\n\
-//	```\n\
-//\n\
-//snippet codeblock-short\n\
-//	{% codeblock %}\n\
-//	${1:code_snippet}\n\
-//	{% endcodeblock %}\n\
-//\n\
-//snippet codeblock-full\n\
-//	{% codeblock ${1:title} lang:${2:language} ${3:URL} ${4:link_text} %}\n\
-//	${5:code_snippet}\n\
-//	{% endcodeblock %}\n\
-//\n\
-//snippet gist-full\n\
-//	{% gist ${1:gist_id} ${2:filename} %}\n\
-//\n\
-//snippet gist-short\n\
-//	{% gist ${1:gist_id} %}\n\
-//\n\
-//snippet img\n\
-//	{% img ${1:class} ${2:URL} ${3:width} ${4:height} ${5:title_text} ${6:alt_text} %}\n\
-//\n\
-//snippet youtube\n\
-//	{% youtube ${1:video_id} %}\n\
-//\n\
-//# The quote should appear only once in the text. It is inherently part of it.\n\
-//# See http://octopress.org/docs/plugins/pullquote/ for more info.\n\
-//\n\
-//snippet pullquote\n\
-//	{% pullquote %}\n\
-//	${1:text} {\" ${2:quote} \"} ${3:text}\n\
-//	{% endpullquote %}\n\
-//";
+// This file needs to be included in app.html as a <script> tag
+
+exports.snippets = [
+    {
+        "tabTrigger": "h1",
+        "name": "Heading level 1",
+        "content": "# ${1:heading}\n\n${0}"
+    },
+    {
+        "tabTrigger": "h2",
+        "name": "Heading level 2",
+        "content": "## ${1:heading}\n\n${0}"
+    },
+    {
+        "tabTrigger": "h3",
+        "name": "Heading level 3",
+        "content": "### ${1:heading}\n\n${0}"
+    },
+    {
+        "tabTrigger": "h4",
+        "name": "Heading level 4",
+        "content": "#### ${1:heading}\n\n${0}"
+    },
+    {
+        "tabTrigger": "h5",
+        "name": "Heading level 5",
+        "content": "##### ${1:heading}\n\n${0}"
+    },
+    {
+        "tabTrigger": "h6",
+        "name": "Heading level 6",
+        "content": "###### ${1:heading}\n\n${0}"
+    },
+    {
+        "tabTrigger": "fence",
+        "name": "Fenced Block",
+        "content": "```${1:language}\n${2}\n```\n${0}"
+    },
+    {
+        "tabTrigger": "xml",
+        "name": "Fenced block of XML",
+        "content": "```xml\n${1}\n```\n${0}"
+    },
+    {
+        "tabTrigger": "text",
+        "name": "Fenced block of plain text",
+        "content": "```text\n${1}\n```\n${0}"
+    },
+    {
+        "tabTrigger": "inline",
+        "name": "Inline code",
+        "content": "`${1:code}`${0}"
+    },
+    {
+        "tabTrigger": "b",
+        "name": "Bold text",
+        "content": "**${1:bold_text}**${0}"
+    },
+    {
+        "tabTrigger": "i",
+        "name": "Italic text",
+        "content": "_${1:italic_text}_${0}"
+    },
+    {
+        "tabTrigger": "s",
+        "name": "Strike-through text",
+        "content": "~~${1:strikethrough_text}~~${0}"
+    },
+    {
+        "tabTrigger": "bi",
+        "name": "Bold italic text",
+        "content": "***${1:bold_italic_text}***${0}"
+    },
+];
+
 exports.scope = "markdown";
+
+//exports.snippetText = "# Markdownn
+//\n
+//# Includes octopress (http://octopress.org/) snippets\n
+//\n
+//snippet [\n
+//	[${1:text}](http://${2:address} \"${3:title}\")\n
+//snippet [*\n
+//	[${1:link}](${2:`@*`} \"${3:title}\")${4}\n
+//\n
+//snippet [:\n
+//	[${1:id}]: http://${2:url} \"${3:title}\"\n
+//snippet [:*\n
+//	[${1:id}]: ${2:`@*`} \"${3:title}\"\n
+//\n
+//snippet ![\n
+//	![${1:alttext}](${2:/images/image.jpg} \"${3:title}\")\n
+//snippet ![*\n
+//	![${1:alt}](${2:`@*`} \"${3:title}\")${4}\n
+//\n
+//snippet ![:\n
+//	![${1:id}]: ${2:url} \"${3:title}\"\n
+//snippet ![:*\n
+//	![${1:id}]: ${2:`@*`} \"${3:title}\"\n
+//\n
+//snippet ===\n
+//regex /^/=+/=*//\n
+//	${PREV_LINE/./=/g}\n
+//	\n
+//	${0}\n
+//snippet ---\n
+//regex /^/-+/-*//\n
+//	${PREV_LINE/./-/g}\n
+//	\n
+//	${0}\n
+//snippet blockquote\n
+//	{% blockquote %}\n
+//	${1:quote}\n
+//	{% endblockquote %}\n
+//\n
+//snippet blockquote-author\n
+//	{% blockquote ${1:author}, ${2:title} %}\n
+//	${3:quote}\n
+//	{% endblockquote %}\n
+//\n
+//snippet blockquote-link\n
+//	{% blockquote ${1:author} ${2:URL} ${3:link_text} %}\n
+//	${4:quote}\n
+//	{% endblockquote %}\n
+//\n
+//snippet bt-codeblock-short\n
+//	```\n
+//	${1:code_snippet}\n
+//	```\n
+//\n
+//snippet bt-codeblock-full\n
+//	``` ${1:language} ${2:title} ${3:URL} ${4:link_text}\n
+//	${5:code_snippet}\n
+//	```\n
+//\n
+//snippet codeblock-short\n
+//	{% codeblock %}\n
+//	${1:code_snippet}\n
+//	{% endcodeblock %}\n
+//\n
+//snippet codeblock-full\n
+//	{% codeblock ${1:title} lang:${2:language} ${3:URL} ${4:link_text} %}\n
+//	${5:code_snippet}\n
+//	{% endcodeblock %}\n
+//\n
+//snippet gist-full\n
+//	{% gist ${1:gist_id} ${2:filename} %}\n
+//\n
+//snippet gist-short\n
+//	{% gist ${1:gist_id} %}\n
+//\n
+//snippet img\n
+//	{% img ${1:class} ${2:URL} ${3:width} ${4:height} ${5:title_text} ${6:alt_text} %}\n
+//\n
+//snippet youtube\n
+//	{% youtube ${1:video_id} %}\n
+//\n
+//# The quote should appear only once in the text. It is inherently part of it.\n
+//# See http://octopress.org/docs/plugins/pullquote/ for more info.\n
+//\n
+//snippet pullquote\n
+//	{% pullquote %}\n
+//	${1:text} {\" ${2:quote} \"} ${3:text}\n
+//	{% endpullquote %}\n
+//";
 
 });                (function() {
                     window.require(["ace/snippets/markdown"], function(m) {
--- stroom-core-client-widget/src/main/java/edu/ycp/cs/dh/acegwt/public/ace/snippets/xml.js
+++ stroom-core-client-widget/src/main/java/edu/ycp/cs/dh/acegwt/public/ace/snippets/xml.js
@@ -10,80 +10,215 @@ define("ace/snippets/xml",["require","exports","module"], function(require, expo
 // can't parse them.
 // See also https://github.com/gchq/stroom/issues/1745
 
-exports.snippetText = "# XSLT Snippets\n\
-snippet apply-templates with-param\n\
-	<xsl:apply-templates select=\"${1:*}\">\n\
-		<xsl:with-param name=\"${2:param}\">${3}</xsl:with-param>${4}\n\
-	</xsl:apply-templates>${5}\n\
-snippet apply-templates sort-by\n\
-	<xsl:apply-templates select=\"${1:*}\">\n\
-		<xsl:sort select=\"${2:node}\" order=\"${3:ascending}\" data-type=\"${4:text}\">${5}\n\
-	</xsl:apply-templates>${6}\n\
-snippet apply-templates plain\n\
-	<xsl:apply-templates select=\"${1:*}\" />${2}\n\
-snippet attribute blank\n\
-	<xsl:attribute name=\"${1:name}\">${2}</xsl:attribute>${3}\n\
-snippet attribute value-of\n\
-	<xsl:attribute name=\"${1:name}\">\n\
-		<xsl:value-of select=\"${2:*}\" />\n\
-	</xsl:attribute>${3}\n\
-snippet call-template\n\
-	<xsl:call-template name=\"${1:template}\" />\n\
-snippet call-template with-param\n\
-	<xsl:call-template name=\"${1:template}\">\n\
-		<xsl:with-param name=\"${2:param}\">${3}</xsl:with-param>${4}\n\
-	</xsl:call-template>${5}\n\
-snippet choose\n\
-	<xsl:choose>\n\
-		<xsl:when test=\"${1:value}\">\n\
-			${2}\n\
-		</xsl:when>${3}\n\
-	</xsl:choose>\n\
-snippet copy-of\n\
-	<xsl:copy-of select=\"${1:*}\" />${2}\n\
-snippet for-each\n\
-	<xsl:for-each select=\"${1:*}\">${2}\n\
-	</xsl:for-each>${3}\n\
-snippet if\n\
-	<xsl:if test=\"${1:test}\">${2}\n\
-	</xsl:if>${3}\n\
-snippet import\n\
-	<xsl:import href=\"${1:stylesheet}\" />${2}\n\
-snippet include\n\
-	<xsl:include href=\"${1:stylesheet}\" />${2}\n\
-snippet otherwise\n\
-	<xsl:otherwise>${1}\n\
-	</xsl:otherwise>\n\
-snippet param\n\
-	<xsl:param name=\"${1:name}\">${2}\n\
-	</xsl:param>${3}\n\
-snippet stylesheet\n\
-	<xsl:stylesheet version=\"1.0\"\n\
-	xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">${1}\n\
-	</xsl:stylesheet>\n\
-snippet template\n\
-	<xsl:template match=\"${1:*}\">${3}\n\
-	</xsl:template>\n\
-snippet template named\n\
-	<xsl:template name=\"${1:name}\">${2}\n\
-	</xsl:template>\n\
-snippet text\n\
-	<xsl:text>${1}</xsl:text>\n\
-snippet value-of\n\
-	<xsl:value-of select=\"${1:*}\" />${2}\n\
-snippet variable blank\n\
-	<xsl:variable name=\"${1:name}\">${2}\n\
-	</xsl:variable>\n\
-snippet variable select\n\
-	<xsl:variable select=\"${1:*}\" />${2}\n\
-snippet when\n\
-	<xsl:when test=\"${1:test}\">${2}\n\
-	</xsl:when>\n\
-snippet with-param\n\
-	<xsl:with-param name=\"${1:name}\">${2}</xsl:with-param>\n\
-snippet with-param select\n\
-	<xsl:with-param name=\"${1:name}\" select=\"${2:*}\" />\n\
-";
+// $1 / ${1} is numbered tab position (with 1 being first) with no default text.
+// ${1:defaultText} is a numbered tab position with default text that is replaced on typing
+// $0 is final tab position
+// tabTrigger is the bit you type then hit tab to trigger the snippet.
+
+// This file needs to be included in app.html as a <script> tag
+
+exports.snippets = [
+    {
+        "tabTrigger": "apply-templates with-param",
+        "name": "apply-templates with-param",
+        "content": `
+<xsl:apply-templates select="${1:*}">
+  <xsl:with-param name="${2:param}">${3}</xsl:with-param>${4}
+</xsl:apply-templates>
+${0}`
+    },
+    {
+    "tabTrigger": "apply-templates sort-by",
+    "name": "apply-templates sort-by",
+    "content": `
+<xsl:apply-templates select="${1:*}">
+  <xsl:sort select="${2:node}" order="${3:ascending}" data-type="${4:text}">${5}
+</xsl:apply-templates>
+${0}`
+    },
+    {
+    "tabTrigger": "apply-templates plain",
+    "name": "apply-templates plain",
+    "content": `
+<xsl:apply-templates select="${1:*}" />
+${0}`
+    },
+    {
+    "tabTrigger": "attribute blank",
+    "name": "attribute blank",
+    "content": `
+<xsl:attribute name="${1:name}">${2}</xsl:attribute>
+${0}`
+    },
+    {
+    "tabTrigger": "attribute value-of",
+    "name": "attribute value-of",
+    "content": `
+<xsl:attribute name="${1:name}">
+  <xsl:value-of select="${2:*}" />
+</xsl:attribute>
+${0}`
+    },
+    {
+    "tabTrigger": "call-template",
+    "name": "call-template",
+    "content": `
+<xsl:call-template name="${1:template}" />
+$0`
+    },
+    {
+    "tabTrigger": "call-template with-param",
+    "name": "call-template with-param",
+    "content": `
+<xsl:call-template name="${1:template}">
+  <xsl:with-param name="${2:param}">${3}</xsl:with-param>${4}
+</xsl:call-template>
+${0}`
+    },
+    {
+    "tabTrigger": "choose",
+    "name": "choose",
+    "content": `
+<xsl:choose>
+  <xsl:when test="${1:value}">
+    ${2}
+  </xsl:when>${3}
+</xsl:choose>
+$0`
+    },
+    {
+    "tabTrigger": "copy-of",
+    "name": "copy-of",
+    "content": `
+<xsl:copy-of select="${1:*}" />
+${0}`
+    },
+    {
+    "tabTrigger": "for-each",
+    "name": "for-each",
+    "content": `
+<xsl:for-each select="${1:*}">
+  ${2}
+</xsl:for-each>
+${0}`
+    },
+    {
+    "tabTrigger": "if",
+    "name": "if",
+    "content": `
+<xsl:if test="${1:test}">
+  ${2}
+</xsl:if>
+${0}`
+    },
+    {
+    "tabTrigger": "import",
+    "name": "import",
+    "content": `
+<xsl:import href="${1:stylesheet}" />
+${0}`
+    },
+    {
+    "tabTrigger": "include",
+    "name": "include",
+    "content": `
+<xsl:include href="${1:stylesheet}" />
+${0}`
+    },
+    {
+    "tabTrigger": "otherwise",
+    "name": "otherwise",
+    "content": `
+<xsl:otherwise>
+  ${1}
+</xsl:otherwise>
+$0`
+    },
+    {
+    "tabTrigger": "param",
+    "name": "param",
+    "content": `
+<xsl:param name="${1:name}">
+  ${2}
+</xsl:param>
+${0}`
+    },
+    {
+    "tabTrigger": "stylesheet",
+    "name": "stylesheet",
+    "content": `
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  ${0}
+</xsl:stylesheet>`
+    },
+    {
+    "tabTrigger": "template",
+    "name": "template",
+    "content": `
+<xsl:template match="${1:*}">
+  ${3}
+</xsl:template>
+$0`
+    },
+    {
+    "tabTrigger": "template named",
+    "name": "template named",
+    "content": `
+<xsl:template name="${1:name}">
+  ${2}
+</xsl:template>
+$0`
+    },
+    {
+    "tabTrigger": "text",
+    "name": "text",
+    "content": `
+<xsl:text>${1}</xsl:text>
+$0`
+    },
+    {
+    "tabTrigger": "value-of",
+    "name": "value-of",
+    "content": `
+<xsl:value-of select="${1:*}" />
+${0}`
+    },
+    {
+    "tabTrigger": "variable blank",
+    "name": "variable blank",
+    "content": `
+<xsl:variable name="${1:name}">
+  ${2}
+</xsl:variable>`
+    },
+    {
+    "tabTrigger": "variable select",
+    "name": "variable select",
+    "content": `
+<xsl:variable select="${1:*}" />${2} `
+    },
+    {
+    "tabTrigger": "when",
+    "name": "when",
+    "content": `
+<xsl:when test="${1:test}">
+  ${2}
+</xsl:when>`
+    },
+    {
+    "tabTrigger": "with-param",
+    "name": "with-param",
+    "content": `
+<xsl:with-param name="${1:name}">${2}</xsl:with-param>`
+    },
+    {
+    "tabTrigger": "with-param select",
+    "name": "with-param select",
+    "content": `
+<xsl:with-param name="${1:name}" select="${2:*}" />`
+    },
+];
+
 exports.scope = "xml";
 
 });                (function() {
