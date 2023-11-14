1e749b2c83480057624c9304808655cd727bad24
stroomdev66 <stroomdev66@gmail.com>
2023-11-14 13:57:08 +0000

    #3909 Add multiple recipients to rule notification emails

--- stroom-core-shared/src/main/java/stroom/analytics/shared/AnalyticNotificationEmailDestination.java
+++ stroom-core-shared/src/main/java/stroom/analytics/shared/AnalyticNotificationEmailDestination.java
@@ -13,15 +13,32 @@ import java.util.Objects;
 public class AnalyticNotificationEmailDestination extends AnalyticNotificationDestination {
 
     @JsonProperty
-    private final String emailAddress;
+    private final String to;
+    @JsonProperty
+    private final String cc;
+    @JsonProperty
+    private final String bcc;
+
 
     @JsonCreator
-    public AnalyticNotificationEmailDestination(@JsonProperty("emailAddress") final String emailAddress) {
-        this.emailAddress = emailAddress;
+    public AnalyticNotificationEmailDestination(@JsonProperty("to") final String to,
+                                                @JsonProperty("cc") final String cc,
+                                                @JsonProperty("bcc") final String bcc) {
+        this.to = to;
+        this.cc = cc;
+        this.bcc = bcc;
     }
 
-    public String getEmailAddress() {
-        return emailAddress;
+    public String getTo() {
+        return to;
+    }
+
+    public String getCc() {
+        return cc;
+    }
+
+    public String getBcc() {
+        return bcc;
     }
 
     @Override
@@ -33,18 +50,22 @@ public class AnalyticNotificationEmailDestination extends AnalyticNotificationDe
             return false;
         }
         final AnalyticNotificationEmailDestination that = (AnalyticNotificationEmailDestination) o;
-        return Objects.equals(emailAddress, that.emailAddress);
+        return Objects.equals(to, that.to) && Objects.equals(cc, that.cc) && Objects.equals(
+                bcc,
+                that.bcc);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(emailAddress);
+        return Objects.hash(to, cc, bcc);
     }
 
     @Override
     public String toString() {
         return "AnalyticNotificationEmailDestination{" +
-                "emailAddress=" + emailAddress +
+                "to='" + to + '\'' +
+                ", cc='" + cc + '\'' +
+                ", bcc='" + bcc + '\'' +
                 '}';
     }
 
@@ -59,22 +80,36 @@ public class AnalyticNotificationEmailDestination extends AnalyticNotificationDe
 
     public static class Builder {
 
-        private String emailAddress;
+        private String to;
+        private String cc;
+        private String bcc;
 
         private Builder() {
         }
 
         private Builder(final AnalyticNotificationEmailDestination config) {
-            this.emailAddress = config.emailAddress;
+            this.to = config.to;
+            this.cc = config.cc;
+            this.bcc = config.bcc;
         }
 
-        public Builder emailAddress(final String emailAddress) {
-            this.emailAddress = emailAddress;
+        public Builder to(final String to) {
+            this.to = to;
+            return this;
+        }
+
+        public Builder cc(final String cc) {
+            this.cc = cc;
+            return this;
+        }
+
+        public Builder bcc(final String bcc) {
+            this.bcc = bcc;
             return this;
         }
 
         public AnalyticNotificationEmailDestination build() {
-            return new AnalyticNotificationEmailDestination(emailAddress);
+            return new AnalyticNotificationEmailDestination(to, cc, bcc);
         }
     }
 }
