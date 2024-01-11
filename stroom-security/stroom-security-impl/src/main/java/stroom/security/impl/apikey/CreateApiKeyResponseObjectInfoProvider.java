073d22380f9ad0837b54d357531e9bd057c5086a
at055612 <22818309+at055612@users.noreply.github.com>
2024-01-11 16:05:49 +0000

    gh-3937 Fix CS

--- stroom-security/stroom-security-impl/src/main/java/stroom/security/impl/apikey/CreateApiKeyResponseObjectInfoProvider.java
+++ stroom-security/stroom-security-impl/src/main/java/stroom/security/impl/apikey/CreateApiKeyResponseObjectInfoProvider.java
@@ -1,14 +1,10 @@
 package stroom.security.impl.apikey;
 
 import stroom.security.shared.CreateApiKeyResponse;
-import stroom.util.logging.LambdaLogger;
-import stroom.util.logging.LambdaLoggerFactory;
 
 import event.logging.BaseObject;
 
 public class CreateApiKeyResponseObjectInfoProvider extends ApiKeyObjectInfoProvider {
-
-    private static final LambdaLogger LOGGER = LambdaLoggerFactory.getLogger(CreateApiKeyResponseObjectInfoProvider.class);
 
     @Override
     public BaseObject createBaseObject(final Object object) {
