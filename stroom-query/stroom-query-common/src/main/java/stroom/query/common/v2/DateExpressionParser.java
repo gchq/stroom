9fbfe43693cd992cc0cf8a9d78e45dc984952b09
stroomdev66 <stroomdev66@gmail.com>
2024-01-29 14:46:38 +0000

    #4030 Fix token validation for date expressions

--- stroom-query/stroom-query-common/src/main/java/stroom/query/common/v2/DateExpressionParser.java
+++ stroom-query/stroom-query-common/src/main/java/stroom/query/common/v2/DateExpressionParser.java
@@ -19,10 +19,14 @@ package stroom.query.common.v2;
 import stroom.expression.api.DateTimeSettings;
 import stroom.query.api.v2.TimeFilter;
 import stroom.query.api.v2.TimeRange;
+import stroom.query.language.token.AbstractToken;
+import stroom.query.language.token.TokenException;
+import stroom.query.language.token.TokenType;
 import stroom.util.logging.LambdaLogger;
 import stroom.util.logging.LambdaLoggerFactory;
 
 import java.time.DateTimeException;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -33,6 +37,7 @@ import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalField;
 import java.time.temporal.WeekFields;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.Optional;
@@ -101,6 +106,103 @@ public class DateExpressionParser {
         return parse(expression, DateTimeSettings.builder().referenceTime(referenceTime).build());
     }
 
+    public static Optional<ZonedDateTime> parse(final List<AbstractToken> tokens,
+                                                final DateTimeSettings dts) {
+        final DateTimeSettings dateTimeSettings = Objects
+                .requireNonNullElseGet(dts, () -> DateTimeSettings.builder().build());
+        final long referenceTime = Objects
+                .requireNonNullElseGet(dateTimeSettings.getReferenceTime(), System::currentTimeMillis);
+
+        ZonedDateTime computedDateTime = null;
+        TokenType operator = null;
+        for (final AbstractToken token : tokens) {
+            if (TokenType.FUNCTION_GROUP.equals(token.getTokenType())) {
+                final String function = token.getUnescapedText();
+
+                ZonedDateTime dateTime = null;
+                for (final DatePoint datePoint : DatePoint.values()) {
+                    if (datePoint.getFunction().equals(function)) {
+                        dateTime = convertDatePoint(datePoint, fromEpochMillis(referenceTime));
+                        break;
+                    }
+                }
+                if (dateTime == null) {
+                    throw new TokenException(token, "Unexpected function in value");
+                } else if (computedDateTime != null) {
+                    throw new TokenException(token, "Unexpected date point function after time");
+                } else {
+                    computedDateTime = dateTime;
+                }
+
+            } else if (TokenType.DURATION.equals(token.getTokenType())) {
+                if (computedDateTime == null) {
+                    throw new TokenException(token, "Unexpected duration");
+                }
+                if (operator == null) {
+                    throw new TokenException(token, "Unexpected duration without operator");
+                }
+                final TimeFunction function = parseDuration(token.getUnescapedText(),
+                        operator == TokenType.PLUS
+                                ? '+'
+                                : '-');
+                computedDateTime = function.apply(computedDateTime);
+                operator = null;
+
+            } else if (TokenType.NUMBER.equals(token.getTokenType())) {
+                final long millis;
+                try {
+                    millis = Long.parseLong(token.getUnescapedText());
+                } catch (final NumberFormatException e) {
+                    throw new TokenException(token, "Error parsing number");
+                }
+
+                if (computedDateTime == null) {
+                    computedDateTime = fromEpochMillis(millis);
+
+                } else {
+                    if (operator == null) {
+                        throw new TokenException(token, "Unexpected duration in millis without operator");
+                    }
+                    final Duration duration = Duration.ofMillis(millis);
+                    if (operator == TokenType.MINUS) {
+                        computedDateTime = computedDateTime.minus(duration);
+                    } else {
+                        computedDateTime = computedDateTime.plus(duration);
+                    }
+                }
+
+            } else if (TokenType.MINUS.equals(token.getTokenType())) {
+                if (computedDateTime == null) {
+                    throw new TokenException(token, "Unexpected operator");
+                }
+                if (operator == TokenType.MINUS) {
+                    operator = TokenType.PLUS;
+                } else {
+                    operator = TokenType.MINUS;
+                }
+            } else if (TokenType.PLUS.equals(token.getTokenType())) {
+                if (computedDateTime == null) {
+                    throw new TokenException(token, "Unexpected operator");
+                }
+                if (operator == null) {
+                    operator = TokenType.PLUS;
+                }
+            } else {
+                if (computedDateTime != null) {
+                    throw new TokenException(token, "Unexpected token");
+                } else {
+                    try {
+                        computedDateTime = parseTimeString(token.getUnescapedText(), dateTimeSettings);
+                    } catch (final RuntimeException e) {
+                        throw new TokenException(token, "Unexpected token");
+                    }
+                }
+            }
+        }
+
+        return Optional.ofNullable(computedDateTime);
+    }
+
     public static Optional<ZonedDateTime> parse(final String expression,
                                                 final DateTimeSettings dts) {
         final DateTimeSettings dateTimeSettings = Objects
@@ -122,46 +224,7 @@ public class DateExpressionParser {
 
         if (index != -1) {
             final String trimmed = new String(chars).trim();
-            ZonedDateTime time = null;
-
-            try {
-                // Assume a timezone is specified on the string.
-                time = ZonedDateTime.parse(trimmed);
-            } catch (final DateTimeParseException e) {
-
-                try {
-                    if (dateTimeSettings.getTimeZone() != null && dateTimeSettings.getTimeZone().getUse() != null) {
-                        switch (dateTimeSettings.getTimeZone().getUse()) {
-                            case LOCAL -> {
-                                final ZoneId zoneId = ZoneId.of(dateTimeSettings.getLocalZoneId());
-                                time = LocalDateTime.parse(trimmed).atZone(zoneId);
-                            }
-                            case UTC -> {
-                                final ZoneId zoneId = ZoneId.of("Z");
-                                time = LocalDateTime.parse(trimmed).atZone(zoneId);
-                            }
-                            case ID -> {
-                                final ZoneId zoneId = ZoneId.of(dateTimeSettings.getTimeZone().getId());
-                                time = LocalDateTime.parse(trimmed).atZone(zoneId);
-                            }
-                            case OFFSET -> {
-                                final ZoneOffset zoneOffset = ZoneOffset
-                                        .ofHoursMinutes(dateTimeSettings.getTimeZone().getOffsetHours(),
-                                                dateTimeSettings.getTimeZone().getOffsetMinutes());
-                                time = LocalDateTime.parse(trimmed).atOffset(zoneOffset).toZonedDateTime();
-                            }
-                        }
-                    }
-                } catch (final RuntimeException ex) {
-                    // Ignore error
-                }
-
-                // If no time zone was specified then try and parse as a local datetime.
-                if (time == null) {
-                    time = LocalDateTime.parse(trimmed).atZone(DEFAULT_TIME_ZONE);
-                }
-            }
-
+            final ZonedDateTime time = parseTimeString(trimmed, dateTimeSettings);
             parts[index] = new Part(trimmed, time);
         }
 
@@ -170,16 +233,16 @@ public class DateExpressionParser {
 
         for (final Part part : parts) {
             if (part != null) {
-                if (part.getObject() instanceof ZonedDateTime) {
+                if (part.object() instanceof ZonedDateTime) {
                     if (time != null) {
                         throw new DateTimeException("Attempt to set the date and time twice with '" +
                                 part.toString().trim() +
                                 "'. You cannot have more than one declaration of date and time.");
                     }
 
-                    time = (ZonedDateTime) part.getObject();
+                    time = (ZonedDateTime) part.object();
 
-                } else if (part.getObject() instanceof final TimeFunction duration) {
+                } else if (part.object() instanceof final TimeFunction duration) {
                     if (time == null) {
                         throw new DateTimeException("You must specify a time or time constant before adding or " +
                                 "subtracting duration '" + part.toString().trim() + "'.");
@@ -202,46 +265,85 @@ public class DateExpressionParser {
             while (start != -1) {
                 final int end = start + function.length();
 
-                // Obliterate the matched part of the expression so it can't be matched by any other matcher.
+                // Obliterate the matched part of the expression, so it can't be matched by any other matcher.
                 Arrays.fill(chars, start, end, ' ');
 
-                ZonedDateTime time;
-                switch (datePoint) {
-                    case NOW:
-                        time = now;
-                        break;
-                    case SECOND:
-                        time = now.truncatedTo(ChronoUnit.SECONDS);
-                        break;
-                    case MINUTE:
-                        time = now.truncatedTo(ChronoUnit.MINUTES);
-                        break;
-                    case HOUR:
-                        time = now.truncatedTo(ChronoUnit.HOURS);
-                        break;
-                    case DAY:
-                        time = now.truncatedTo(ChronoUnit.DAYS);
-                        break;
-                    case WEEK:
-                        TemporalField fieldISO = WeekFields.of(Locale.UK).dayOfWeek();
-                        time = now.with(fieldISO, 1); // Monday
-                        time = time.truncatedTo(ChronoUnit.DAYS);
-                        break;
-                    case MONTH:
-                        time = ZonedDateTime.of(now.getYear(), now.getMonthValue(), 1, 0, 0, 0, 0, now.getZone());
-                        break;
-                    case YEAR:
-                        time = ZonedDateTime.of(now.getYear(), 1, 1, 0, 0, 0, 0, now.getZone());
-                        break;
-                    default:
-                        throw new RuntimeException("Unexpected datePoint " + datePoint);
-                }
-
+                final ZonedDateTime time = convertDatePoint(datePoint, now);
                 parts[start] = new Part(function, time);
 
                 start = expression.indexOf(function, end);
             }
         }
+    }
+
+    private static ZonedDateTime parseTimeString(final String trimmed,
+                                                 final DateTimeSettings dateTimeSettings)
+            throws DateTimeParseException {
+        try {
+            // Assume a timezone is specified on the string.
+            return ZonedDateTime.parse(trimmed);
+        } catch (final DateTimeParseException e) {
+
+            try {
+                if (dateTimeSettings.getTimeZone() != null && dateTimeSettings.getTimeZone().getUse() != null) {
+                    switch (dateTimeSettings.getTimeZone().getUse()) {
+                        case LOCAL -> {
+                            final ZoneId zoneId = ZoneId.of(dateTimeSettings.getLocalZoneId());
+                            return LocalDateTime.parse(trimmed).atZone(zoneId);
+                        }
+                        case UTC -> {
+                            final ZoneId zoneId = ZoneId.of("Z");
+                            return LocalDateTime.parse(trimmed).atZone(zoneId);
+                        }
+                        case ID -> {
+                            final ZoneId zoneId = ZoneId.of(dateTimeSettings.getTimeZone().getId());
+                            return LocalDateTime.parse(trimmed).atZone(zoneId);
+                        }
+                        case OFFSET -> {
+                            final ZoneOffset zoneOffset = ZoneOffset
+                                    .ofHoursMinutes(dateTimeSettings.getTimeZone().getOffsetHours(),
+                                            dateTimeSettings.getTimeZone().getOffsetMinutes());
+                            return LocalDateTime.parse(trimmed).atOffset(zoneOffset).toZonedDateTime();
+                        }
+                    }
+                }
+            } catch (final RuntimeException ex) {
+                // Ignore error
+            }
+
+            // If no time zone was specified then try and parse as a local datetime.
+            return LocalDateTime.parse(trimmed).atZone(DEFAULT_TIME_ZONE);
+        }
+    }
+
+    private static ZonedDateTime convertDatePoint(final DatePoint datePoint,
+                                                  final ZonedDateTime now) {
+        switch (datePoint) {
+            case NOW:
+                return now;
+            case SECOND:
+                return now.truncatedTo(ChronoUnit.SECONDS);
+            case MINUTE:
+                return now.truncatedTo(ChronoUnit.MINUTES);
+            case HOUR:
+                return now.truncatedTo(ChronoUnit.HOURS);
+            case DAY:
+                return now.truncatedTo(ChronoUnit.DAYS);
+            case WEEK:
+                final TemporalField fieldISO = WeekFields.of(Locale.UK).dayOfWeek();
+                final ZonedDateTime time = now.with(fieldISO, 1); // Monday
+                return time.truncatedTo(ChronoUnit.DAYS);
+            case MONTH:
+                return ZonedDateTime.of(now.getYear(), now.getMonthValue(), 1, 0, 0, 0, 0, now.getZone());
+            case YEAR:
+                return ZonedDateTime.of(now.getYear(), 1, 1, 0, 0, 0, 0, now.getZone());
+            default:
+                throw new RuntimeException("Unexpected datePoint " + datePoint);
+        }
+    }
+
+    private static ZonedDateTime fromEpochMillis(final long nowEpochMilli) {
+        return ZonedDateTime.ofInstant(Instant.ofEpochMilli(nowEpochMilli), ZoneOffset.UTC);
     }
 
     private static void parseDurations(final char[] chars, final Part[] parts) {
@@ -281,7 +383,7 @@ public class DateExpressionParser {
 
             final String section = new String(chars, index, end - index);
 
-            // Obliterate the matched part of the expression so it can't be matched by any other matcher.
+            // Obliterate the matched part of the expression, so it can't be matched by any other matcher.
             Arrays.fill(chars, start, end, ' ');
 
             final TimeFunction duration = parseDuration(section, sign);
@@ -321,48 +423,29 @@ public class DateExpressionParser {
     }
 
     private static TimeFunction createFunction(final char sign, final char type, final long value) {
-        switch (sign) {
-            case '+':
-                switch (type) {
-                    case 's':
-                        return time -> time.plusSeconds(value);
-                    case 'm':
-                        return time -> time.plusMinutes(value);
-                    case 'h':
-                        return time -> time.plusHours(value);
-                    case 'd':
-                        return time -> time.plusDays(value);
-                    case 'w':
-                        return time -> time.plusWeeks(value);
-                    case 'M':
-                        return time -> time.plusMonths(value);
-                    case 'y':
-                        return time -> time.plusYears(value);
-                    default:
-                        throw new DateTimeException("Unknown duration type '" + type + "'.");
-                }
-            case '-':
-                switch (type) {
-                    case 's':
-                        return time -> time.minusSeconds(value);
-                    case 'm':
-                        return time -> time.minusMinutes(value);
-                    case 'h':
-                        return time -> time.minusHours(value);
-                    case 'd':
-                        return time -> time.minusDays(value);
-                    case 'w':
-                        return time -> time.minusWeeks(value);
-                    case 'M':
-                        return time -> time.minusMonths(value);
-                    case 'y':
-                        return time -> time.minusYears(value);
-                    default:
-                        throw new DateTimeException("Unknown duration type '" + type + "'.");
-                }
-            default:
-                throw new DateTimeException("Unknown sign '" + sign + "'.");
-        }
+        return switch (sign) {
+            case '+' -> switch (type) {
+                case 's' -> time -> time.plusSeconds(value);
+                case 'm' -> time -> time.plusMinutes(value);
+                case 'h' -> time -> time.plusHours(value);
+                case 'd' -> time -> time.plusDays(value);
+                case 'w' -> time -> time.plusWeeks(value);
+                case 'M' -> time -> time.plusMonths(value);
+                case 'y' -> time -> time.plusYears(value);
+                default -> throw new DateTimeException("Unknown duration type '" + type + "'.");
+            };
+            case '-' -> switch (type) {
+                case 's' -> time -> time.minusSeconds(value);
+                case 'm' -> time -> time.minusMinutes(value);
+                case 'h' -> time -> time.minusHours(value);
+                case 'd' -> time -> time.minusDays(value);
+                case 'w' -> time -> time.minusWeeks(value);
+                case 'M' -> time -> time.minusMonths(value);
+                case 'y' -> time -> time.minusYears(value);
+                default -> throw new DateTimeException("Unknown duration type '" + type + "'.");
+            };
+            default -> throw new DateTimeException("Unknown sign '" + sign + "'.");
+        };
     }
 
 
@@ -394,19 +477,7 @@ public class DateExpressionParser {
     // --------------------------------------------------------------------------------
 
 
-    private static class Part {
-
-        private final String string;
-        private final Object object;
-
-        Part(final String string, final Object object) {
-            this.string = string;
-            this.object = object;
-        }
-
-        public Object getObject() {
-            return object;
-        }
+    private record Part(String string, Object object) {
 
         @Override
         public String toString() {
