946a8639f9f84d92e1e2e3506a04a1d172345ca9
p-kimberley <peter@gradata.com.au>
2024-02-05 21:23:15 +1100

    Rewrite tests

--- stroom-pipeline/src/test/java/stroom/pipeline/xsltfunctions/TestHexToString.java
+++ stroom-pipeline/src/test/java/stroom/pipeline/xsltfunctions/TestHexToString.java
@@ -1,7 +1,5 @@
 package stroom.pipeline.xsltfunctions;
 
-import stroom.util.logging.LambdaLogger;
-import stroom.util.logging.LambdaLoggerFactory;
 import stroom.util.shared.Severity;
 
 import net.sf.saxon.om.EmptyAtomicSequence;
@@ -14,8 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TestHexToString extends AbstractXsltFunctionTest<HexToString> {
 
-    private static final LambdaLogger LOGGER = LambdaLoggerFactory.getLogger(TestHexToString.class);
-
     private HexToString hexToString;
 
     @BeforeEach
@@ -24,38 +20,63 @@ class TestHexToString extends AbstractXsltFunctionTest<HexToString> {
     }
 
     @Test
-    void call() {
-
-        Sequence sequence = callFunctionWithSimpleArgs("74 65 73 74 69 6e 67 20 31 32 33", "ASCII");
+    void decodeAscii() {
+        final Sequence sequence = callFunctionWithSimpleArgs("74 65 73 74 69 6e 67 20 31 32 33", "ASCII");
         assertThat(getAsStringValue(sequence).orElseThrow())
                 .isEqualTo("testing 123")
                 .describedAs("ASCII encoded text should be decoded correctly");
+    }
 
-        sequence = callFunctionWithSimpleArgs("74 65 73 74 69 6e 67 20 31 32 33", "UTF-8");
+    @Test
+    void decodeUtf8() {
+        final Sequence sequence = callFunctionWithSimpleArgs("74 65 73 74 69 6e 67 20 31 32 33", "UTF-8");
         assertThat(getAsStringValue(sequence).orElseThrow())
                 .isEqualTo("testing 123")
                 .describedAs("UTF-8 encoded text should be decoded correctly");
+    }
 
-        sequence = callFunctionWithSimpleArgs("74 65 73 74 69 6E 67 20 31 32 33", "UTF-8");
+    @Test
+    void decodeUtf8UpperCase() {
+        final Sequence sequence = callFunctionWithSimpleArgs("74 65 73 74 69 6E 67 20 31 32 33", "UTF-8");
         assertThat(getAsStringValue(sequence).orElseThrow())
                 .isEqualTo("testing 123")
                 .describedAs("UTF-8 encoded text should be decoded correctly, regardless of input case");
+    }
 
-        sequence = callFunctionWithSimpleArgs("00 74 00 65 00 73 00 74 00 69 00 6e 00 67 00 20 00 31 00 32 00 33",
+    @Test
+    void decodeUtf16Be() {
+        final Sequence sequence = callFunctionWithSimpleArgs("00 74 00 65 00 73 00 74 00 69 00 6e 00 67 00 20 00 31 00 32 00 33",
                 "UTF-16BE");
         assertThat(getAsStringValue(sequence).orElseThrow())
                 .isEqualTo("testing 123")
                 .describedAs("UTF-16BE encoded text should be decoded correctly");
+    }
 
-        sequence = callFunctionWithSimpleArgs("", "UTF-8");
+    @Test
+    void decodeEmptyHex() {
+        final Sequence sequence = callFunctionWithSimpleArgs("", "UTF-8");
         assertThat(getAsStringValue(sequence).orElseThrow())
                 .isEqualTo("")
                 .describedAs("Empty hex value should return an empty string");
     }
 
     @Test
-    void call_invalid() {
+    void invalidCharset() {
+        logLogCallsToDebug();
 
+        final Sequence sequence = callFunctionWithSimpleArgs("74 65 73 74 69 6E 67 20 31 32 33", "invalid charset");
+        assertThat(sequence)
+                .isInstanceOf(EmptyAtomicSequence.class);
+
+        final LogArgs logArgs = verifySingleLogCall();
+        Assertions.assertThat(logArgs.getSeverity())
+                .isEqualTo(Severity.ERROR);
+        Assertions.assertThat(logArgs.getMessage())
+                .containsIgnoringCase("invalid charset");
+    }
+
+    @Test
+    void invalidHexLength() {
         logLogCallsToDebug();
 
         final Sequence sequence = callFunctionWithSimpleArgs("7 ", "UTF-8");
