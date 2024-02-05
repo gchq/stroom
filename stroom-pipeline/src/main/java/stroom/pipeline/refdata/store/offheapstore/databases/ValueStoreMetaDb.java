ecdbed222c15f8b155bccfa4f06044bec683c05f
at055612 <22818309+at055612@users.noreply.github.com>
2024-02-05 13:42:32 +0000

    gh-4085 Fix ref data ref count overflow error msg

--- stroom-pipeline/src/main/java/stroom/pipeline/refdata/store/offheapstore/databases/ValueStoreMetaDb.java
+++ stroom-pipeline/src/main/java/stroom/pipeline/refdata/store/offheapstore/databases/ValueStoreMetaDb.java
@@ -39,8 +39,6 @@ import org.lmdbjava.Cursor;
 import org.lmdbjava.GetOp;
 import org.lmdbjava.PutFlags;
 import org.lmdbjava.Txn;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.nio.ByteBuffer;
 import java.util.Optional;
@@ -58,14 +56,13 @@ import java.util.OptionalInt;
  * key        | value
  * (hash|id)  | (type|referenceCount)
  * ---------------------------------------------
- * (1234|00)  | (0|0001)
- * (1234|01)  | (0|0001)
- * (4567|00)  | (0|0001)
- * (7890|00)  | (1|0001)
+ * (1234|00)  | (0|001)
+ * (1234|01)  | (0|001)
+ * (4567|00)  | (0|001)
+ * (7890|00)  | (1|001)
  */
 public class ValueStoreMetaDb extends AbstractLmdbDb<ValueStoreKey, ValueStoreMeta> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ValueStoreMetaDb.class);
     private static final LambdaLogger LAMBDA_LOGGER = LambdaLoggerFactory.getLogger(ValueStoreMetaDb.class);
 
     private static final String DB_NAME = "ValueStoreMeta";
