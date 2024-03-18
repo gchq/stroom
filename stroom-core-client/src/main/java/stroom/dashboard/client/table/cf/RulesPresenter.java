d3721fa3cafea66d7ee75423c59968b7d15078eb
at055612 <22818309+at055612@users.noreply.github.com>
2024-03-18 17:27:20 +0000

    gh-4135 Fix dialog width

--- stroom-core-client/src/main/java/stroom/dashboard/client/table/cf/RulesPresenter.java
+++ stroom-core-client/src/main/java/stroom/dashboard/client/table/cf/RulesPresenter.java
@@ -205,7 +205,7 @@ public class RulesPresenter
         selectionBoxModel.addItems(fields.stream().map(FieldInfo::create).collect(Collectors.toList()));
         editRulePresenter.read(newRule, selectionBoxModel);
 
-        final PopupSize popupSize = PopupSize.resizable(1400, 550);
+        final PopupSize popupSize = PopupSize.resizable(800, 550);
         ShowPopupEvent.builder(editRulePresenter)
                 .popupType(PopupType.OK_CANCEL_DIALOG)
                 .popupSize(popupSize)
