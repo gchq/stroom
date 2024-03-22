6aca198da4b1c4b5133978ce5d4b852f22e6ee3b
stroomdev66 <stroomdev66@gmail.com>
2024-03-22 16:23:41 +0000

    #4179 Add text input control to dashboards

---
+++ stroom-core-client/src/main/java/stroom/dashboard/client/input/TextInputPresenter.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2017 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package stroom.dashboard.client.input;
+
+import stroom.dashboard.client.main.AbstractComponentPresenter;
+import stroom.dashboard.client.main.ComponentChangeEvent;
+import stroom.dashboard.client.main.ComponentRegistry.ComponentType;
+import stroom.dashboard.client.main.ComponentRegistry.ComponentUse;
+import stroom.dashboard.client.input.TextInputPresenter.TextInputView;
+import stroom.dashboard.client.main.HasParams;
+import stroom.dashboard.shared.ComponentConfig;
+import stroom.dashboard.shared.ComponentSettings;
+import stroom.dashboard.shared.TextInputComponentSettings;
+import stroom.query.api.v2.Param;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.mvp.client.HasUiHandlers;
+import com.gwtplatform.mvp.client.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextInputPresenter
+        extends AbstractComponentPresenter<TextInputView>
+        implements TextInputUiHandlers, HasParams {
+
+    public static final ComponentType TYPE =
+            new ComponentType(1,
+                    "text-input",
+                    "Text Input",
+                    ComponentUse.INPUT);
+
+    @Inject
+    public TextInputPresenter(final EventBus eventBus,
+                              final TextInputView view,
+                              final Provider<TextInputSettingsPresenter> settingsPresenterProvider) {
+        super(eventBus, view, settingsPresenterProvider);
+        view.setUiHandlers(this);
+    }
+
+    @Override
+    public void onValueChanged(final String value) {
+        setSettings(getTextInputSettings().copy().value(value).build());
+        ComponentChangeEvent.fire(this, this);
+        setDirty(true);
+    }
+
+    @Override
+    public List<Param> getParams() {
+        final List<Param> list = new ArrayList<>();
+        final String key = getTextInputSettings().getKey();
+        final String value = getView().getValue();
+        if (key != null && key.trim().length() > 0 && value != null && value.trim().length() > 0) {
+            final Param param = new Param(key.trim(), value.trim());
+            list.add(param);
+        }
+        return list;
+    }
+
+    @Override
+    public void read(final ComponentConfig componentConfig) {
+        super.read(componentConfig);
+
+        ComponentSettings settings = componentConfig.getSettings();
+        if (!(settings instanceof TextInputComponentSettings)) {
+            setSettings(createSettings());
+        }
+
+        update(getTextInputSettings());
+    }
+
+    private void update(final TextInputComponentSettings settings) {
+        getView().setValue(settings.getValue());
+    }
+
+    private TextInputComponentSettings getTextInputSettings() {
+        return (TextInputComponentSettings) getSettings();
+    }
+
+    private TextInputComponentSettings createSettings() {
+        return TextInputComponentSettings.builder().build();
+    }
+
+
+    @Override
+    public void link() {
+    }
+
+    @Override
+    public void changeSettings() {
+        super.changeSettings();
+        update(getTextInputSettings());
+    }
+
+    @Override
+    public ComponentType getType() {
+        return TYPE;
+    }
+
+    public interface TextInputView extends View, HasUiHandlers<TextInputUiHandlers> {
+
+        void setValue(String value);
+
+        String getValue();
+    }
+}
---
+++ stroom-core-shared/src/main/java/stroom/dashboard/shared/TextInputComponentSettings.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2017 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package stroom.dashboard.shared;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.Objects;
+
+@JsonPropertyOrder({
+        "key",
+        "value"
+})
+@JsonInclude(Include.NON_NULL)
+public class TextInputComponentSettings implements ComponentSettings {
+
+    @JsonProperty
+    private final String key;
+    @JsonProperty
+    private final String value;
+
+    @JsonCreator
+    public TextInputComponentSettings(@JsonProperty("key") final String key,
+                                      @JsonProperty("value") final String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final TextInputComponentSettings that = (TextInputComponentSettings) o;
+        return Objects.equals(key, that.key) &&
+                Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return "DropDownInputComponentSettings{" +
+                "key='" + key + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public Builder copy() {
+        return new Builder(this);
+    }
+
+    public static final class Builder implements ComponentSettings.Builder {
+
+        private String key;
+        private String value;
+
+        private Builder() {
+        }
+
+        private Builder(final TextInputComponentSettings keyValueInputComponentSettings) {
+            this.key = keyValueInputComponentSettings.key;
+            this.value = keyValueInputComponentSettings.value;
+        }
+
+        public Builder key(final String key) {
+            this.key = key;
+            return this;
+        }
+
+        public Builder value(final String value) {
+            this.value = value;
+            return this;
+        }
+
+        @Override
+        public TextInputComponentSettings build() {
+            return new TextInputComponentSettings(
+                    key,
+                    value
+            );
+        }
+    }
+}
