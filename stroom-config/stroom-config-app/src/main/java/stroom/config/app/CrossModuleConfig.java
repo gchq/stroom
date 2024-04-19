ab97679a5f7ae44d921b288b214a4371a7f129a4
at055612 <22818309+at055612@users.noreply.github.com>
2024-04-19 16:19:46 +0100

    Fix config providers

--- stroom-config/stroom-config-app/src/main/java/stroom/config/app/CrossModuleConfig.java
+++ stroom-config/stroom-config-app/src/main/java/stroom/config/app/CrossModuleConfig.java
@@ -1,6 +1,11 @@
 package stroom.config.app;
 
+import stroom.config.common.AbstractDbConfig;
+import stroom.config.common.ConnectionConfig;
+import stroom.config.common.ConnectionPoolConfig;
+import stroom.config.common.HasDbConfig;
 import stroom.util.shared.AbstractConfig;
+import stroom.util.shared.BootStrapConfig;
 import stroom.util.shared.IsStroomConfig;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -8,7 +13,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder(alphabetic = true)
-public class CrossModuleConfig extends AbstractConfig implements IsStroomConfig {
+public class CrossModuleConfig extends AbstractConfig implements IsStroomConfig, HasDbConfig {
 
     public static final String NAME = "crossModule";
     public static final String PROP_NAME_DB = "db";
@@ -25,7 +30,27 @@ public class CrossModuleConfig extends AbstractConfig implements IsStroomConfig 
     }
 
     @JsonProperty(PROP_NAME_DB)
-    public CrossModuleDbConfig getAppDbConfig() {
+    public CrossModuleDbConfig getDbConfig() {
         return crossModuleDbConfig;
+    }
+
+
+    // --------------------------------------------------------------------------------
+
+
+    @BootStrapConfig
+    public static class CrossModuleDbConfig extends AbstractDbConfig implements IsStroomConfig {
+
+        public CrossModuleDbConfig() {
+            super();
+        }
+
+        @SuppressWarnings("unused")
+        @JsonCreator
+        public CrossModuleDbConfig(
+                @JsonProperty(PROP_NAME_CONNECTION) final ConnectionConfig connectionConfig,
+                @JsonProperty(PROP_NAME_CONNECTION_POOL) final ConnectionPoolConfig connectionPoolConfig) {
+            super(connectionConfig, connectionPoolConfig);
+        }
     }
 }
