c06f56989622c68401e580b63c0b689b761d3da7
at055612 <22818309+at055612@users.noreply.github.com>
2024-05-15 15:37:43 +0100

    gh-4266 Add new types to Memory stats for off-heap buffers

--- stroom-core-client-widget/src/main/java/stroom/widget/util/client/KeyBinding.java
+++ stroom-core-client-widget/src/main/java/stroom/widget/util/client/KeyBinding.java
@@ -2,6 +2,7 @@ package stroom.widget.util.client;
 
 import stroom.util.shared.GwtNullSafe;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.BrowserEvents;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.EventTarget;
@@ -195,6 +196,7 @@ public class KeyBinding {
             if (Element.is(eventTarget)) {
                 final Element element = Element.as(eventTarget);
                 final String className = GwtNullSafe.string(element.getClassName());
+                GWT.log("className: " + className + " tagName: " + element.getTagName());
                 // TODO this is really clunky. Need to create our own textbox and text area
                 //  components so that we can control the key events
                 shouldCheck = !className.contains("gwt-TextBox")
