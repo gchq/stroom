42722e3f6456d80763690d97ac9cc65f91c07e5f
at055612 <22818309+at055612@users.noreply.github.com>
2024-07-12 09:23:05 +0100

    Change setStroomLogLevels and setProxyLogLevels to allow port override

--- setProxyLogLevels.sh
+++ setProxyLogLevels.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+# Allow the caller to force the admin port, e.g. for devving
+# with >1 node on a host
+ADMIN_PORT_OVERRIDE=$ADMIN_PORT
+
 # shellcheck disable=SC1091
 source ./stroom-proxy/stroom-proxy-app/src/dist/config/scripts.env
+
+ADMIN_PORT=${ADMIN_PORT_OVERRIDE:-ADMIN_PORT}
 
 # Override jar path for dev
 PATH_TO_JAR="./stroom-proxy/stroom-proxy-app//build/libs/stroom-proxy-app-all.jar" \
