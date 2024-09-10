79c7a2277e76d791d4126585d0050060a5e1a875
at055612 <22818309+at055612@users.noreply.github.com>
2024-09-10 14:56:10 +0100

    gh-4436 Update jooq classes

--- stroom-security/stroom-security-impl-db-jooq/src/main/java/stroom/security/impl/db/jooq/Indexes.java
+++ stroom-security/stroom-security-impl-db-jooq/src/main/java/stroom/security/impl/db/jooq/Indexes.java
@@ -9,6 +9,7 @@ import org.jooq.OrderField;
 import org.jooq.impl.DSL;
 import org.jooq.impl.Internal;
 
+import stroom.security.impl.db.jooq.tables.ApiKey;
 import stroom.security.impl.db.jooq.tables.DocPermission;
 
 
@@ -22,5 +23,6 @@ public class Indexes {
     // INDEX definitions
     // -------------------------------------------------------------------------
 
+    public static final Index API_KEY_API_KEY_PREFIX_IDX = Internal.createIndex(DSL.name("api_key_prefix_idx"), ApiKey.API_KEY, new OrderField[] { ApiKey.API_KEY.API_KEY_PREFIX }, false);
     public static final Index DOC_PERMISSION_DOC_PERMISSION_DOC_UUID = Internal.createIndex(DSL.name("doc_permission_doc_uuid"), DocPermission.DOC_PERMISSION, new OrderField[] { DocPermission.DOC_PERMISSION.DOC_UUID }, false);
 }
