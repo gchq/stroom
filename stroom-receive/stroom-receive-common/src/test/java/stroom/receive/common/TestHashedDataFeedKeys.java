ad1333fbb3e6726b31f9ebe6c479a49abd3d72bc
at055612 <22818309+at055612@users.noreply.github.com>
2025-01-24 14:23:36 +0000

    Fix CS

--- stroom-receive/stroom-receive-common/src/test/java/stroom/receive/common/TestHashedDataFeedKeys.java
+++ stroom-receive/stroom-receive-common/src/test/java/stroom/receive/common/TestHashedDataFeedKeys.java
@@ -27,41 +27,6 @@ class TestHashedDataFeedKeys {
     public static void main(String[] args) throws IOException {
         final KeyWithHash keyWithHash1 = DataFeedKeyGenerator.generateFixedTestKey1();
         final KeyWithHash keyWithHash2 = DataFeedKeyGenerator.generateFixedTestKey2();
-//        final DataFeedKeyHasher hasher = new Argon2DataFeedKeyHasher();
-//
-//        @SuppressWarnings("checkstyle:lineLength") final String key1 =
-//                "sdk_"
-//                + hasher.getAlgorithm().getUniqueId()
-//                + "_"
-//                + "okfXqkmtns3k4828fZcnutWUFmegj3hqk83o9sYCLefWGTrRrpT6Bt23FuT1ebwcftPNaL1B7aFbK37gbpefZgQeeP3esbnvNXu612co4awVxpn33He6i1vn7g8kUFEk";
-//        @SuppressWarnings("checkstyle:lineLength") final String key2 =
-//                "sdk_"
-//                + hasher.getAlgorithm().getUniqueId()
-//                + "_"
-//                + "7GqxzCAhBnui4wSCicVtFdmghBxtBAQVDbLrsqDAqthuoHTmVEorJf6xvWviWajwKboJUDvanQXK8UpYroqwfxxYhsG264acXbjcpeQPutNqXrq3rTNqWWYNWaQrj2e1";
-
-//        final HashedDataFeedKey hashedDataFeedKey1 = new HashedDataFeedKey(
-//                hasher.hash(key1),
-//                hasher.getAlgorithm().getUniqueId(),
-//                "datafeed-key-user1",
-//                "user 1",
-//                "1234",
-//                Map.of(
-//                        "key1", "val1",
-//                        "key2", "val2"),
-//                Instant.now().plus(10, ChronoUnit.DAYS).toEpochMilli());
-//
-//        final HashedDataFeedKey hashedDataFeedKey2 = new HashedDataFeedKey(
-//                hasher.hash(key2),
-//                hasher.getAlgorithm().getUniqueId(),
-//                "datafeed-key-user2",
-//                "user 2",
-//                "6789",
-//                Map.of(
-//                        "key3", "val3",
-//                        "key4", "val4"),
-//                Instant.now().plus(10, ChronoUnit.DAYS).toEpochMilli());
-
         final HashedDataFeedKeys hashedDataFeedKeys = new HashedDataFeedKeys(List.of(
                 keyWithHash1.hashedDataFeedKey(),
                 keyWithHash2.hashedDataFeedKey()));
