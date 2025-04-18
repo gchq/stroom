3b171415d41cbb7f6744eb93014c5d99ed5bfc73
stroomdev66 <stroomdev66@gmail.com>
2025-04-18 14:40:03 +0100

    #4456 Fix selection handling across multiple components by uniquely namespacing selections

--- stroom-core-client/src/test/java/stroom/util/client/TestParamUtil.java
+++ stroom-core-client/src/test/java/stroom/util/client/TestParamUtil.java
@@ -74,23 +74,20 @@ class TestParamUtil {
 
         paramValues = new ParamValuesImpl(getMap("key1=value1 key2=value2"));
         result = ParamUtil.replaceParameters("this is $${key1} ${key2}", paramValues);
-        assertThat(result).isEqualTo("this is ${key1} value2");
-
-        result = ParamUtil.replaceParameters("this is $$${key1} ${key2}", paramValues);
         assertThat(result).isEqualTo("this is $value1 value2");
 
-        result = ParamUtil.replaceParameters("this is $$$${key1} ${key2}", paramValues);
-        assertThat(result).isEqualTo("this is $${key1} value2");
-
-        result = ParamUtil.replaceParameters("this is $$$$${key1} ${key2}", paramValues);
+        result = ParamUtil.replaceParameters("this is $$${key1} ${key2}", paramValues);
         assertThat(result).isEqualTo("this is $$value1 value2");
 
-        result = ParamUtil.replaceParameters("$this is $$$$${key1} ${key2}", paramValues);
-        assertThat(result).isEqualTo("$this is $$value1 value2");
+        result = ParamUtil.replaceParameters("this is $$$${key1} ${key2}", paramValues);
+        assertThat(result).isEqualTo("this is $$$value1 value2");
 
-        paramValues = new ParamValuesImpl(getMap("user=user1 user2"));
+        result = ParamUtil.replaceParameters("this is \"${key1}\" ${key2}", paramValues);
+        assertThat(result).isEqualTo("this is \"${key1}\" value2");
+
+        paramValues = new ParamValuesImpl(getMap("user=\"user1 user2\""));
         result = ParamUtil.replaceParameters("${user}", paramValues);
-        assertThat(result).isEqualTo("user1 user2");
+        assertThat(result).isEqualTo("'user1 user2'");
     }
 
     @Test
