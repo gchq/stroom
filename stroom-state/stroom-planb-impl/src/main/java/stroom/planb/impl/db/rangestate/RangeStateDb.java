63dda87c743abc43fb42d643da56f4629d6638de
stroomdev66 <stroomdev66@gmail.com>
2025-07-14 16:19:14 +0100

    #4974 Fix Plan B condense job

--- stroom-state/stroom-planb-impl/src/main/java/stroom/planb/impl/db/rangestate/RangeStateDb.java
+++ stroom-state/stroom-planb-impl/src/main/java/stroom/planb/impl/db/rangestate/RangeStateDb.java
@@ -306,7 +306,25 @@ public class RangeStateDb extends AbstractDb<Key, Val> {
 
     @Override
     public long deleteOldData(final Instant deleteBefore, final boolean useStateTime) {
-        return env.readAndWrite((readTxn, writer) -> {
+        return env.write(writer -> {
+            final long count = deleteOldData(writer, deleteBefore);
+
+            // Delete unused lookup keys.
+            if (!Thread.currentThread().isInterrupted()) {
+                env.read(readTxn -> {
+                    keyRecorder.deleteUnused(readTxn, writer);
+                    valueRecorder.deleteUnused(readTxn, writer);
+                    return null;
+                });
+            }
+
+            return count;
+        });
+    }
+
+    private long deleteOldData(final LmdbWriter writer,
+                               final Instant deleteBefore) {
+        return env.read(readTxn -> {
             long changeCount = 0;
             try (final CursorIterable<ByteBuffer> cursor = dbi.iterate(readTxn)) {
                 final Iterator<KeyVal<ByteBuffer>> iterator = cursor.iterator();
@@ -318,20 +336,16 @@ public class RangeStateDb extends AbstractDb<Key, Val> {
                     if (value.insertTime().isBefore(deleteBefore)) {
                         // If this is data we no longer want to retain then delete it.
                         dbi.delete(writer.getWriteTxn(), kv.key());
-                        writer.tryCommit();
                         changeCount++;
                     } else {
                         // Record used lookup keys.
                         keyRecorder.recordUsed(writer, kv.key());
                         valueRecorder.recordUsed(writer, kv.val());
                     }
+                    writer.tryCommit();
                 }
             }
-
-            // Delete unused lookup keys.
-            keyRecorder.deleteUnused(readTxn, writer);
-            valueRecorder.deleteUnused(readTxn, writer);
-
+            writer.commit();
             return changeCount;
         });
     }
