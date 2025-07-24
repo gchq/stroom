8d42f45f619b16e07b6da8993b3e68b0d99d4ec3
stroomworks4959 <stroomworks4959@gmail.com>
2025-07-24 15:06:50 +0100

    reverting incorrect changes

--- stroom-core-client-widget/src/main/java/stroom/widget/form/client/FormGroup.java
+++ stroom-core-client-widget/src/main/java/stroom/widget/form/client/FormGroup.java
@@ -10,7 +10,6 @@ import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HasWidgets;
@@ -349,13 +348,6 @@ public class FormGroup extends Composite implements HasWidgets {
         if (helpButton.hasHelpContent()) {
             labelPanel.add(helpButton);
         }
-
-        if (NullSafe.isNonBlankString(formLabel.getLabel())) {
-            labelPanel.add(formLabel);
-        }
-        if (helpButton.hasHelpContent()) {
-            labelPanel.add(helpButton);
-        }
     }
 
     private void updateDescriptionPanel() {
@@ -377,13 +369,4 @@ public class FormGroup extends Composite implements HasWidgets {
             return false;
         }
     }
-
-    private native void copyToClipboard(String text) /*-{
-    var textarea = $doc.createElement("textarea");
-    textarea.value = text;
-    $doc.body.appendChild(textarea);
-    textarea.select();
-    $doc.execCommand("copy");
-    $doc.body.removeChild(textarea);
-}-*/;
 }
