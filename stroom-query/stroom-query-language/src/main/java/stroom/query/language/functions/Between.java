83f574b7638f8156f0ac627c344ae4e90adfbbfb
stroomworks4959 <stroomworks4959@gmail.com>
2025-07-28 13:03:06 +0100

    checkstyle fixes

--- stroom-query/stroom-query-language/src/main/java/stroom/query/language/functions/Between.java
+++ stroom-query/stroom-query-language/src/main/java/stroom/query/language/functions/Between.java
@@ -43,9 +43,9 @@ public class Between extends AbstractFunction {
                     return ValErr.create("Between function requires exactly 3 parameters");
                 }
 
-                Val value = params[0] instanceof Val ? (Val) params[0] : Val.create(params[0]);
-                Val lower = params[1] instanceof Val ? (Val) params[1] : Val.create(params[1]);
-                Val upper = params[2] instanceof Val ? (Val) params[2] : Val.create(params[2]);
+                final Val value = params[0] instanceof Val ? (Val) params[0] : Val.create(params[0]);
+                final Val lower = params[1] instanceof Val ? (Val) params[1] : Val.create(params[1]);
+                final Val upper = params[2] instanceof Val ? (Val) params[2] : Val.create(params[2]);
 
                 if (value == null || lower == null || upper == null
                     || value == ValNull.INSTANCE
@@ -57,9 +57,9 @@ public class Between extends AbstractFunction {
                 // Check if all are date strings
                 if (value instanceof ValString && lower instanceof ValString && upper instanceof ValString) {
                     try {
-                        java.time.LocalDate v = java.time.LocalDate.parse(value.toString());
-                        java.time.LocalDate l = java.time.LocalDate.parse(lower.toString());
-                        java.time.LocalDate u = java.time.LocalDate.parse(upper.toString());
+                        final java.time.LocalDate v = java.time.LocalDate.parse(value.toString());
+                        final java.time.LocalDate l = java.time.LocalDate.parse(lower.toString());
+                        final java.time.LocalDate u = java.time.LocalDate.parse(upper.toString());
                         return ValBoolean.create((!v.isBefore(l)) && (!v.isAfter(u)));
                     } catch (Exception e) {
                         return ValErr.create("Parameters must be valid ISO date strings for between function");
@@ -69,12 +69,13 @@ public class Between extends AbstractFunction {
                 // Check if all are numeric
                 if (value instanceof ValNumber && lower instanceof ValNumber && upper instanceof ValNumber) {
                     try {
-                        double v = value.toDouble();
-                        double l = lower.toDouble();
-                        double u = upper.toDouble();
+                        final double v = value.toDouble();
+                        final double l = lower.toDouble();
+                        final double u = upper.toDouble();
                         return ValBoolean.create(l <= v && v <= u);
                     } catch (Exception e) {
-                        return ValErr.create("Parameters must be all numeric or all ISO date strings for between function");
+                        return ValErr
+                                .create("Parameters must be all numeric or all ISO date strings for between function");
                     }
                 }
 
--- stroom-query/stroom-query-language/src/test/java/stroom/query/language/functions/TestBetween.java
+++ stroom-query/stroom-query-language/src/test/java/stroom/query/language/functions/TestBetween.java
@@ -13,21 +13,35 @@ class TestBetween extends AbstractFunctionTest<Between> {
     Stream<TestCase> getTestCases() {
         return Stream.of(
                 //between lower and upper
-                TestCase.of("value between bounds", ValBoolean.TRUE, ValDouble.create(5), ValDouble.create(1), ValDouble.create(10)),
+                TestCase.of("value between bounds",
+                        ValBoolean.TRUE, ValDouble.create(5),
+                        ValDouble.create(1), ValDouble.create(10)),
                 //equal to lower bound
-                TestCase.of("value equals lower", ValBoolean.TRUE, ValDouble.create(1), ValDouble.create(1), ValDouble.create(10)),
+                TestCase.of("value equals lower",
+                        ValBoolean.TRUE, ValDouble.create(1),
+                        ValDouble.create(1), ValDouble.create(10)),
                 //equal to upper bound
-                TestCase.of("value equals upper", ValBoolean.TRUE, ValDouble.create(10), ValDouble.create(1), ValDouble.create(10)),
+                TestCase.of("value equals upper",
+                        ValBoolean.TRUE, ValDouble.create(10),
+                        ValDouble.create(1), ValDouble.create(10)),
                 //below lower bound
-                TestCase.of("value below lower", ValBoolean.FALSE, ValDouble.create(0), ValDouble.create(1), ValDouble.create(10)),
+                TestCase.of("value below lower",
+                        ValBoolean.FALSE, ValDouble.create(0), ValDouble.create(1), ValDouble.create(10)),
                 //above upper bound
-                TestCase.of("value above upper", ValBoolean.FALSE, ValDouble.create(11), ValDouble.create(1), ValDouble.create(10)),
+                TestCase.of("value above upper",
+                        ValBoolean.FALSE, ValDouble.create(11), ValDouble.create(1), ValDouble.create(10)),
                 //non-numeric
-                TestCase.of("non-numeric value", ValErr.create("Parameters must be all numeric or all ISO date strings for between function"), ValString.create("a"), ValDouble.create(1), ValDouble.create(10)),
+                TestCase.of("non-numeric value",
+                        ValErr.create("Parameters must be all numeric or all ISO date strings for between function"),
+                        ValString.create("a"), ValDouble.create(1), ValDouble.create(10)),
                 //null
-                TestCase.of("null value", ValErr.create("Null parameter in between function"), ValNull.INSTANCE, ValDouble.create(1), ValDouble.create(10)),
+                TestCase.of("null value",
+                        ValErr.create("Null parameter in between function"),
+                        ValNull.INSTANCE, ValDouble.create(1), ValDouble.create(10)),
                 //date
-                TestCase.of("date between bounds", ValBoolean.TRUE, ValString.create("2023-01-05"), ValString.create("2023-01-01"), ValString.create("2023-01-10"))
+                TestCase.of("date between bounds",
+                        ValBoolean.TRUE, ValString.create("2023-01-05"),
+                        ValString.create("2023-01-01"), ValString.create("2023-01-10"))
         );
     }
 }
