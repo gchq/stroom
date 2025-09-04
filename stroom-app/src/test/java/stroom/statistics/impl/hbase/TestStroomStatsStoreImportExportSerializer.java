c2def35db454864634593607dbf98fa1351136a3
Stroomworks 4092 <stroomworks4092@gmail.com>
2025-09-04 11:59:06 +0100

    Added a couple of tests for V2 Import/Export

--- stroom-app/src/test/java/stroom/statistics/impl/hbase/TestStroomStatsStoreImportExportSerializer.java
+++ stroom-app/src/test/java/stroom/statistics/impl/hbase/TestStroomStatsStoreImportExportSerializer.java
@@ -113,4 +113,64 @@ class TestStroomStatsStoreImportExportSerializer extends AbstractCoreIntegration
 
         assertThat(importedDataSource.getConfig()).isEqualTo(entity.getConfig());
     }
+
+    /**
+     * Create a populated {@link StatisticStore} object, serialise it to file,
+     * de-serialise it back to an object then compare the first object with the
+     * second one
+     */
+    @Test
+    void testStatisticsDataSourceV2() {
+        final ExplorerNode statNode = explorerService.create(StroomStatsStoreDoc.TYPE,
+                "StatName1",
+                null,
+                null);
+        final StroomStatsStoreDoc entity = stroomStatsStoreStore.readDocument(statNode.getDocRef());
+        entity.setDescription("My Description");
+        entity.setStatisticType(StatisticType.COUNT);
+        entity.setConfig(new StroomStatsStoreEntityData());
+        entity.getConfig().addStatisticField(new StatisticField("tag1"));
+        entity.getConfig().addStatisticField(new StatisticField("tag2"));
+        stroomStatsStoreStore.writeDocument(entity);
+
+        assertThat(stroomStatsStoreStore.list().size()).isEqualTo(1);
+
+        final Path testDataDir = getCurrentTestDir().resolve("ExportTest");
+
+        FileUtil.deleteDir(testDataDir);
+        FileUtil.mkdirs(testDataDir);
+
+        importExportSerializer.write(
+                null,
+                testDataDir,
+                Set.of(statNode.getDocRef()),
+                Collections.emptySet(),
+                true,
+                ImportExportVersion.V2);
+
+        assertThat(FileUtil.count(testDataDir)).isEqualTo(2);
+
+        // now clear out the java entities and import from file
+        commonTestControl.clear();
+
+        assertThat(stroomStatsStoreStore.list().size()).isEqualTo(0);
+
+        importExportSerializer.read(
+                testDataDir,
+                null,
+                ImportSettings.auto());
+
+        final List<DocRef> dataSources = stroomStatsStoreStore.list();
+
+        assertThat(dataSources.size()).isEqualTo(1);
+
+        final StroomStatsStoreDoc importedDataSource = stroomStatsStoreStore.readDocument(dataSources.get(0));
+
+        assertThat(importedDataSource.getName()).isEqualTo(entity.getName());
+        assertThat(importedDataSource.getStatisticType()).isEqualTo(entity.getStatisticType());
+        assertThat(importedDataSource.getDescription()).isEqualTo(entity.getDescription());
+
+        assertThat(importedDataSource.getConfig()).isEqualTo(entity.getConfig());
+    }
+
 }
--- stroom-app/src/test/java/stroom/statistics/impl/sql/TestStatisticsDataSourceImportExportSerializer.java
+++ stroom-app/src/test/java/stroom/statistics/impl/sql/TestStatisticsDataSourceImportExportSerializer.java
@@ -123,6 +123,62 @@ class TestStatisticsDataSourceImportExportSerializer extends AbstractCoreIntegra
         assertThat(importedDataSource.getConfig()).isEqualTo(statisticsDataSource.getConfig());
     }
 
+    /**
+     * Create a populated {@link StatisticStore} object, serialise it to file,
+     * de-serialise it back to an object then compare the first object with the
+     * second one
+     */
+    @Test
+    void testStatisticsDataSourceV2() {
+        final ExplorerNode statNode = explorerService.create(StatisticStoreDoc.TYPE, "StatName1", null, null);
+        final StatisticStoreDoc statisticsDataSource = statisticStoreStore.readDocument(statNode.getDocRef());
+        statisticsDataSource.setDescription("My Description");
+        statisticsDataSource.setStatisticType(StatisticType.COUNT);
+        statisticsDataSource.setConfig(new StatisticsDataSourceData());
+        statisticsDataSource.getConfig().addStatisticField(new StatisticField("tag1"));
+        statisticsDataSource.getConfig().addStatisticField(new StatisticField("tag2"));
+        statisticStoreStore.writeDocument(statisticsDataSource);
+
+        assertThat(statisticStoreStore.list().size()).isEqualTo(1);
+
+        final Path testDataDir = getCurrentTestDir().resolve("ExportTest");
+
+        FileUtil.deleteDir(testDataDir);
+        FileUtil.mkdirs(testDataDir);
+
+        importExportSerializer.write(
+                null,
+                testDataDir,
+                buildFindFolderCriteria(),
+                Collections.emptySet(),
+                true,
+                ImportExportVersion.V2);
+
+        assertThat(FileUtil.count(testDataDir)).isEqualTo(2);
+
+        // now clear out the java entities and import from file
+        commonTestControl.clear();
+
+        assertThat(statisticStoreStore.list().size()).isEqualTo(0);
+
+        importExportSerializer.read(
+                testDataDir,
+                null,
+                ImportSettings.auto());
+
+        final List<DocRef> dataSources = statisticStoreStore.list();
+
+        assertThat(dataSources.size()).isEqualTo(1);
+
+        final StatisticStoreDoc importedDataSource = statisticStoreStore.readDocument(dataSources.get(0));
+
+        assertThat(importedDataSource.getName()).isEqualTo(statisticsDataSource.getName());
+        assertThat(importedDataSource.getStatisticType()).isEqualTo(statisticsDataSource.getStatisticType());
+        assertThat(importedDataSource.getDescription()).isEqualTo(statisticsDataSource.getDescription());
+
+        assertThat(importedDataSource.getConfig()).isEqualTo(statisticsDataSource.getConfig());
+    }
+
     @Test
     void testDeSerialiseOnLoad() {
         final DocRef docRef = statisticStoreStore.createDocument("StatName1");
@@ -149,4 +205,5 @@ class TestStatisticsDataSourceImportExportSerializer extends AbstractCoreIntegra
 
         final DocRef statisticDataSource3DocRef = DocRefUtil.create(statisticsDataSource3);
     }
+
 }
