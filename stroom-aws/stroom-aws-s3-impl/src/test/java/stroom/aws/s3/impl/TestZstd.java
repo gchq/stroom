dc0a0b63d4419f5c78e108affb3f5316ebd63dd1
at055612 <22818309+at055612@users.noreply.github.com>
2025-12-03 11:09:07 +0000

    gh-5279 Fix compile error post merge

--- stroom-aws/stroom-aws-s3-impl/src/test/java/stroom/aws/s3/impl/TestZstd.java
+++ stroom-aws/stroom-aws-s3-impl/src/test/java/stroom/aws/s3/impl/TestZstd.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2025 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package stroom.aws.s3.impl;
 
 import stroom.bytebuffer.ByteBufferUtils;
@@ -299,10 +315,12 @@ public class TestZstd {
         // Includes the skippable frame
         final int totalCompressedSize = compressedBuffer.capacity();
         final int len = FrameInfo.SEEKABLE_MAGIC_NUMBER.length;
-        final int result = ByteBufferUtils.compareTo(
-                compressedBuffer, totalCompressedSize - len, len,
-                FrameInfo.SEEKABLE_MAGIC_NUMBER_BUFFER, 0, len);
-        return result == 0;
+        return ByteBufferUtils.equals(
+                compressedBuffer,
+                totalCompressedSize - len,
+                FrameInfo.SEEKABLE_MAGIC_NUMBER_BUFFER,
+                0,
+                len);
     }
 
     private int getFrameCount(final ByteBuffer compressedBuffer) {
