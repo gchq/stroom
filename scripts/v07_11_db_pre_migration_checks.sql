abc480d37955a3bfc7022104361574a9fc7729ba
at055612 <22818309+at055612@users.noreply.github.com>
2026-04-16 16:03:24 +0100

    Add pre-mig script for 7.11

---
+++ scripts/v07_11_db_pre_migration_checks.sql
@@ -0,0 +1,25 @@
+-- A set of SQL queries to run before migrating from v6 to v7
+-- See https://gchq.github.io/stroom-docs/releases/v07.11/upgrade-notes/
+-- 
+-- Run with the mysql --table arg to get formatted output
+-- e.g.
+-- mysql --force --table -h"localhost" -P"3306" -u"stroomuser" -p"stroompassword1" stroom < v07_11_db_pre_migration_checks.sql > v07_11_db_pre_migration_checks.out 2>&1
+-- docker exec -i stroom-all-dbs mysql --force --table -h"localhost" -P"3307" -u"stroomuser" -p"stroompassword1" stroom < v07_11_db_pre_migration_checks.sql > v07_11_db_pre_migration_checks.out 2>&1
+
+
+\! echo 'Find annotation entries with no assigned user. No action required if this returns nothing.';
+
+SELECT a.id, a.title, ae.id AS entry_id
+FROM annotation a
+INNER JOIN annotation_entry ae ON a.id = ae.fk_annotation_id
+WHERE ae.entry_user_uuid IS NULL;
+
+\! echo 'Listing all enabled Stroom users for reference';
+
+SELECT uuid, name, display_name, full_name
+FROM stroom_user su
+WHERE is_group = false
+AND enabled = true
+ORDER by name;
+
+\! echo 'Finished';
