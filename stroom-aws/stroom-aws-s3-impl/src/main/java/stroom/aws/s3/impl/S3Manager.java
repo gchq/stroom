cb6c13256ecfac0efdc86f7ef5a0d0ee3f73a17b
stroomdev66 <stroomdev66@gmail.com>
2026-06-08 10:39:32 +0100

    #5552 Add additional S3 properties to S3Appender

--- stroom-aws/stroom-aws-s3-impl/src/main/java/stroom/aws/s3/impl/S3Manager.java
+++ stroom-aws/stroom-aws-s3-impl/src/main/java/stroom/aws/s3/impl/S3Manager.java
@@ -708,18 +708,10 @@ public class S3Manager {
                 .metadata(metadata);
 
         if (uploadProperties != null) {
-            if (NullSafe.isNonBlankString(uploadProperties.cacheControl())) {
-                builder = builder.cacheControl(uploadProperties.cacheControl());
-            }
-            if (NullSafe.isNonBlankString(uploadProperties.contentDisposition())) {
-                builder = builder.contentDisposition(uploadProperties.contentDisposition());
-            }
-            if (NullSafe.isNonBlankString(uploadProperties.contentEncoding())) {
-                builder = builder.contentEncoding(uploadProperties.contentEncoding());
-            }
-            if (NullSafe.isNonBlankString(uploadProperties.contentType())) {
-                builder = builder.contentType(uploadProperties.contentType());
-            }
+            NullSafe.consumeNonBlankString(uploadProperties.cacheControl(), builder::cacheControl);
+            NullSafe.consumeNonBlankString(uploadProperties.contentDisposition(), builder::contentDisposition);
+            NullSafe.consumeNonBlankString(uploadProperties.contentEncoding(), builder::contentEncoding);
+            NullSafe.consumeNonBlankString(uploadProperties.contentType(), builder::contentType);
         }
 
         return builder.build();
