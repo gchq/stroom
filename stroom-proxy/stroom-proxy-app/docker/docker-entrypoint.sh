06e4e87a74ae8ef6e0a34c6fe1591fdfc513ab82
at055612 <22818309+at055612@users.noreply.github.com>
2026-06-11 13:11:56 +0100

    Combine entrypoint into one script, add su-exec back in

--- stroom-proxy/stroom-proxy-app/docker/docker-entrypoint.sh
+++ stroom-proxy/stroom-proxy-app/docker/docker-entrypoint.sh
@@ -17,7 +17,62 @@
 
 set -e
 
-# shellcheck disable=SC1091
-. /stroom-proxy/add_container_identity_headers.sh
+HEADERS_FILE="/stroom-proxy/logs/extra_headers.txt"
+# These should be set in the dockerfile, but just in case
+USER_ID="${USER_ID:-1000}"
+GROUP_ID="${GROUP_ID:-0}"
 
-exec "$@"
+add_to_headers() {
+    token="$1"
+    value="$2"
+    if [ -n "${value}" ]; then
+        echo "${token}:${value}" >> "${HEADERS_FILE}"
+    fi
+}
+
+create_headers_file() {
+    # This command works in alpine linux 3.8, but may be fragile and subject to change
+    # with different docker versions.
+    container_id=
+    if [ -f /proc/self/cgroup ]; then
+      container_id="$( \
+        grep -o -e "docker/.*" /proc/self/cgroup 2>/dev/null \
+          | head -n 1 \
+          | sed "s/docker\/\(.*\)/\\1/" \
+      )" || true
+    fi
+    container_id="${container_id:-}"
+
+    # Create the empty file
+    : > "${HEADERS_FILE}"
+
+    # Ensure anyone can read this file
+    chmod ugo+r "${HEADERS_FILE}"
+
+    # Write the details of the host of this container to the headers file
+    # for stroom-log-sender
+
+    # These two should be passed in as env vars at container run time
+    add_to_headers "OriginalHost" "${DOCKER_HOST_HOSTNAME:-}"
+    add_to_headers "OriginalIP" "${DOCKER_HOST_IP:-}"
+    # This env var should be automatcially set from the build arg at container creation time
+    add_to_headers "OriginalImageGitTag" "${GIT_TAG:-}"
+    add_to_headers "OriginalContainerId" "${container_id:-}"
+
+    echo "Dumping ${HEADERS_FILE} contents:"
+    echo "-------------------------------------------"
+    cat "${HEADERS_FILE}"
+    echo "-------------------------------------------"
+}
+
+create_headers_file
+
+# In case anyone tries to run the container as root drop down
+if [ "$(id -u)" = '0' ]; then
+    #su-exec is the alpine equivalent of gosu
+    #runs all args as user USER_ID:GROUP_ID, rather than as root
+    exec su-exec "$USER_ID:$GROUP_ID" "$@"
+else
+  # Already non-root so just crack on and run the cmd as is
+  exec "$@"
+fi
