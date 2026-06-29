080672359bf8e210b465dca758ef750c4f8e8780
at055612 <22818309+at055612@users.noreply.github.com>
2026-06-29 11:06:56 +0100

    gh-5596 Change S3Appender to replaceContextVars in key template

--- stroom-aws/stroom-aws-s3-impl/src/main/java/stroom/aws/s3/impl/S3Manager.java
+++ stroom-aws/stroom-aws-s3-impl/src/main/java/stroom/aws/s3/impl/S3Manager.java
@@ -395,6 +395,7 @@ public class S3Manager {
     public String createBucketName(final String bucketNamePattern,
                                    final Meta meta) {
         String bucketName = bucketNamePattern;
+
         bucketName = pathCreator.replace(bucketName, "feed", meta::getFeedName);
         bucketName = pathCreator.replace(bucketName, "type", meta::getTypeName);
         bucketName = bucketName.toLowerCase(Locale.ROOT);
@@ -643,6 +644,8 @@ public class S3Manager {
                 ZonedDateTime.ofInstant(Instant.ofEpochMilli(meta.getCreateMs()), ZoneOffset.UTC);
         final String idPadded = padId(meta.getId());
         keyName = pathCreator.replaceTimeVars(keyName, zonedDateTime);
+        // Parse for stuff like partNo, pipeline, node, etc.
+        keyName = pathCreator.replaceContextVars(keyName);
         keyName = pathCreator.replace(keyName, "feed", meta::getFeedName);
         keyName = pathCreator.replace(keyName, "type", meta::getTypeName);
         keyName = pathCreator.replace(keyName, "id", () -> String.valueOf(meta.getId()));
