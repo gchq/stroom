d6c305c9ba454d2f9d6433710244c07e1cd77acb
stroomdev66 <stroomdev66@gmail.com>
2026-08-04 13:01:01 +0100

    Improve javadoc

--- stroom-cluster/stroom-cluster-lock-api/src/main/java/stroom/cluster/lock/api/ClusterLockService.java
+++ stroom-cluster/stroom-cluster-lock-api/src/main/java/stroom/cluster/lock/api/ClusterLockService.java
@@ -19,28 +19,48 @@ package stroom.cluster.lock.api;
 import java.util.function.Supplier;
 
 /**
- * tryLock and release lock utilises the cluster but the master node must be up
- * to work
+ * Cluster wide mutual exclusion, used to ensure that only one node at a time runs some piece of
+ * work.
  * <p>
- * lock uses the database
+ * All methods are backed by a row lock on the {@code cluster_lock} table. <b>No master node and no
+ * inter-node communication is involved</b>, so a lock can be taken by any node and works whether or
+ * not a master has been elected. This makes a cluster lock the preferred way to say "only one node
+ * should do this at a time" in preference to gating work on the node being the master.
+ * <p>
+ * The lock is held for as long as the supplied runnable/supplier takes to run and is released when
+ * its transaction commits, so the work done under a lock should be bounded.
+ * <p>
+ * Locking is <b>not fair</b>. A node waiting on a lock that is held for longer than the database
+ * lock wait timeout will rejoin the back of the queue, so waiters are not served in arrival order.
+ * <p>
+ * Lock rows are created on demand the first time a name is used and are never deleted.
  */
 public interface ClusterLockService {
 
     /**
      * Will attempt to get the database backed lock for lockName. If it gets the lock then will run
-     * runnable under that lock. If not it will return without running runnable.
+     * runnable under that lock. If not it will return immediately without running runnable.
+     * <p>
+     * Use this for periodic work where missing a cycle is harmless, as a contended lock skips the
+     * work rather than queueing a thread behind it.
      */
     void tryLock(final String lockName, final Runnable runnable);
 
     /**
      * Will block until the database backed lock for lockName is obtained, then will run
-     * runnable under that lock
+     * runnable under that lock.
+     *
+     * @throws RuntimeException if the lock could not be obtained within the configured lock
+     *                          timeout.
      */
     void lock(final String lockName, final Runnable runnable);
 
     /**
      * Will block until the database backed lock for lockName is obtained, then will run
-     * supplier under that lock, returning supplier's return value
+     * supplier under that lock, returning supplier's return value.
+     *
+     * @throws RuntimeException if the lock could not be obtained within the configured lock
+     *                          timeout.
      */
     <T> T lockResult(final String lockName, final Supplier<T> supplier);
 }
