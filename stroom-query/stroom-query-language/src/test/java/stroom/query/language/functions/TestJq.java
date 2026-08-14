23c513d97f63238b78dcde23657d3968acffaff7
stroomdev66 <stroomdev66@gmail.com>
2026-08-14 13:09:59 +0100

    #5645 Change the dashboard/query `xpath()` function to concatenate the values of all matched items

--- stroom-query/stroom-query-language/src/test/java/stroom/query/language/functions/TestJq.java
+++ stroom-query/stroom-query-language/src/test/java/stroom/query/language/functions/TestJq.java
@@ -51,10 +51,40 @@ class TestJq extends AbstractFunctionTest<Jq> {
                         ValString.create(".arr[1]")),
                 TestCase.of(
                         "Filter",
-                        // Multiple nodes are returned in an array string with standard spacing
-                        ValString.create("[2, 3]"),
+                        // The values of all the matched elements are concatenated.
+                        ValString.create("23"),
                         ValString.create(json),
                         ValString.create(".arr[] | select(. > 1)")),
+                TestCase.of(
+                        "Filter with delimiter",
+                        ValString.create("2, 3"),
+                        ValString.create(json),
+                        ValString.create(".arr[] | select(. > 1)"),
+                        ValString.create(", ")),
+                TestCase.of(
+                        "All array elements",
+                        ValString.create("1|2|3"),
+                        ValString.create(json),
+                        ValString.create(".arr[]"),
+                        ValString.create("|")),
+                TestCase.of(
+                        "Multiple objects",
+                        ValString.create("{\"a\":1},{\"b\":2}"),
+                        ValString.create("{\"items\":[{\"a\":1},{\"b\":2}]}"),
+                        ValString.create(".items[]"),
+                        ValString.create(",")),
+                TestCase.of(
+                        "Single match with delimiter",
+                        ValString.create("bar"),
+                        ValString.create(json),
+                        ValString.create(".foo"),
+                        ValString.create(",")),
+                TestCase.of(
+                        "Delimiter is only inserted between values",
+                        ValString.create("bar"),
+                        ValString.create(json),
+                        ValString.create(".foo"),
+                        ValString.create("")),
                 TestCase.of(
                         "No match",
                         ValNull.INSTANCE,
@@ -67,11 +97,16 @@ class TestJq extends AbstractFunctionTest<Jq> {
                         ValString.create("{unclosed"),
                         ValString.create(".")),
                 TestCase.of(
-                        "Invalid JQ",
+                        "Empty JQ",
                         ValString.create(
                                 "An empty JQ expression has been defined for second argument of 'Jq' function"),
                         ValString.create(json),
-                        ValString.create(""))
+                        ValString.create("")),
+                TestCase.of(
+                        "Invalid JQ",
+                        ValString.create("Error in JQ expression"),
+                        ValString.create(json),
+                        ValString.create(".["))
         );
     }
 }
