bb8a029599e3f13a9e7c7d7c84cdb9a095253e4c
stroomdev66 <stroomdev66@gmail.com>
2026-08-27 16:40:49 +0100

    #5745 Fix meta find query performance

--- stroom-meta/stroom-meta-impl-db/src/main/java/stroom/meta/impl/db/jooq/tables/MetaVal.java
+++ stroom-meta/stroom-meta-impl-db/src/main/java/stroom/meta/impl/db/jooq/tables/MetaVal.java
@@ -4,10 +4,9 @@
 package stroom.meta.impl.db.jooq.tables;
 
 
-import stroom.meta.impl.db.jooq.Indexes;
-import stroom.meta.impl.db.jooq.Keys;
-import stroom.meta.impl.db.jooq.Stroom;
-import stroom.meta.impl.db.jooq.tables.records.MetaValRecord;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import org.jooq.Condition;
 import org.jooq.Field;
@@ -28,9 +27,10 @@ import org.jooq.impl.DSL;
 import org.jooq.impl.SQLDataType;
 import org.jooq.impl.TableImpl;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import stroom.meta.impl.db.jooq.Indexes;
+import stroom.meta.impl.db.jooq.Keys;
+import stroom.meta.impl.db.jooq.Stroom;
+import stroom.meta.impl.db.jooq.tables.records.MetaValRecord;
 
 
 /**
